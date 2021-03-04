cb425fa4bbc763cee1f2f86719263bbd8115665d
Hermet Park <chuneon.park@samsung.com>
2021-03-04 11:14:16 +0900

    capi: fix incorrect composite api.
    Api prototype is completely wrong, now it's corrected.

--- inc/thorvg_capi.h
+++ inc/thorvg_capi.h
@@ -579,15 +579,16 @@ TVG_EXPORT Tvg_Paint* tvg_paint_duplicate(Tvg_Paint* paint);
 TVG_EXPORT Tvg_Result tvg_paint_get_bounds(const Tvg_Paint* paint, float* x, float* y, float* w, float* h);
 
 /*!
-* \fn TVG_EXPORT Tvg_Result tvg_paint_set_composite_method(const Tvg_Paint* paint, Tvg_Composite_Method method)
-* \brief The function set composition method
-* \param[in] paint Tvg_Paint pointer
+* \fn TVG_EXPORT Tvg_Result tvg_paint_set_composite_method(Tvg_Paint* paint, Tvg_Paint* target, Tvg_Composite_Method method)
+* \brief The function set composition method.
+* \param[in] paint Tvg_Paint composition source
+* \param[in] target Tvg_Paint composition target
 * \param[in] method Tvg_Composite_Method used composite method
 * \return Tvg_Result return value
 * - TVG_RESULT_SUCCESS: if ok.
 * - TVG_RESULT_INVALID_PARAMETERS: if paint is invalid
 */
-TVG_EXPORT Tvg_Result tvg_paint_set_composite_method(const Tvg_Paint* paint, Tvg_Composite_Method method);
+TVG_EXPORT Tvg_Result tvg_paint_set_composite_method(Tvg_Paint* paint, Tvg_Paint* target, Tvg_Composite_Method method);
 
 /************************************************************************/
 /* Shape API                                                            */
