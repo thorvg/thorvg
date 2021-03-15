d16ae4b228902bd91cf0bb1b5dff9e3ead373ff6
Michal Maciola <m.maciola@samsung.com>
2021-03-15 16:57:12 +0100

    shape: stroke reset withdraw

--- src/lib/sw_engine/tvgSwShape.cpp
+++ src/lib/sw_engine/tvgSwShape.cpp
@@ -465,7 +465,6 @@ void shapeDelOutline(SwShape* shape, uint32_t tid)
 void shapeReset(SwShape* shape)
 {
     rleReset(shape->rle);
-    rleReset(shape->strokeRle);
     shape->rect = false;
     _initBBox(shape->bbox);
 }
