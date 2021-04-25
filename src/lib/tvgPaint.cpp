a56d97b8434ae072a9ea5ce89eb7478a6711cb50
Mira Grudzinska <m.grudzinska@samsung.com>
2021-04-25 23:40:03 +0200

    common paint: changed result in composite API
    A possible error is due to erroneous arguments, so
    InvalidArgument is return instead of InsufficientCondition

--- src/lib/tvgPaint.cpp
+++ src/lib/tvgPaint.cpp
@@ -298,7 +298,7 @@ Paint* Paint::duplicate() const noexcept
 Result Paint::composite(std::unique_ptr<Paint> target, CompositeMethod method) const noexcept
 {
     if (pImpl->composite(target.release(), method)) return Result::Success;
-    return Result::InsufficientCondition;
+    return Result::InvalidArguments;
 }
 
 
