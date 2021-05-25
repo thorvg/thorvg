0b936c13d7c853a0fa5ad643dfaf34a2b60d7e81
JunsuChoi <jsuya.choi@samsung.com>
2021-05-25 14:44:35 +0900

    infra Documentation: Update main page and remove treeview

--- docs/html/group__ThorVGCapi__Scene.js
+++
@@ -1,7 +0,0 @@
-var group__ThorVGCapi__Scene =
-[
-    [ "tvg_scene_clear", "group__ThorVGCapi__Scene.html#ga00044cc1d187a83c196debd44eab0248", null ],
-    [ "tvg_scene_new", "group__ThorVGCapi__Scene.html#ga7572898d23143d37faa85cc89ad1625e", null ],
-    [ "tvg_scene_push", "group__ThorVGCapi__Scene.html#ga16cbf3d1bdd02ebfc5a93c5d03f2d6cf", null ],
-    [ "tvg_scene_reserve", "group__ThorVGCapi__Scene.html#ga8b712509a62cf5a3200ecbca53e773d3", null ]
-];
--- docs/html/modules.js
+++
@@ -1,5 +0,0 @@
-var modules =
-[
-    [ "ThorVG", "group__ThorVG.html", "group__ThorVG" ],
-    [ "ThorVG_CAPI (BETA version)", "group__ThorVG__CAPI.html", "group__ThorVG__CAPI" ]
-];
