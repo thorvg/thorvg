98fa959ad7aaf25c57fea135eb8ddf14ec182c98
Hermet Park <chuneon.park@samsung.com>
2021-06-03 18:07:05 +0900

    tests: introduce catch2 unit tests infrastructure.
    Catch2 is a multi-paradigm test framework for C++.
    It is primarily distributed as a single header file,
    very easy and simple to adopt this to thorvg project.
    
    This patch introduces catch2 infrastsructure and one prototype as a sample.
    
    You can refer "testInitializer.cpp", how to add unit test!
    while ignoring else files such as "catch2.hpp", "testMain.cpp"
    
    Also, enable Unit-tests with meson option when you change any thorvg code.
    
    $meson build -Dtests=true.
    
    launch tvgUnitTest in the build result then verify 100% coverage
    before submitting any patches.

---
+++ test/testInitializer.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020-2021 Samsung Electronics Co., Ltd. All rights reserved.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <thorvg.h>
+#include "catch.hpp"
+
+using namespace tvg;
+
+
+TEST_CASE("Basic initialization", "[tvgInitializer]")
+{
+   REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+   REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Multiple initialization", "[tvgInitializer]")
+{
+   REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+   REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+   REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+
+   REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+   REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+   REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Negative termination", "[tvgInitializer]")
+{
+   REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::InsufficientCondition);
+}
+
+TEST_CASE("Many threads", "[tvgInitializer]")
+{
+   REQUIRE(Initializer::init(CanvasEngine::Sw, -1) == Result::FailedAllocation);
+   REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::InsufficientCondition);
+}
+
+TEST_CASE("Non supported engine", "[tvgInitializer]")
+{
+   REQUIRE(Initializer::init(CanvasEngine(0), 0) == Result::InvalidArguments);
+}
