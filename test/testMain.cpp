6b7aa8ad9e6c446e6c9aef5557a54504d48594f0
Hermet Park <hermetpark@gmail.com>
2021-06-04 16:46:34 +0900

    tests: introduce catch2 unit tests infrastructure.
    Catch2 is a multi-paradigm test framework for C++.
    It is primarily distributed as a single header file,
    very easy and simple to adopt this to thorvg project.
    
    This patch introduces catch2 infrastsructure and one prototype as a sample.
    
    You can refer "testInitializer.cpp", how to add unit test!
    while ignoring else files such as "catch2.hpp", "testMain.cpp"
    
    Also, enable Unit-tests with meson option when you change any thorvg code.
    
    $meson build -Dtests=true.
    
    launch tvgUnitTest in the build result then verify 100% coverage
    before submitting any patches.

---
+++ test/testMain.cpp
@@ -0,0 +1,5 @@
+// The only purpose of this file is to DEFINE the catch config so it can include main()
+
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
