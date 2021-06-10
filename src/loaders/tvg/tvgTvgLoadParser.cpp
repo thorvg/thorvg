50b3328fe121d5d7d24067ee597081f792116c16
Michal Maciola <m.maciola@samsung.com>
2021-06-10 14:04:30 +0200

    TVG Loader/Saver: tvg loader introduced: Fix #6
    Deleted functions comments

--- src/loaders/tvg/tvgTvgLoadParser.cpp
+++ src/loaders/tvg/tvgTvgLoadParser.cpp
@@ -23,13 +23,10 @@
 #include <string.h>
 #include "tvgTvgLoadParser.h"
 
-// Enumeration specifying the loading result
 enum class LoaderResult { InvalidType, Success, SizeCorruption, MemoryCorruption, LogicalCorruption };
 
-// Load paint from .tvg binary file.
 static LoaderResult _parsePaint(tvgBlock block, Paint ** paint);
 
-// Read tvgBlock from pointer location.
 static tvgBlock _readTvgBlock(const char *pointer)
 {
     tvgBlock block;
@@ -40,19 +37,6 @@ static tvgBlock _readTvgBlock(const char *pointer)
     return block;
 }
 
-/*
- * Read header of the .tvg binary file
- * Returns true on success, false otherwise.
- * Details:
- * Each file starts with "TVG" phrase and three chars of version number.
- * Two next bytes are the metadata length (amount of bytes to skip if the meta is not read).
- * Metadata may contain file generation time and date or other information about device and file.
- * ["TVG"][version eg. "01a"][uint16 metadata length eg. 0x0000][metadata]
- *
- * Starting phase together with metadata are called header.
- * After header, binary section starts.
- * NOTE: In whole file little endian is used.
- */
 static bool _readTvgHeader(const char **pointer)
 {
     // Sign phase, always "TVG" declared in TVG_HEADER_TVG_SIGN_CODE
@@ -80,10 +64,6 @@ static bool _readTvgHeader(const char **pointer)
 }
 
 // Paint
-/*
- * Load paint's cmpMethod and cmpTarget from .tvg binary file.
- * Returns LoaderResult::Success on success or LoaderResult::SizeCorruption / MemoryCorruption if corrupted.
- */
 static LoaderResult _parseCmpTarget(const char *pointer, const char *end, Paint *paint)
 {
     auto block = _readTvgBlock(pointer);
@@ -119,15 +99,6 @@ static LoaderResult _parseCmpTarget(const char *pointer, const char *end, Paint 
     return LoaderResult::Success;
 }
 
-/*
- * Load paint from .tvg binary file.
- * Returns LoaderResult::Success on success, LoaderResult::InvalidType or other if corrupted.
- *
- * Details:
- * [TVG_PAINT_OPACITY_INDICATOR][1][uint8_t opacity]
- * [TVG_PAINT_TRANSFORM_MATRIX_INDICATOR][36][Matrix]
- * [TVG_PAINT_CMP_TARGET_INDICATOR] - see _parseCmpTarget()
- */
 static LoaderResult _parsePaintProperty(tvgBlock block, Paint *paint)
 {
     switch (block.type)
@@ -156,14 +127,6 @@ static LoaderResult _parsePaintProperty(tvgBlock block, Paint *paint)
 }
 
 // Scene
-/*
- * Load scene from .tvg binary file.
- * Returns LoaderResult::Success on success, LoaderResult::InvalidType or other if corrupted.
- *
- * Details:
- * [TVG_SCENE_FLAG_RESERVEDCNT][4][uint32_t reservedCnt]
- * other paints - see _parsePaint()
- */
 static LoaderResult _parseScene(tvgBlock block, Scene *scene)
 {
     switch (block.type)
@@ -189,14 +152,6 @@ static LoaderResult _parseScene(tvgBlock block, Scene *scene)
 }
 
 // Shape
-/*
- * Load shape path from .tvg binary file.
- * Returns LoaderResult::Success on success or LoaderResult::SizeCorruption if size corrupted.
- *
- * Details:
- * [uint32_t cmdCnt][uint32_t ptsCnt][cmdCnt * PathCommand cmds][ptsCnt * Point pts]
- * Updates flag with RenderUpdateFlag::Path bit.
- */
 static LoaderResult _parseShapePath(const char *pointer, const char *end, Shape *shape)
 {
     // ShapePath
@@ -217,20 +172,6 @@ static LoaderResult _parseShapePath(const char *pointer, const char *end, Shape 
     return LoaderResult::Success;
 }
 
-/*
- * Load fill (gradient) for shape or shape stroke from .tvg binary file.
- * Returns LoaderResult::Success on success or LoaderResult::SizeCorruption if size corrupted,
- * LoaderResult::LogicalCorruption if logically corrupted (gradient parameters before gradient type).
- *
- * Details:
- * Must start with gradient type:
- * [TVG_GRADIENT_FLAG_TYPE_RADIAL][12][float x][float y][float radius]
- * or
- * [TVG_GRADIENT_FLAG_TYPE_LINEAR][16][float x1][float y1][float x2][float y2]
- * Next are gradient parameters:
- * [TVG_FILL_FLAG_FILLSPREAD][1][FILLSPREAD_PAD/FILLSPREAD_REFLECT/FILLSPREAD_REPEAT]
- * [TVG_FILL_FLAG_COLORSTOPS][8*stopsCnt][stopsCnt * [float offset][uint8_t r][uint8_t g][uint8_t b][uint8_t a]]
- */
 static LoaderResult _parseShapeFill(const char *pointer, const char *end, Fill **fillOutside)
 {
     unique_ptr<Fill> fillGrad;
@@ -326,14 +267,6 @@ static LoaderResult _parseShapeFill(const char *pointer, const char *end, Fill *
     return LoaderResult::Success;
 }
 
-/*
- * Load stroke dash pattern from .tvg binary file.
- * Returns LoaderResult::Success on success or LoaderResult::SizeCorruption if size corrupted,
- * LoaderResult::MemoryCorruption if memory corruption.
- *
- * Details:
- * [uint32_t dashPatternCnt][dashPatternCnt * float dashPattern]
- */
 static LoaderResult _parseShapeStrokeDashPattern(const char *pointer, const char *end, Shape *shape)
 {
     uint32_t dashPatternCnt;
@@ -348,19 +281,6 @@ static LoaderResult _parseShapeStrokeDashPattern(const char *pointer, const char
     return LoaderResult::Success;
 }
 
-/*
- * Load stroke from .tvg binary file.
- * Returns LoaderResult:: Success on success and moves pointer to next position,
- * LoaderResult::SizeCorruption if corrupted or LoaderResult::InvalidType if not applicable for paint.
- *
- * Details:
- * [TVG_SHAPE_STROKE_FLAG_CAP][1][CAP_SQUARE/CAP_ROUND/CAP_BUTT]
- * [TVG_SHAPE_STROKE_FLAG_JOIN][1][JOIN_BEVEL/JOIN_ROUND/JOIN_MITER]
- * [TVG_SHAPE_STROKE_FLAG_WIDTH][4][float width]
- * [TVG_SHAPE_STROKE_FLAG_COLOR][4][color color]
- * [TVG_SHAPE_STROKE_FLAG_HAS_FILL] - see _parseShapeFill()
- * [TVG_SHAPE_STROKE_FLAG_HAS_DASHPTRN] - see _parseShapeStrokeDashPattern()
- */
 static LoaderResult _parseShapeStroke(const char *pointer, const char *end, Shape *shape)
 {
     while (pointer < end)
@@ -440,17 +360,6 @@ static LoaderResult _parseShapeStroke(const char *pointer, const char *end, Shap
     return LoaderResult::Success;
 }
 
-/*
- * Load shape from .tvg binary file.
- * Returns LoaderResult::Success on success, LoaderResult::InvalidType or other if corrupted.
- *
- * Details:
- * [TVG_SHAPE_FLAG_HAS_PATH] - see _parseShapePath()
- * [TVG_SHAPE_FLAG_HAS_STROKE] - see _parseShapeStroke()
- * [TVG_SHAPE_FLAG_HAS_FILL] - see _parseShapeFill()
- * [TVG_SHAPE_FLAG_COLOR][4][color color]
- * [TVG_SHAPE_FLAG_FILLRULE][1][FILLRULE_WINDING/FILLRULE_EVENODD]
- */
 static LoaderResult _parseShape(tvgBlock block, Shape *shape)
 {
     switch (block.type)
@@ -505,14 +414,6 @@ static LoaderResult _parseShape(tvgBlock block, Shape *shape)
 }
 
 // Picture
-/*
- * Load picture from .tvg binary file.
- * Returns LoaderResult::Success on success, LoaderResult::InvalidType or other if corrupted.
- *
- * Details:
- * [TVG_RAW_IMAGE_BEGIN_INDICATOR][8+size][uint32_t width][uint32_t height][pixels]
- * other single paint - see _parsePaint()
- */
 static LoaderResult _parsePicture(tvgBlock block, Picture *picture)
 {
     switch (block.type)
@@ -548,11 +449,7 @@ static LoaderResult _parsePicture(tvgBlock block, Picture *picture)
     return result;
 }
 
-/*
- * Read general paint section of the .tvg binary file
- * base_block should point scene / shape / picture begin indicator.
- * Returns LoaderResult::Success on success, LoaderResult::InvalidType or other if corrupted.
- */
+
 static LoaderResult _parsePaint(tvgBlock base_block, Paint **paint)
 {
     switch (base_block.type)
@@ -638,7 +535,6 @@ static LoaderResult _parsePaint(tvgBlock base_block, Paint **paint)
     return LoaderResult::Success;
 }
 
-// Load .tvg file to pointed scene
 unique_ptr<Scene> tvgParseTvgFile(const char *pointer, uint32_t size)
 {
     const char* end = pointer + size;
