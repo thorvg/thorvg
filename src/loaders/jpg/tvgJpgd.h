a4494919ac06f5ee8f41240682eaf23a656f7c9e
Hermet Park <chuneon.park@samsung.com>
2021-10-27 13:47:52 +0900

    jpg_loader: support multi-thread and header reading in prior to decoding.
    revise the code to support async loading of the static jpeg_loader,
    also support header reading in prior to decoding.

--- src/loaders/jpg/tvgJpgd.h
+++ src/loaders/jpg/tvgJpgd.h
@@ -25,12 +25,11 @@
 #ifndef _TVG_JPGD_H_
 #define _TVG_JPGD_H_
 
-// Loads a JPEG image from a memory buffer or a file.
-// req_comps can be 1 (grayscale), 3 (RGB), or 4 (RGBA).
-// On return, width/height will be set to the image's dimensions, and actual_comps will be set to the either 1 (grayscale) or 3 (RGB).
-// Notes: For more control over where and how the source data is read, see the decompress_jpeg_image_from_stream() function below, or call the jpeg_decoder class directly.
-// Requesting a 8 or 32bpp image is currently a little faster than 24bpp because the jpeg_decoder class itself currently always unpacks to either 8 or 32bpp.
-unsigned char *decompress_jpeg_image_from_memory(const unsigned char *pSrc_data, int src_data_size, int *width, int *height, int *actual_comps, int req_comps);
-unsigned char *decompress_jpeg_image_from_file(const char *pSrc_filename, int *width, int *height, int *actual_comps, int req_comps);
+class jpeg_decoder;
+
+jpeg_decoder* jpgdHeader(const char* data, int size, int* width, int* height);
+jpeg_decoder* jpgdHeader(const char* filename, int* width, int* height);
+unsigned char* jpgdDecompress(jpeg_decoder* decoder);
+void jpgdDelete(jpeg_decoder* decoder);
 
 #endif //_TVG_JPGD_H_
