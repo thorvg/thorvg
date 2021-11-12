48e8685cc67cec5979e8c8133c08b7e693e01110
JunsuChoi <jsuya.choi@samsung.com>
2021-11-12 16:35:07 +0900

    infra bot: Hotfix memcheck bot for push event

--- .github/workflows/memcheck_valgrind.sh
+++ .github/workflows/memcheck_valgrind.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-echo "Run Valgrind"
-echo "valgrind --leak-check=yes ./tvgUnitTests"
-cd ./build/test
-
-valgrind --leak-check=yes ./tvgUnitTests > memcheck_valgrind.txt 2>&1
-
-
-PAYLOAD_MEMCHECK=`cat memcheck_valgrind.txt`
-COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
-
-echo $COMMENTS_URL
-echo "MEMCHECK errors:"
-echo $PAYLOAD_MEMCHECK
-
-if [[ $PAYLOAD_MEMCHECK == *"definitely lost:"* || $PAYLOAD_MEMCHECK == *"Invalid read "* || $PAYLOAD_MEMCHECK == *"Invalid write "* ]]; then
-  OUTPUT+=$'\n**MEMCHECK(VALGRIND) RESULT**:\n'
-  OUTPUT+=$'\n`valgrind --leak-check=yes ./tvgUnitTests`\n'
-  OUTPUT+=$'\n```\n'
-  OUTPUT+="$PAYLOAD_MEMCHECK"
-  OUTPUT+=$'\n```\n' 
+if [[ -z "$GITHUB_TOKEN" ]]; then
+    echo "The GITHUB_TOKEN is required."
+    exit 1
 fi
 
-PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+    echo "Run Valgrind"
+    echo "valgrind --leak-check=yes ./tvgUnitTests"
+    cd ./build/test
 
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+    valgrind --leak-check=yes ./tvgUnitTests > memcheck_valgrind.txt 2>&1
+
+
+    PAYLOAD_MEMCHECK=`cat memcheck_valgrind.txt`
+    COMMENTS_URL=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.comments_url)
+
+    echo $COMMENTS_URL
+    echo "MEMCHECK errors:"
+    echo $PAYLOAD_MEMCHECK
+
+    if [[ $PAYLOAD_MEMCHECK == *"definitely lost:"* || $PAYLOAD_MEMCHECK == *"Invalid read "* || $PAYLOAD_MEMCHECK == *"Invalid write "* ]]; then
+        OUTPUT+=$'\n**MEMCHECK(VALGRIND) RESULT**:\n'
+        OUTPUT+=$'\n`valgrind --leak-check=yes ./tvgUnitTests`\n'
+        OUTPUT+=$'\n```\n'
+        OUTPUT+="$PAYLOAD_MEMCHECK"
+        OUTPUT+=$'\n```\n' 
+    fi
+
+    PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+
+    curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/vnd.github.VERSION.text+json" --data "$PAYLOAD" "$COMMENTS_URL"
+fi
+
