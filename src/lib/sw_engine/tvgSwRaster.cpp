thorvg/thorvg

commit 90fa26b7bb75cb938290170882e65da8d9fc5d9e

sw_engine SwRaster: Initialize uninitialized transform value

src/lib/sw_engine/tvgSwRaster.cpp
@@ -499,7 +499,9 @@ static bool _rasterScaledRleRGBAImage(SwSurface* surface, const SwImage* image, 
 static bool _scaledRleRGBAImage(SwSurface* surface, const SwImage* image, const Matrix* transform, const SwBBox& region, uint32_t opacity)
 {
     Matrix itransform;
+
     if (transform && !mathInverse(transform, &itransform)) return false;
+    else mathIdentity(&itransform);
 
     auto halfScale = _halfScale(image->scale);
 
@@ -844,7 +846,9 @@ static bool _rasterScaledRGBAImage(SwSurface* surface, const SwImage* image, con
 static bool _scaledRGBAImage(SwSurface* surface, const SwImage* image, const Matrix* transform, const SwBBox& region, uint32_t opacity)
 {
     Matrix itransform;
+
     if (transform && !mathInverse(transform, &itransform)) return false;
+    else mathIdentity(&itransform);
 
     auto halfScale = _halfScale(image->scale);
 

commit 031493ea01c703d3fae71ff0539eef44bc4c8026

sw_engine raster: fix a regression bug.
Picture example were broken by 90fa26b7bb75cb938290170882e65da8d9fc5d9e

the correct condition must be like this change.

src/lib/sw_engine/tvgSwRaster.cpp
@@ -500,8 +500,9 @@ static bool _scaledRleRGBAImage(SwSurface* surface, const SwImage* image, const 
 {
     Matrix itransform;
 
-    if (transform && !mathInverse(transform, &itransform)) return false;
-    else mathIdentity(&itransform);
+    if (transform) {
+        if (!mathInverse(transform, &itransform)) return false;
+    } else mathIdentity(&itransform);
 
     auto halfScale = _halfScale(image->scale);
 
@@ -847,8 +848,9 @@ static bool _scaledRGBAImage(SwSurface* surface, const SwImage* image, const Mat
 {
     Matrix itransform;
 
-    if (transform && !mathInverse(transform, &itransform)) return false;
-    else mathIdentity(&itransform);
+    if (transform) {
+        if (!mathInverse(transform, &itransform)) return false;
+    } else mathIdentity(&itransform);
 
     auto halfScale = _halfScale(image->scale);
 