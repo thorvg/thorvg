thorvg/thorvg

commit afea40a947c7c902b5242e82c13ec5625bedfe59

common taskscheduler: increase thread efficiency.
Revise the logic to avoid potential blocking of the main thread.
Move the clippers job completion to worker-threads to prevent main thread blocks
and enhance maximum parallelization efficiency.

src/lib/sw_engine/tvgSwRenderer.cpp
@@ -150,6 +150,9 @@ struct SwShapeTask : SwTask
 
         //Clip Path
         for (auto clip = clips.data; clip < (clips.data + clips.count); ++clip) {
+            //Guarantee composition targets get ready.
+            static_cast<SwShapeTask*>(*clip)->done(tid);
+
             auto clipper = &static_cast<SwShapeTask*>(*clip)->shape;
             //Clip shape rle
             if (shape.rle) {
@@ -202,6 +205,9 @@ struct SwImageTask : SwTask
                 if (!imageGenRle(&image, bbox, false)) goto end;
                 if (image.rle) {
                     for (auto clip = clips.data; clip < (clips.data + clips.count); ++clip) {
+                        //Guarantee composition targets get ready.
+                        static_cast<SwShapeTask*>(*clip)->done(tid);
+
                         auto clipper = &static_cast<SwShapeTask*>(*clip)->shape;
                         if (clipper->fastTrack) rleClipRect(image.rle, &clipper->bbox);
                         else if (clipper->rle) rleClipPath(image.rle, clipper->rle);
@@ -590,13 +596,7 @@ void* SwRenderer::prepareCommon(SwTask* task, const RenderTransform* transform, 
     //Finish previous task if it has duplicated request.
     task->done();
 
-    if (clips.count > 0) {
-        //Guarantee composition targets get ready.
-        for (auto clip = clips.data; clip < (clips.data + clips.count); ++clip) {
-            static_cast<SwShapeTask*>(*clip)->done();
-        }
-        task->clips = clips;
-    }
+    if (clips.count > 0) task->clips = clips;
 
     if (transform) {
         if (!task->transform) task->transform = static_cast<Matrix*>(malloc(sizeof(Matrix)));

src/lib/tvgTaskScheduler.cpp
@@ -100,13 +100,12 @@ struct TaskQueue {
 };
 
 
-class TaskSchedulerImpl
+struct TaskSchedulerImpl
 {
-public:
-    unsigned                       threadCnt;
+    uint32_t                       threadCnt;
     vector<thread>                 threads;
     vector<TaskQueue>              taskQueues;
-    atomic<unsigned>               idx{0};
+    uint32_t                       idx = 0;
 
     TaskSchedulerImpl(unsigned threadCnt) : threadCnt(threadCnt), taskQueues(threadCnt)
     {

src/lib/tvgTaskScheduler.h
@@ -44,20 +44,25 @@ struct Task
 {
 private:
     mutex                   mtx;
-    condition_variable      cv;
-    bool                    ready{true};
-    bool                    pending{false};
+    bool                    finished = true;
+    bool                    running = false;
 
 public:
     virtual ~Task() = default;
 
-    void done()
+    void done(unsigned tid = 0)
     {
-        if (!pending) return;
+        if (finished) return;
 
         unique_lock<mutex> lock(mtx);
-        while (!ready) cv.wait(lock);
-        pending = false;
+
+        if (finished) return;
+
+        //the job hasn't been launched yet.
+        running = true;
+        run(tid);
+        running = false;
+        finished = true;
     }
 
 protected:
@@ -66,17 +71,21 @@ protected:
 private:
     void operator()(unsigned tid)
     {
-        run(tid);
+        if (finished || running) return;
 
         lock_guard<mutex> lock(mtx);
-        ready = true;
-        cv.notify_one();
+
+        if (finished || running) return;
+
+        running = true;
+        run(tid);
+        running = false;
+        finished = true;
     }
 
     void prepare()
     {
-        ready = false;
-        pending = true;
+        finished = false;
     }
 
     friend class TaskSchedulerImpl;

commit bc7b448c01f35a027bf2ba0b207ab3f55ec3d945

sw_engine: ++thread-safety
One more regression bug by afea40a947c7c902b5242e82c13ec5625bedfe59

As tasks will now be triggered by other tasks on the worker threads,
the designated memory pool data (outline) for each thread should be
reset before moving on to other tasks.

@Issue: https://github.com/thorvg/thorvg/issues/1409

src/lib/sw_engine/tvgSwRenderer.cpp
@@ -167,6 +167,9 @@ struct SwShapeTask : SwTask
             }
         }
 
+        //Clear current task memorypool here if the clippers would use the same memory pool
+        shapeDelOutline(&shape, mpool, tid);
+
         //Clip Path
         for (auto clip = clips.data; clip < (clips.data + clips.count); ++clip) {
             auto clipper = static_cast<SwTask*>(*clip);
@@ -177,11 +180,10 @@ struct SwShapeTask : SwTask
             //Clip stroke rle
             if (shape.strokeRle && !clipper->clip(shape.strokeRle)) goto err;
         }
-        goto end;
+        return;
 
     err:
         shapeReset(&shape);
-    end:
         shapeDelOutline(&shape, mpool, tid);
     }
 
@@ -299,17 +301,19 @@ struct SwImageTask : SwTask
             if (mesh->triangleCnt == 0 && clips.count > 0) {
                 if (!imageGenRle(&image, bbox, false)) goto end;
                 if (image.rle) {
+                    //Clear current task memorypool here if the clippers would use the same memory pool
+                    imageDelOutline(&image, mpool, tid);
                     for (auto clip = clips.data; clip < (clips.data + clips.count); ++clip) {
                         auto clipper = static_cast<SwTask*>(*clip);
                         //Guarantee composition targets get ready.
                         clipper->done(tid);
                         if (!clipper->clip(image.rle)) goto err;
                     }
+                    return;
                 }
             }
         }
         goto end;
-
     err:
         rleReset(image.rle);
     end: