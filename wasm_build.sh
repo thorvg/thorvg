b8d625b47dae9ca1023a9e95d845ab181e4773b3
Hermet Park <hermet@lottiefiles.com>
2023-08-01 13:07:35 +0900

    infra wasm: update the build script.
    revise the compile options to optmize the binary size.
    also remove all compile warnings.
    
    size: 552616 -> 446847

--- wasm_build.sh
+++ wasm_build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Emscripten SDK PATH is not provided"
-    echo "Usage: wasm_build EMSDK_PATH"
-    exit 1;
-fi
-
-if [ ! -d "./builddir_wasm" ]; then
+if [ ! -d "./build_wasm" ]; then
     sed "s|EMSDK:|$1|g" wasm_cross.txt > /tmp/.wasm_cross.txt
-    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all, lottie_beta" -Dsavers="all" -Dlog=true --cross-file /tmp/.wasm_cross.txt builddir_wasm
-    cp ./test/wasm/wasm_test.html builddir_wasm/src/index.html
+    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all, lottie_beta" -Dsavers="all" -Dlog=true --cross-file /tmp/.wasm_cross.txt build_wasm
+    cp ./test/wasm/wasm_test.html build_wasm/src/index.html
 fi
 
-ninja -C builddir_wasm/
+ninja -C build_wasm/
 echo "RESULT:"
-echo " thorvg-wasm.wasm and thorvg-wasm.js can be found in builddir_wasm/src folder"
-ls -lrt builddir_wasm/src/thorvg-wasm.*
+echo " thorvg-wasm.wasm and thorvg-wasm.js can be found in build_wasm/src folder"
+ls -lrt build_wasm/src/thorvg-wasm.*
