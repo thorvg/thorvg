0c47d2d250672795fbafe29e3812e641e4a226ee
Hermet Park <hermet@lottiefiles.com>
2023-08-03 01:14:26 +0900

    infra wasm: remove useless wasm test.
    the test hasn't been maintained properly,
    and we won't be maintaining it anymore.
    
    please use thorvg.viewer intead. it's much more accurate.

--- wasm_build.sh
+++ wasm_build.sh
@@ -5,7 +5,6 @@
 if [ ! -d "./build_wasm" ]; then
     sed "s|EMSDK:|$1|g" wasm_cross.txt > /tmp/.wasm_cross.txt
     meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all, lottie_beta" -Dsavers="all" -Dbindings="wasm" -Dlog=true --cross-file /tmp/.wasm_cross.txt build_wasm
-    cp ./test/wasm/wasm_test.html build_wasm/src/index.html
 fi
 
 ninja -C build_wasm/
