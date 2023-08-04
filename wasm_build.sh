0eed9bc16ead2358c3eb562965ab95f205a3d3a5
Hermet Park <hermet@lottiefiles.com>
2023-08-04 13:27:55 +0900

    infra wasm: migrate wasm into bindings.

--- wasm_build.sh
+++ wasm_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+#https://github.com/thorvg/thorvg/wiki/ThorVG-Viewer-Development-Guide
+
 if [ ! -d "./build_wasm" ]; then
     sed "s|EMSDK:|$1|g" wasm_cross.txt > /tmp/.wasm_cross.txt
-    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all, lottie_beta" -Dsavers="all" -Dlog=true --cross-file /tmp/.wasm_cross.txt build_wasm
+    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all, lottie_beta" -Dsavers="all" -Dbindings="wasm" -Dlog=true --cross-file /tmp/.wasm_cross.txt build_wasm
     cp ./test/wasm/wasm_test.html build_wasm/src/index.html
 fi
 
 ninja -C build_wasm/
-echo "RESULT:"
-echo " thorvg-wasm.wasm and thorvg-wasm.js can be found in build_wasm/src folder"
-ls -lrt build_wasm/src/thorvg-wasm.*
+ls -lrt build_wasm/src/bindings/wasm/thorvg-wasm.*
