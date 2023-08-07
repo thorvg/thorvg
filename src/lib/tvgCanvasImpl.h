f18cc7f346f03a8e4add05dc7b129a3b5fb40996
Hermet Park <hermet@lottiefiles.com>
2023-08-07 13:47:33 +0900

    common: deleted paints only when they are no longer valid.
    Pictures can be shared with Animation instances.
    The condition can now be distinguished by checking the `dispose()` return value.

--- src/lib/tvgCanvasImpl.h
+++ src/lib/tvgCanvasImpl.h
@@ -65,8 +65,9 @@ struct Canvas::Impl
 
         //Free paints
         for (auto paint : paints) {
-            paint->pImpl->dispose(*renderer);
-            if (free) delete(paint);
+            if (paint->pImpl->dispose(*renderer)) {
+            	if (free) delete(paint);
+	    }
         }
 
         paints.clear();
