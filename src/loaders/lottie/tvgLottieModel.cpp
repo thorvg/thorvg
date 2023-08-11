df43f916447f09288251caa61ce9b20a5c53499b
Hermet Park <hermet@lottiefiles.com>
2023-08-12 00:31:54 +0900

    loader/lottie: optimize the header task.
    To achieve full threading efficiency,
    the header should return the necessary properties:
    view size, framerate, and duration.
    
    This commit adds duration parsing.
    
    Also don't do the header task in case of single thread
    because there wouldn't be parallel effects.

--- src/loaders/lottie/tvgLottieModel.cpp
+++ src/loaders/lottie/tvgLottieModel.cpp
@@ -20,10 +20,6 @@
  * SOFTWARE.
  */
 
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 #include "tvgLottieModel.h"
 
 
