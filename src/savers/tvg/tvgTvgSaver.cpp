c92936f3e1518727dcc42925bcb02808f60f002f
Hermet Park <hermet@lottiefiles.com>
2023-08-11 23:56:30 +0900

    loader lottie: tiny data copy optimization.
    This patch tries to skip the matrix data copy as possible.

--- src/savers/tvg/tvgTvgSaver.cpp
+++ src/savers/tvg/tvgTvgSaver.cpp
@@ -51,8 +51,6 @@ static FILE* _fopen(const char* filename, const char* mode)
 
 #define SIZE(A) sizeof(A)
 
-#define P(A) A->pImpl
-
 /************************************************************************/
 /* Internal Class Implementation                                        */
 /************************************************************************/
