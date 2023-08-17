0142c2cb31c3b9dcaeb6ab3c4b622887cabe67cd
Hermet Park <hermet@lottiefiles.com>
2023-08-17 18:04:42 +0900

    infra wasm: update the build script.
    Disable the log in default for outside users.
    we gonna turn it on manually for the lottie viewer

--- wasm_build.sh
+++ wasm_build.sh
@@ -4,7 +4,7 @@
 
 if [ ! -d "./build_wasm" ]; then
     sed "s|EMSDK:|$1|g" wasm_cross.txt > /tmp/.wasm_cross.txt
-    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all, lottie" -Dsavers="all" -Dbindings="wasm_beta" -Dlog=true --cross-file /tmp/.wasm_cross.txt build_wasm
+    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dbindings="wasm_beta" --cross-file /tmp/.wasm_cross.txt build_wasm
 fi
 
 ninja -C build_wasm/
