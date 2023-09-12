7875e5435044ccba89624610978e5c2fa1dae79f
Hermet Park <hermet@lottiefiles.com>
2023-09-12 21:30:58 +0900

    sw_engine: fix a wrong compuation.
    corrected a mistake caused by a dead brain.

--- src/lib/sw_engine/tvgSwRenderer.cpp
+++ src/lib/sw_engine/tvgSwRenderer.cpp
@@ -96,7 +96,8 @@ struct SwShapeTask : SwTask
 
         if (!rshape->stroke->fill && (MULTIPLY(rshape->stroke->color[3], opacity) == 0)) return 0.0f;
         if (mathZero(rshape->stroke->trim.begin - rshape->stroke->trim.end)) return 0.0f;
-        if (transform) return (width / sqrt(transform->e11 * transform->e11 + transform->e12 * transform->e12));
+
+        if (transform) return (width * sqrt(transform->e11 * transform->e11 + transform->e12 * transform->e12));
         else return width;
     }
 
