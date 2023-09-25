c7123a1547f5e32e69a26911a52be7bc74e86f8f
Hermet Park <hermet@lottiefiles.com>
2023-09-25 16:04:06 +0900

    test: enhanced the unit-test coverage
    supplements animation/lottie/sw_engine test cases.
    
    updated the page: https://github.com/thorvg/thorvg/wiki/Unit-Tests
    
    Issue: https://github.com/thorvg/thorvg/issues/1669

---
+++ test/testAnimation.cpp
@@ -0,0 +1,193 @@
+/*
+ * Copyright (c) 2023 the ThorVG project. All rights reserved.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <thorvg.h>
+#include <fstream>
+#include <cstring>
+#include "config.h"
+#include "catch.hpp"
+
+using namespace tvg;
+using namespace std;
+
+
+TEST_CASE("Animation Basic", "[tvgAnimation]")
+{
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    //Negative cases
+    REQUIRE(animation->frame(0) == Result::InsufficientCondition);
+    REQUIRE(animation->curFrame() == 0);
+    REQUIRE(animation->totalFrame() == 0);
+    REQUIRE(animation->duration() == 0.0f);
+}
+
+#ifdef THORVG_LOTTIE_LOADER_SUPPORT
+
+TEST_CASE("Animation Lottie", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 1) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/invalid.json") == Result::InvalidArguments);
+    REQUIRE(picture->load(TEST_DIR"/test.json") == Result::Success);
+
+    REQUIRE(animation->totalFrame() == 120);
+    REQUIRE(animation->curFrame() == 0);
+    REQUIRE(animation->duration() == Approx(4).margin(004004));
+    REQUIRE(animation->frame(20) == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie2", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test2.json") == Result::Success);
+
+    REQUIRE(animation->frame(20) == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie3", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test3.json") == Result::Success);
+    
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie4", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test4.json") == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie5", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test5.json") == Result::Success);
+    
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie6", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test6.json") == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie7", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test7.json") == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie8", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test8.json") == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+TEST_CASE("Animation Lottie9", "[tvgAnimation]")
+{
+    REQUIRE(Initializer::init(CanvasEngine::Sw, 0) == Result::Success);
+
+    auto animation = Animation::gen();
+    REQUIRE(animation);
+
+    auto picture = animation->picture();
+    REQUIRE(picture->identifier == Picture::identifier);
+
+    REQUIRE(picture->load(TEST_DIR"/test9.json") == Result::Success);
+
+    REQUIRE(Initializer::term(CanvasEngine::Sw) == Result::Success);
+}
+
+#endif
