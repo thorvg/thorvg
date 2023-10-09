01dd93be117654c36840003b76e274538123eb30
Hermet Park <hermet@lottiefiles.com>
2023-10-09 12:29:02 +0900

    infra: migrate cross files into one folder.

--- wasm_build.sh
+++ wasm_build.sh
@@ -3,7 +3,7 @@
 #https://github.com/thorvg/thorvg/wiki/ThorVG-Viewer-Development-Guide
 
 if [ ! -d "./build_wasm" ]; then
-    sed "s|EMSDK:|$1|g" wasm_cross.txt > /tmp/.wasm_cross.txt
+    sed "s|EMSDK:|$1|g" ./cross/wasm_x86_i686.txt > /tmp/.wasm_cross.txt
     meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dbindings="wasm_beta" --cross-file /tmp/.wasm_cross.txt build_wasm
 fi
 
