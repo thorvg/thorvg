4a9b008b83d69388e70a2a7bffa31da1b2c15a1a
Hermet Park <hermet@lottiefiles.com>
2023-10-13 11:18:32 +0900

    examples: allow gl test

--- src/examples/Animation.cpp
+++ src/examples/Animation.cpp
@@ -20,13 +20,14 @@
  * SOFTWARE.
  */
 
-#define NO_GL_EXAMPLE
-
 #include "Common.h"
 
 /************************************************************************/
 /* Drawing Commands                                                     */
 /************************************************************************/
+
+static unique_ptr<tvg::Animation> animation;
+static Elm_Transit *transit;
 
 void tvgUpdateCmds(tvg::Canvas* canvas, tvg::Animation* animation, float progress)
 {
@@ -41,21 +42,8 @@ void tvgUpdateCmds(tvg::Canvas* canvas, tvg::Animation* animation, float progres
     }
 }
 
-
-/************************************************************************/
-/* Sw Engine Test Code                                                  */
-/************************************************************************/
-
-static unique_ptr<tvg::SwCanvas> swCanvas;
-static unique_ptr<tvg::Animation> animation;
-static Elm_Transit *transit;
-
-void tvgSwTest(uint32_t* buffer)
+void tvgDrawCmds(tvg::Canvas* canvas)
 {
-    //Create a Canvas
-    swCanvas = tvg::SwCanvas::gen();
-    swCanvas->target(buffer, WIDTH, WIDTH, HEIGHT, tvg::SwCanvas::ARGB8888);
-
     //Animation Controller
     animation = tvg::Animation::gen();
     auto picture = animation->picture();
@@ -65,7 +53,7 @@ void tvgSwTest(uint32_t* buffer)
     shape->appendRect(0, 0, WIDTH, HEIGHT);
     shape->fill(50, 50, 50);
 
-    if (swCanvas->push(std::move(shape)) != tvg::Result::Success) return;
+    if (canvas->push(std::move(shape)) != tvg::Result::Success) return;
 
     if (picture->load(EXAMPLE_DIR"/sample.json") != tvg::Result::Success) {
         cout << "Lottie is not supported. Did you enable Lottie Loader?" << endl;
@@ -89,12 +77,28 @@ void tvgSwTest(uint32_t* buffer)
     picture->scale(scale);
     picture->translate(shiftX, shiftY);
 
-    swCanvas->push(tvg::cast<tvg::Picture>(picture));
+    canvas->push(tvg::cast<tvg::Picture>(picture));
 
     //Run animation loop
     elm_transit_duration_set(transit, animation->duration());
     elm_transit_repeat_times_set(transit, -1);
     elm_transit_go(transit);
+}
+
+
+/************************************************************************/
+/* Sw Engine Test Code                                                  */
+/************************************************************************/
+
+static unique_ptr<tvg::SwCanvas> swCanvas;
+
+void tvgSwTest(uint32_t* buffer)
+{
+    //Create a Canvas
+    swCanvas = tvg::SwCanvas::gen();
+    swCanvas->target(buffer, WIDTH, WIDTH, HEIGHT, tvg::SwCanvas::ARGB8888);
+
+    tvgDrawCmds(swCanvas.get());
 }
 
 void drawSwView(void* data, Eo* obj)
@@ -116,29 +120,85 @@ void transitSwCb(Elm_Transit_Effect *effect, Elm_Transit* transit, double progre
 
 
 /************************************************************************/
+/* GL Engine Test Code                                                  */
+/************************************************************************/
+
+static unique_ptr<tvg::GlCanvas> glCanvas;
+
+void initGLview(Evas_Object *obj)
+{
+    static constexpr auto BPP = 4;
+
+    //Create a Canvas
+    glCanvas = tvg::GlCanvas::gen();
+    glCanvas->target(nullptr, WIDTH * BPP, WIDTH, HEIGHT);
+
+    tvgDrawCmds(glCanvas.get());
+}
+
+void drawGLview(Evas_Object *obj)
+{
+    auto gl = elm_glview_gl_api_get(obj);
+    gl->glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    gl->glClear(GL_COLOR_BUFFER_BIT);
+
+    if (glCanvas->draw() == tvg::Result::Success) {
+        glCanvas->sync();
+    }
+}
+
+void transitGlCb(Elm_Transit_Effect *effect, Elm_Transit* transit, double progress)
+{
+    tvgUpdateCmds(glCanvas.get(), animation.get(), progress);
+    elm_glview_changed_set((Evas_Object*)effect);
+}
+
+
+/************************************************************************/
 /* Main Code                                                            */
 /************************************************************************/
 
 int main(int argc, char **argv)
 {
+    tvg::CanvasEngine tvgEngine = tvg::CanvasEngine::Sw;
+
+    if (argc > 1) {
+        if (!strcmp(argv[1], "gl")) tvgEngine = tvg::CanvasEngine::Gl;
+    }
+
+    //Initialize ThorVG Engine
+    if (tvgEngine == tvg::CanvasEngine::Sw) {
+        cout << "tvg engine: software" << endl;
+    } else {
+        cout << "tvg engine: opengl" << endl;
+    }
+
     //Threads Count
     auto threads = std::thread::hardware_concurrency();
     if (threads > 0) --threads;    //Allow the designated main thread capacity
 
     //Initialize ThorVG Engine
-    if (tvg::Initializer::init(tvg::CanvasEngine::Sw, threads) == tvg::Result::Success) {
+    if (tvg::Initializer::init(tvgEngine, threads) == tvg::Result::Success) {
 
         elm_init(argc, argv);
 
         transit = elm_transit_add();
-        auto view = createSwView();
-        elm_transit_effect_add(transit, transitSwCb, view, nullptr);
+
+        if (tvgEngine == tvg::CanvasEngine::Sw) {
+            auto view = createSwView(1024, 1024);
+            elm_transit_effect_add(transit, transitSwCb, view, nullptr);
+        } else {
+            auto view = createGlView(1024, 1024);
+            elm_transit_effect_add(transit, transitGlCb, view, nullptr);
+        }
 
         elm_run();
         elm_shutdown();
 
         //Terminate ThorVG Engine
-        tvg::Initializer::term(tvg::CanvasEngine::Sw);
+        tvg::Initializer::term(tvgEngine);
+    } else {
+        cout << "engine is not supported" << endl;
     }
 
     return 0;
