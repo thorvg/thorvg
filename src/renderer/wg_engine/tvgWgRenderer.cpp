b500dd1dcec4b3c619b3fa1e6d120aa611689c4b
Sergii Liebodkin <s.lebedkin@gmail.com>
2023-10-17 10:10:32 +0300

    wg_engine: Added ability to draw multiple solid color filled shapes
    [issues 1479: Shape](https://github.com/thorvg/thorvg/issues/1479)
    
    In order to build you need third party libraries. Before you start please read this: [LearnWebGPU](https://eliemichel.github.io/LearnWebGPU/getting-started/hello-webgpu.html)
    
    Usage example:
    
        // init glfw
        glfwInit();
    
        // create a windowed mode window and its opengl context
        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
        GLFWwindow* window = glfwCreateWindow(800, 800, "WebGPU base app", nullptr, nullptr);
    
        // get window size
        int width{}, height{};
        glfwGetWindowSize(window, &width, &height);
    
        // init engine webgpu
        tvg::Initializer::init(tvg::CanvasEngine::Wg, 0);
    
        // create wg canvas
        auto canvasWg = tvg::WgCanvas::gen();
        canvas_wg->target(glfwGetWin32Window(window), width, height);
    
        // prepare a shape (Rectangle + Rectangle + Circle + Circle)
        auto shape1 = tvg::Shape::gen();
        shape1->appendRect(0, 0, 200, 200);                //x, y, w, h
        shape1->appendRect(100, 100, 300, 300, 100, 100);  //x, y, w, h, rx, ry
        shape1->appendCircle(400, 400, 100, 100);          //cx, cy, radiusW, radiusH
        shape1->appendCircle(400, 500, 170, 100);          //cx, cy, radiusW, radiusH
        shape1->fill(255, 255, 0);                         //r, g, b
    
        canvas_wg->push(std::move(shape1));
    
        while (!glfwWindowShouldClose(window)) {
            // webgpu
            canvas_wg->draw();
            canvas_wg->sync();
    
            // pull events
            glfwPollEvents();
        }
    
        // terminate engine and window
        tvg::Initializer::term(tvg::CanvasEngine::Wg);
        glfwDestroyWindow(window);
        glfwTerminate();

--- src/renderer/wg_engine/tvgWgRenderer.cpp
+++ src/renderer/wg_engine/tvgWgRenderer.cpp
@@ -22,6 +22,14 @@
 
 #include "tvgWgRenderer.h"
 
+#include <iostream>
+#ifdef _WIN32
+// TODO: cross-platform realization
+#include <windows.h>
+#endif
+#include "tvgWgRenderData.h"
+#include "tvgWgShaderSrc.h"
+
 WgRenderer::WgRenderer() {
     initialize();
 }
@@ -31,15 +39,117 @@ WgRenderer::~WgRenderer() {
 }
 
 void WgRenderer::initialize() {
-    TVGERR("WG_ENGINE", "TODO: WgRenderer::initialize()");
+    // create instance
+    WGPUInstanceDescriptor instanceDesc{};
+    instanceDesc.nextInChain = nullptr;
+    mInstance = wgpuCreateInstance(&instanceDesc);
+    assert(mInstance);
+
+    // request adapter options
+    WGPURequestAdapterOptions requestAdapterOptions{};
+    requestAdapterOptions.nextInChain = nullptr;
+    requestAdapterOptions.compatibleSurface = nullptr;
+    requestAdapterOptions.powerPreference = WGPUPowerPreference_HighPerformance;
+    requestAdapterOptions.forceFallbackAdapter = false;
+    // on adapter request ended function
+    auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const * message, void * pUserData) {
+        if (status != WGPURequestAdapterStatus_Success)
+            TVGERR("WG_RENDERER", "Adapter request: %s", message);
+        *((WGPUAdapter*)pUserData) = adapter;
+    };
+    // request adapter
+    wgpuInstanceRequestAdapter(mInstance, &requestAdapterOptions, onAdapterRequestEnded, &mAdapter);
+    assert(mAdapter);
+
+    // adapter enumarate fueatures
+    WGPUFeatureName featureNames[32]{};
+    size_t featuresCount = wgpuAdapterEnumerateFeatures(mAdapter, featureNames);
+    WGPUAdapterProperties adapterProperties{};
+    wgpuAdapterGetProperties(mAdapter, &adapterProperties);
+    WGPUSupportedLimits supportedLimits{};
+    wgpuAdapterGetLimits(mAdapter, &supportedLimits);
+
+    // reguest device
+    WGPUDeviceDescriptor deviceDesc{};
+    deviceDesc.nextInChain = nullptr;
+    deviceDesc.label = "The device";
+    deviceDesc.requiredFeaturesCount = featuresCount;
+    deviceDesc.requiredFeatures = featureNames;
+    deviceDesc.requiredLimits = nullptr;
+    deviceDesc.defaultQueue.nextInChain = nullptr;
+    deviceDesc.defaultQueue.label = "The default queue";
+    deviceDesc.deviceLostCallback = nullptr;
+    deviceDesc.deviceLostUserdata = nullptr;
+    // on device request ended function
+    auto onDeviceRequestEnded = [](WGPURequestDeviceStatus status, WGPUDevice device, char const * message, void * pUserData) {
+        if (status != WGPURequestDeviceStatus_Success)
+            TVGERR("WG_RENDERER", "Device request: %s", message);
+        *((WGPUDevice*)pUserData) = device;
+    };
+    // request device
+    wgpuAdapterRequestDevice(mAdapter, &deviceDesc, onDeviceRequestEnded, &mDevice);
+    assert(mDevice);
+
+    // on device error function
+    auto onDeviceError = [](WGPUErrorType type, char const* message, void* pUserData) {
+        TVGERR("WG_RENDERER", "Uncaptured device error: %s", message);
+    };
+    // set device error handling
+    wgpuDeviceSetUncapturedErrorCallback(mDevice, onDeviceError, nullptr);
+
+    mQueue = wgpuDeviceGetQueue(mDevice);
+    assert(mQueue);
+    
+    // create pipelines
+    mPipelineEmpty.initialize(mDevice);
+    mPipelineSolid.initialize(mDevice);
+    mPipelineBindGroupEmpty.initialize(mDevice, mPipelineEmpty);
+    mGeometryDataPipeline.initialize(mDevice);
 }
 
 void WgRenderer::release() {
-    TVGERR("WG_ENGINE", "TODO: WgRenderer::release()");
+    if (mStencilTex) {
+        wgpuTextureDestroy(mStencilTex);
+        wgpuTextureRelease(mStencilTex);
+    }
+    if (mStencilTexView) wgpuTextureViewRelease(mStencilTexView);
+    if (mSwapChain) wgpuSwapChainRelease(mSwapChain);
+    if (mSurface) wgpuSurfaceRelease(mSurface);
+    mGeometryDataPipeline.release();
+    mPipelineBindGroupEmpty.release();
+    mPipelineSolid.release();
+    mPipelineEmpty.release();
+    if (mDevice) {
+        wgpuDeviceDestroy(mDevice);
+        wgpuDeviceRelease(mDevice);
+    }
+    if (mAdapter) wgpuAdapterRelease(mAdapter);
+    if (mInstance) wgpuInstanceRelease(mInstance);
 }
 
 RenderData WgRenderer::prepare(const RenderShape& rshape, RenderData data, const RenderTransform* transform, Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags, bool clipper) {
-    return nullptr;
+    // get or create render data shape
+    auto renderDataShape = (WgRenderDataShape*)data;
+    if (!renderDataShape) {
+        renderDataShape = new WgRenderDataShape();
+        renderDataShape->initialize(mDevice);
+        renderDataShape->mPipelineBindGroupSolid.initialize(mDevice, mPipelineSolid);
+    }
+    
+    if (flags & RenderUpdateFlag::Path)
+        renderDataShape->tesselate(mDevice, mQueue, rshape);
+
+    // setup solid fill properties
+    if ((flags & (RenderUpdateFlag::Color | RenderUpdateFlag::Transform)) && (!rshape.fill)) {
+        WgPipelineDataSolid pipelineDataSolid{};
+        pipelineDataSolid.updateMatrix(mViewMatrix, transform);
+        pipelineDataSolid.updateColor(rshape);
+        renderDataShape->mPipelineBindGroupSolid.update(mQueue, pipelineDataSolid);
+        renderDataShape->mPipelineBindGroup = &renderDataShape->mPipelineBindGroupSolid;
+        renderDataShape->mPipelineBase = &mPipelineSolid;
+    }
+
+    return renderDataShape;
 }
 
 RenderData WgRenderer::prepare(const Array<RenderData>& scene, RenderData data, const RenderTransform* transform, Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags) {
@@ -55,6 +165,7 @@ bool WgRenderer::preRender() {
 }
 
 bool WgRenderer::renderShape(RenderData data) {
+    mRenderDatas.push(data);
     return true;
 }
 
@@ -67,6 +178,8 @@ bool WgRenderer::postRender() {
 }
 
 bool WgRenderer::dispose(RenderData data) {
+    auto renderData = (WgRenderData*)data;
+    if (renderData) renderData->release();
     return true;
 }
 
@@ -95,10 +208,187 @@ bool WgRenderer::clear() {
 }
 
 bool WgRenderer::sync() {
+    WGPUTextureView backBufferView = wgpuSwapChainGetCurrentTextureView(mSwapChain);
+    
+    // command buffer descriptor
+    WGPUCommandBufferDescriptor commandBufferDesc{};
+    commandBufferDesc.nextInChain = nullptr;
+    commandBufferDesc.label = "The command buffer";
+    WGPUCommandBuffer commandsBuffer = nullptr; {
+        // command encoder descriptor
+        WGPUCommandEncoderDescriptor commandEncoderDesc{};
+        commandEncoderDesc.nextInChain = nullptr;
+        commandEncoderDesc.label = "The command encoder";
+        // begin render pass
+        WGPUCommandEncoder commandEncoder = wgpuDeviceCreateCommandEncoder(mDevice, &commandEncoderDesc); {
+            // render pass depth stencil attachment
+            WGPURenderPassDepthStencilAttachment depthStencilAttachment{};
+            depthStencilAttachment.view = mStencilTexView;
+            depthStencilAttachment.depthLoadOp = WGPULoadOp_Clear;
+            depthStencilAttachment.depthStoreOp = WGPUStoreOp_Store;
+            depthStencilAttachment.depthClearValue = 1.0f;
+            depthStencilAttachment.depthReadOnly = false;
+            depthStencilAttachment.stencilLoadOp = WGPULoadOp_Clear;
+            depthStencilAttachment.stencilStoreOp = WGPUStoreOp_Store;
+            depthStencilAttachment.stencilClearValue = 0;
+            depthStencilAttachment.stencilReadOnly = false;
+            // render pass color attachment
+            WGPURenderPassColorAttachment colorAttachment{};
+            colorAttachment.view = backBufferView;
+            colorAttachment.resolveTarget = nullptr;
+            colorAttachment.loadOp = WGPULoadOp_Clear;
+            colorAttachment.storeOp = WGPUStoreOp_Store;
+            colorAttachment.clearValue = { 0.0f, 0.0f, 0.0f, 1.0 };
+            // render pass descriptor
+            WGPURenderPassDescriptor renderPassDesc{};
+            renderPassDesc.nextInChain = nullptr;
+            renderPassDesc.label = "The render pass";
+            renderPassDesc.colorAttachmentCount = 1;
+            renderPassDesc.colorAttachments = &colorAttachment;
+            renderPassDesc.depthStencilAttachment = &depthStencilAttachment;
+            //renderPassDesc.depthStencilAttachment = nullptr;
+            renderPassDesc.occlusionQuerySet = nullptr;
+            renderPassDesc.timestampWriteCount = 0;
+            renderPassDesc.timestampWrites = nullptr;
+            // begin render pass
+            WGPURenderPassEncoder renderPassEncoder = wgpuCommandEncoderBeginRenderPass(commandEncoder, &renderPassDesc); {
+                // iterate render data
+                for (size_t i = 0; i < mRenderDatas.count; i++) {
+                    WgRenderDataShape* renderData = (WgRenderDataShape*)(mRenderDatas[i]);
+                    
+                    for (uint32_t j = 0; j < renderData->mGeometryDataFill.count; j++) {
+                        // draw to stencil (first pass)
+                        mPipelineEmpty.set(renderPassEncoder);
+                        mPipelineBindGroupEmpty.bind(renderPassEncoder, 0);
+                        renderData->mGeometryDataFill[j]->draw(renderPassEncoder);
+
+                        // fill shape (secind pass)
+                        renderData->mPipelineBase->set(renderPassEncoder);
+                        renderData->mPipelineBindGroup->bind(renderPassEncoder, 0);
+                        mGeometryDataPipeline.draw(renderPassEncoder);
+                    }
+                }
+            }
+            // end render pass
+            wgpuRenderPassEncoderEnd(renderPassEncoder);
+            wgpuRenderPassEncoderRelease(renderPassEncoder);
+
+            // release backbuffer and present
+            wgpuTextureViewRelease(backBufferView);
+        }
+        commandsBuffer = wgpuCommandEncoderFinish(commandEncoder, &commandBufferDesc);
+        wgpuCommandEncoderRelease(commandEncoder);
+    }
+    wgpuQueueSubmit(mQueue, 1, &commandsBuffer);
+    wgpuCommandBufferRelease(commandsBuffer);
+    
+    // go to the next frame
+    wgpuSwapChainPresent(mSwapChain);
+
+    mRenderDatas.clear();
     return true;
 }
 
+bool WgRenderer::target(uint32_t* buffer, uint32_t stride, uint32_t w, uint32_t h) {
+    // store target surface properties
+    mTargetSurface.stride = stride;
+    mTargetSurface.w = w;
+    mTargetSurface.h = h;
+
+    // update view matrix
+    mViewMatrix[0]  = +2.0f / w; mViewMatrix[1]  = +0.0f;     mViewMatrix[2]  = +0.0f; mViewMatrix[3]  = +0.0f;
+    mViewMatrix[4]  = +0.0f;     mViewMatrix[5]  = -2.0f / h; mViewMatrix[6]  = +0.0f; mViewMatrix[7]  = +0.0f;
+    mViewMatrix[8]  = +0.0f;     mViewMatrix[9]  = +0.0f;     mViewMatrix[10] = -1.0f; mViewMatrix[11] = +0.0f;
+    mViewMatrix[12] = -1.0f;     mViewMatrix[13] = +1.0f;     mViewMatrix[14] = +0.0f; mViewMatrix[15] = +1.0f;
+
+    // TODO: Add ability to render into offscreen buffer
+    return true;
+}
+
+// target for native window handle
 bool WgRenderer::target(void* window, uint32_t w, uint32_t h) {
+    // store target surface properties
+    mTargetSurface.stride = w;
+    mTargetSurface.w = w > 0 ? w : 1;
+    mTargetSurface.h = h > 0 ? h : 1;
+
+    // update view matrix
+    mViewMatrix[0]  = +2.0f / w; mViewMatrix[1]  = +0.0f;     mViewMatrix[2]  = +0.0f; mViewMatrix[3]  = +0.0f;
+    mViewMatrix[4]  = +0.0f;     mViewMatrix[5]  = -2.0f / h; mViewMatrix[6]  = +0.0f; mViewMatrix[7]  = +0.0f;
+    mViewMatrix[8]  = +0.0f;     mViewMatrix[9]  = +0.0f;     mViewMatrix[10] = -1.0f; mViewMatrix[11] = +0.0f;
+    mViewMatrix[12] = -1.0f;     mViewMatrix[13] = +1.0f;     mViewMatrix[14] = +0.0f; mViewMatrix[15] = +1.0f;
+
+    // TODO: replace solution to cross-platform realization
+    // surface descriptor from windows hwnd
+    WGPUSurfaceDescriptorFromWindowsHWND surfaceDescHwnd{};
+    surfaceDescHwnd.chain.next = nullptr;
+    surfaceDescHwnd.chain.sType = WGPUSType_SurfaceDescriptorFromWindowsHWND;
+    surfaceDescHwnd.hinstance = GetModuleHandle(NULL);
+    surfaceDescHwnd.hwnd = (HWND)window;
+    WGPUSurfaceDescriptor surfaceDesc{};
+    surfaceDesc.nextInChain = (const WGPUChainedStruct*)&surfaceDescHwnd;
+    surfaceDesc.label = "The surface";
+    mSurface = wgpuInstanceCreateSurface(mInstance, &surfaceDesc);
+    assert(mSurface);
+
+    // get preferred format
+    WGPUTextureFormat swapChainFormat = WGPUTextureFormat_BGRA8Unorm;
+    // swapchain descriptor
+    WGPUSwapChainDescriptor swapChainDesc{};
+    swapChainDesc.nextInChain = nullptr;
+    swapChainDesc.label = "The swapchain";
+    swapChainDesc.usage = WGPUTextureUsage_RenderAttachment;
+    swapChainDesc.format = swapChainFormat;
+    swapChainDesc.width = mTargetSurface.w;
+    swapChainDesc.height = mTargetSurface.h;
+    swapChainDesc.presentMode = WGPUPresentMode_Mailbox;
+    mSwapChain = wgpuDeviceCreateSwapChain(mDevice, mSurface, &swapChainDesc);
+    assert(mSwapChain);
+
+    // depth-stencil texture
+    WGPUTextureDescriptor textureDesc{};
+    textureDesc.nextInChain = nullptr;
+    textureDesc.label = "The depth-stencil texture";
+    textureDesc.usage = WGPUTextureUsage_RenderAttachment;
+    textureDesc.dimension = WGPUTextureDimension_2D;
+    textureDesc.size = { swapChainDesc.width, swapChainDesc.height, 1 }; // window size
+    textureDesc.format = WGPUTextureFormat_Stencil8;
+    textureDesc.mipLevelCount = 1;
+    textureDesc.sampleCount = 1;
+    textureDesc.viewFormatCount = 0;
+    textureDesc.viewFormats = nullptr;
+    mStencilTex = wgpuDeviceCreateTexture(mDevice, &textureDesc);
+    assert(mStencilTex);
+
+    // depth-stencil texture view
+    WGPUTextureViewDescriptor textureViewDesc{};
+    textureViewDesc.nextInChain = nullptr;
+    textureViewDesc.label = "The depth-stencil texture view";
+    textureViewDesc.format = WGPUTextureFormat_Stencil8;
+    textureViewDesc.dimension = WGPUTextureViewDimension_2D;
+    textureViewDesc.baseMipLevel = 0;
+    textureViewDesc.mipLevelCount = 1;
+    textureViewDesc.baseArrayLayer = 0;
+    textureViewDesc.arrayLayerCount = 1;
+    textureViewDesc.aspect = WGPUTextureAspect_All;
+    mStencilTexView = wgpuTextureCreateView(mStencilTex, &textureViewDesc);
+    assert(mStencilTexView);
+
+    // update pipeline geometry data
+    static float vertexData[] = {
+            0.0f,     0.0f, 0.0f,
+        (float)w,     0.0f, 0.0f,
+        (float)w, (float)h, 0.0f,
+            1.0f, (float)h, 0.0f
+    };
+    static uint32_t indexData[] = { 0, 1, 2, 0, 2, 3 };
+    // update render data pipeline empty
+    mGeometryDataPipeline.update(mDevice, mQueue, vertexData, 4, indexData, 6);
+    // update bind group pipeline empty
+    WgPipelineDataEmpty pipelineDataEmpty{};
+    pipelineDataEmpty.updateMatrix(mViewMatrix, nullptr);
+    mPipelineBindGroupEmpty.update(mQueue, pipelineDataEmpty);
+
     return true;
 }
 
