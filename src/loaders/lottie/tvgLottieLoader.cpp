thorvg/thorvg

commit 29b5bc372de91cc9ef62b02d6c3117a4e24a7ff0

lottie/loader: Corrected an issue with the return value when loading fails.
Previously, Picture::load(const char* data, uint32_t size, const std::string& mimeType, bool copy = false)
would return 'Success' even when the data is invalid.

This issue only occurred when the number of threads is set to 0.

src/loaders/lottie/tvgLottieLoader.cpp
@@ -66,10 +66,11 @@ void LottieLoader::run(unsigned tid)
         builder->update(comp, frameNo);
     //initial loading
     } else {
-        LottieParser parser(content, dirName);
-        if (!parser.parse()) return;
-        comp = parser.comp;
-        if (!comp) return;
+        if (!comp) {
+            LottieParser parser(content, dirName);
+            if (!parser.parse()) return;
+            comp = parser.comp;
+        }
         builder->build(comp);
         w = static_cast<float>(comp->w);
         h = static_cast<float>(comp->h);
@@ -104,7 +105,12 @@ LottieLoader::~LottieLoader()
 bool LottieLoader::header()
 {
     //A single thread doesn't need to perform intensive tasks.
-    if (TaskScheduler::threads() == 0) return true;
+    if (TaskScheduler::threads() == 0) {
+        LottieParser parser(content, dirName);
+        if (!parser.parse()) return false;
+        comp = parser.comp;
+        return true;
+    }
 
     //Quickly validate the given Lottie file without parsing in order to get the animation info.
     auto startFrame = 0.0f;

commit 2367ef7958494073aee2d0e6219029b37a116a17

lottie/builder: Fix a broken animation
The animation couldn't be triggered on a single thread.
Regression bug introduced by 29b5bc372de91cc9ef62b02d6c3117a4e24a7ff0

src/loaders/lottie/tvgLottieLoader.cpp
@@ -75,6 +75,7 @@ void LottieLoader::run(unsigned tid)
         w = static_cast<float>(comp->w);
         h = static_cast<float>(comp->h);
         frameDuration = comp->duration();
+        frameCnt = comp->frameCnt();
     }
 }
 
@@ -106,10 +107,9 @@ bool LottieLoader::header()
 {
     //A single thread doesn't need to perform intensive tasks.
     if (TaskScheduler::threads() == 0) {
-        LottieParser parser(content, dirName);
-        if (!parser.parse()) return false;
-        comp = parser.comp;
-        return true;
+        run(0);
+        if (comp) return true;
+        else return false;
     }
 
     //Quickly validate the given Lottie file without parsing in order to get the animation info.
@@ -297,10 +297,6 @@ bool LottieLoader::read()
     if (comp) return true;
 
     TaskScheduler::request(this);
-
-    if (comp && TaskScheduler::threads() == 0) {
-        frameCnt = comp->frameCnt();
-    }
 
     return true;
 }