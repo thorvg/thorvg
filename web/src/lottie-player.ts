c913d2ef6939bf8c965fdc3a06762e538603c427
Jinny You <jinny@lottiefiles.com>
2023-12-11 14:08:43 +0900

    web: Add wasm loading logic

--- web/src/lottie-player.ts
+++ web/src/lottie-player.ts
@@ -1,5 +1,12 @@
-import { LitElement, type TemplateResult, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+// @ts-ignore: WASM Glue code doesn't have type & Only available on build progress
+import Module from '../dist/thorvg-wasm';
+import { ExportableType, LibraryVersion, LottiePlayerModel, PlayerEvent, PlayerState } from './lottie-player.model';
+
+let _tvg: any;
+(async () => {  
+  const module = await Module();
+  _tvg = new module.TvgLottieAnimation();
+})();
 
 @customElement('lottie-player')
 export class LottiePlayer extends LitElement {
