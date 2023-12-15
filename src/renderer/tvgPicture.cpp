04d2f46084265c8983f40d05e56566d3017f8946
Hermet Park <hermet@lottiefiles.com>
2023-12-15 19:12:18 +0900

    renderer/picture: fixed a regression
    reverted a wrong change from the previous code refactoring
    in 56433484726f7c1f165c35b27ac1625113234bc0

--- src/renderer/tvgPicture.cpp
+++ src/renderer/tvgPicture.cpp
@@ -42,7 +42,7 @@ RenderUpdateFlag Picture::Impl::load()
                 }
                 return RenderUpdateFlag::None;
             }
-        }
+        } else loader->sync();
 
         if (!surface) {
             if ((surface = loader->bitmap().release())) {
