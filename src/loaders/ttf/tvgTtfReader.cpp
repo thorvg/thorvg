3c4e434b398e2ad7e61b1fa0ff6c5ff0bfe4c331
Hermet Park <hermet@lottiefiles.com>
2023-12-20 14:22:42 +0900

    loader/ttf: introduce a new sfnt(scalable font) loader.
    ttf is an industry standard format that is the most widely used
    in the products. Now thorvg supports the basic features of
    the font to supplement the text drawing.
    
    The implementation is followed the ttf spec,
    the covered features are:
    
    - horizontal layouting with kerning.
    - utf8 -> utf32 converted glyph drawing.
    
    To use the feature, please enable ttf loader:
    $meson -Dloaders="ttf_beta, ..."
    
    @Issue: https://github.com/thorvg/thorvg/issues/969

---
+++ src/loaders/ttf/tvgTtfReader.cpp
@@ -0,0 +1,563 @@
+/*
+ * Copyright (c) 2023 the ThorVG project. All rights reserved.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <memory.h>
+
+#include "tvgMath.h"
+#include "tvgShape.h"
+#include "tvgTtfReader.h"
+
+/************************************************************************/
+/* Internal Class Implementation                                        */
+/************************************************************************/
+
+
+static inline uint32_t _u32(uint8_t* data, uint32_t offset)
+{
+    auto base = data + offset;
+    return (base[0] << 24 | base[1] << 16 | base[2] << 8 | base[3]);
+}
+
+
+static inline uint16_t _u16(uint8_t* data, uint32_t offset)
+{
+    auto base = data + offset;
+    return (base[0] << 8 | base[1]);
+}
+
+
+static inline int16_t _i16(uint8_t* data, uint32_t offset)
+{
+    return (int16_t) _u16(data, offset);
+}
+
+
+static inline uint8_t _u8(uint8_t* data, uint32_t offset)
+{
+    return *(data + offset);
+}
+
+
+static inline int8_t _i8(uint8_t* data, uint32_t offset)
+{
+    return (int8_t) _u8(data, offset);
+}
+
+
+static int _cmpu32(const void *a, const void *b)
+{
+    return memcmp(a, b, 4);
+}
+
+
+bool TtfReader::validate(uint32_t offset, uint32_t margin) const
+{
+#if 1
+    if ((offset > size) || (size - offset < margin)) {
+        TVGERR("TTF", "Invalidate data");
+        return false;
+    }
+#endif
+    return true;
+}
+
+
+uint32_t TtfReader::table(const char* tag)
+{
+    auto tableCnt = _u16(data, 4);
+    if (!validate(12, (uint32_t) tableCnt * 16)) return 0;
+
+    auto match = bsearch(tag, data + 12, tableCnt, 16, _cmpu32);
+    if (!match) {
+        TVGLOG("TTF", "No searching table = %s", tag);
+        return 0;
+    }
+
+    return _u32(data, (uint32_t) ((uint8_t*) match - data + 8));
+}
+
+
+uint32_t TtfReader::cmap_12_13(uint32_t table, uint32_t codepoint, int fmt) const
+{
+    //A minimal header is 16 bytes
+    auto len = _u32(data, table + 4);
+    if (len < 16) return -1;
+
+    if (!validate(table, len)) return -1;
+
+    auto entryCnt = _u32(data, table + 12);
+
+    for (uint32_t i = 0; i < entryCnt; ++i) {
+        auto firstCode = _u32(data, table + (i * 12) + 16);
+        auto lastCode = _u32(data, table + (i * 12) + 16 + 4);
+        if (codepoint < firstCode || codepoint > lastCode) continue;
+        auto glyphOffset = _u32(data, table + (i * 12) + 16 + 8);
+        if (fmt == 12) return (codepoint - firstCode) + glyphOffset;
+        else return glyphOffset;
+    }
+    return -1;
+}
+
+
+uint32_t TtfReader::cmap_4(uint32_t table, uint32_t codepoint) const
+{
+    //cmap format 4 only supports the Unicode BMP.
+    if (codepoint > 0xffff) return -1;
+
+    auto shortCode = static_cast<uint16_t>(codepoint);
+
+    if (!validate(table, 8)) return -1;
+
+    auto segmentCnt = _u16(data, table);
+    if ((segmentCnt & 1) || segmentCnt == 0) return -1;
+
+    //find starting positions of the relevant arrays.
+    auto endCodes = table + 8;
+    auto startCodes = endCodes + segmentCnt + 2;
+    auto idDeltas = startCodes + segmentCnt;
+    auto idRangeOffsets = idDeltas + segmentCnt;
+
+    if (!validate(idRangeOffsets, segmentCnt)) return -1;
+
+    //find the segment that contains shortCode by binary searching over the highest codes in the segments.
+    //binary search, but returns the next highest element if key could not be found.
+    uint8_t* segment = nullptr;
+    auto n = segmentCnt / 2;
+    if (n > 0) {
+        uint8_t key[2] = {(uint8_t)(codepoint >> 8), (uint8_t)codepoint};
+        auto bytes = data + endCodes;
+        auto low = 0;
+        auto high = n - 1;
+        while (low != high) {
+            auto mid = low + (high - low) / 2;
+            auto sample = bytes + mid * 2;
+            if (memcmp(key, sample, 2) > 0) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        segment = (bytes + low * 2);
+    }
+
+    auto segmentIdx = static_cast<uint32_t>(segment - (data + endCodes));
+
+    //look up segment info from the arrays & short circuit if the spec requires.
+    auto startCode = _u16(data, startCodes + segmentIdx);
+    if (startCode > shortCode) return 0;
+
+    auto delta = _u16(data, idDeltas + segmentIdx);
+    auto idRangeOffset = _u16(data, idRangeOffsets + segmentIdx);
+    //intentional integer under- and overflow.
+    if (idRangeOffset == 0) return (shortCode + delta) & 0xffff;
+
+    //calculate offset into glyph array and determine ultimate value.
+    auto offset = idRangeOffsets + segmentIdx + idRangeOffset + 2U * (unsigned int)(shortCode - startCode);
+    if (!validate(offset, 2)) return -1;
+
+    auto id = _u16(data, offset);
+    //intentional integer under- and overflow.
+    if (id > 0) return (id + delta) & 0xffff;
+
+    return 0;
+}
+
+
+uint32_t TtfReader::cmap_6(uint32_t table, uint32_t codepoint) const
+{
+    //cmap format 6 only supports the Unicode BMP.
+    if (codepoint > 0xFFFF) return 0;
+
+    auto firstCode  = _u16(data, table);
+    auto entryCnt = _u16(data, table + 2);
+    if (!validate(table, 4 + 2 * entryCnt)) return -1;
+
+    if (codepoint < firstCode) return -1;
+    codepoint -= firstCode;
+    if (codepoint >= entryCnt) return -1;
+    return _u16(data, table + 4 + 2 * codepoint);
+}
+
+
+//Returns the offset into the font that the glyph's outline is stored at
+uint32_t TtfReader::outlineOffset(uint32_t glyph)
+{
+    uint32_t cur, next;
+
+    if (!loca) loca = table("loca");
+    if (!glyf) glyf = table("glyf");
+
+    if (metrics.locaFormat == 0) {
+        auto base = loca + 2 * glyph;
+        if (!validate(base, 4)) {
+            TVGERR("TTF", "invalid outline offset");
+            return 0;
+        }
+        cur = 2U * (uint32_t) _u16(data, base);
+        next = 2U * (uint32_t) _u16(data, base + 2);
+    } else {
+        auto base = loca + 4 * glyph;
+        if (!validate(base, 8)) return 0;
+        cur = _u32(data, base);
+        next = _u32(data, base + 4);
+    }
+    if (cur == next) return 0;
+    return glyf + cur;
+}
+
+
+bool TtfReader::points(uint32_t outline, uint8_t* flags, Point* pts, uint32_t ptsCnt, const Point& offset)
+{
+    #define X_CHANGE_IS_SMALL 0x02
+    #define X_CHANGE_IS_POSITIVE 0x10
+    #define X_CHANGE_IS_ZERO 0x10
+    #define Y_CHANGE_IS_SMALL 0x04
+    #define Y_CHANGE_IS_ZERO 0x20
+    #define Y_CHANGE_IS_POSITIVE 0x20
+
+    long accum = 0L;
+
+    for (uint32_t i = 0; i < ptsCnt; ++i) {
+        if (flags[i] & X_CHANGE_IS_SMALL) {
+            if (!validate(outline, 1)) {
+                return false;
+            }
+            auto value = (long) _u8(data, outline++);
+            auto bit = !!(flags[i] & X_CHANGE_IS_POSITIVE);
+            accum -= (value ^ -bit) + bit;
+        } else if (!(flags[i] & X_CHANGE_IS_ZERO)) {
+            if (!validate(outline, 2)) return false;
+            accum += _i16(data, outline);
+            outline += 2;
+        }
+        pts[i].x = offset.x + (float) accum;
+    }
+
+    accum = 0L;
+
+    for (uint32_t i = 0; i < ptsCnt; ++i) {
+        if (flags[i] & Y_CHANGE_IS_SMALL) {
+            if (!validate(outline, 1)) return false;
+            auto value = (long) _u8(data, outline++);
+            auto bit = !!(flags[i] & Y_CHANGE_IS_POSITIVE);
+            accum -= (value ^ -bit) + bit;
+        } else if (!(flags[i] & Y_CHANGE_IS_ZERO)) {
+            if (!validate(outline, 2)) return false;
+            accum += _i16(data, outline);
+            outline += 2;
+        }
+        pts[i].y = offset.y - (float) accum;
+    }
+    return true;
+}
+
+
+bool TtfReader::flags(uint32_t *outline, uint8_t* flags, uint32_t flagsCnt)
+{
+    #define REPEAT_FLAG 0x08
+
+    auto offset = *outline;
+    uint8_t value = 0;
+    uint8_t repeat = 0;
+
+    for (uint32_t i = 0; i < flagsCnt; ++i) {
+        if (repeat) {
+            --repeat;
+        } else {
+            if (!validate(offset, 1)) return false;
+            value = _u8(data, offset++);
+            if (value & REPEAT_FLAG) {
+                if (!validate(offset, 1)) return false;
+                repeat = _u8(data, offset++);
+            }
+        }
+        flags[i] = value;
+    }
+    *outline = offset;
+    return true;
+}
+
+
+/************************************************************************/
+/* External Class Implementation                                        */
+/************************************************************************/
+
+bool TtfReader::header()
+{
+    if (!validate(0, 12)) return false;
+
+    //verify ttf(scalable font)
+    auto type = _u32(data, 0);
+    if (type != 0x00010000 && type != 0x74727565) return false;
+
+    //header
+    auto head = table("head");
+    if (!validate(head, 54)) return false;
+
+    metrics.unitsPerEm = _u16(data, head + 18);
+    metrics.locaFormat = _u16(data, head + 50);
+
+    //horizontal metrics
+    auto hhea = table("hhea");
+    if (!validate(hhea, 36)) return false;
+
+    metrics.hhea.ascent = _i16(data, hhea + 4);
+    metrics.hhea.descent = _i16(data, hhea + 6);
+    metrics.hhea.lineGap = _i16(data, hhea + 8);
+    metrics.numHmtx = _u16(data, hhea + 34);
+
+    //kerning
+    this->kern = table("kern");
+    if (this->kern) {
+        if (!validate(kern, 4)) return false;
+        if (_u16(data, kern) != 0) return false;
+    }
+
+    return true;
+}
+
+
+uint32_t TtfReader::glyph(uint32_t codepoint)
+{
+    if (cmap == 0) {
+        cmap = table("cmap");
+        if (!validate(cmap, 4)) return -1;
+    }
+
+    auto entryCnt = _u16(data, cmap + 2);
+    if (!validate(cmap, 4 + entryCnt * 8)) return -1;
+
+    //full repertory (non-BMP map).
+    for (auto idx = 0; idx < entryCnt; ++idx) {
+        auto entry = cmap + 4 + idx * 8;
+        auto type = _u16(data, entry) * 0100 + _u16(data, entry + 2);
+        //unicode map
+        if (type == 0004 || type == 0312) {
+            auto table = cmap + _u32(data, entry + 4);
+            if (!validate(table, 8)) return -1;
+            //dispatch based on cmap format.
+            auto format = _u16(data, table);
+            switch (format) {
+                case 12: return cmap_12_13(table, codepoint, 12);
+                default: return -1;
+            }
+        }
+    }
+
+    //Try looking for a BMP map.
+    for (auto idx = 0; idx < entryCnt; ++idx) {
+        auto entry = cmap + 4 + idx * 8;
+        auto type = _u16(data, entry) * 0100 + _u16(data, entry + 2);
+        //Unicode BMP
+        if (type == 0003 || type == 0301) {
+            auto table = cmap + _u32(data, entry + 4);
+            if (!validate(table, 6)) return -1;
+            //Dispatch based on cmap format.
+            switch (_u16(data, table)) {
+                case 4: return cmap_4(table + 6, codepoint);
+                case 6: return cmap_6(table + 6, codepoint);
+                default: return -1;
+            }
+        }
+    }
+    return -1;
+}
+
+
+uint32_t TtfReader::glyph(uint32_t codepoint, TtfGlyphMetrics& gmetrics)
+{
+    auto glyph = this->glyph(codepoint);
+    if (glyph == INVALID_GLYPH) {
+        TVGERR("TTF", "invalid glyph id, codepoint(0x%x)", codepoint);
+        return INVALID_GLYPH;
+    }
+
+    //horizontal metrics
+    if (!hmtx) hmtx = table("hmtx");
+
+    //glyph is inside long metrics segment.
+    if (glyph < metrics.numHmtx) {
+        auto offset = hmtx + 4 * glyph;
+        if (!validate(offset, 4)) return INVALID_GLYPH;
+        gmetrics.advanceWidth = _u16(data, offset);
+        gmetrics.leftSideBearing = _i16(data, offset + 2);
+    /* glyph is inside short metrics segment. */
+    } else {
+        auto boundary = hmtx + 4U * (uint32_t) metrics.numHmtx;
+        if (boundary < 4) return INVALID_GLYPH;
+
+        auto offset = boundary - 4;
+        if (!validate(offset, 4)) return INVALID_GLYPH;
+        gmetrics.advanceWidth = _u16(data, offset);
+        offset = boundary + 2 * (glyph - metrics.numHmtx);
+        if (!validate(offset, 2)) return INVALID_GLYPH;
+        gmetrics.leftSideBearing = _i16(data, offset);
+    }
+
+    gmetrics.outline = outlineOffset(glyph);
+    if (!gmetrics.outline || !validate(gmetrics.outline, 10)) return INVALID_GLYPH;
+
+    //read the bounding box from the font file verbatim.
+    float bbox[4];
+    bbox[0] = static_cast<float>(_i16(data, gmetrics.outline + 2));
+    bbox[1] = static_cast<float>(_i16(data, gmetrics.outline + 4));
+    bbox[2] = static_cast<float>(_i16(data, gmetrics.outline + 6));
+    bbox[3] = static_cast<float>(_i16(data, gmetrics.outline + 8));
+
+    if (bbox[2] <= bbox[0] || bbox[3] <= bbox[1]) return INVALID_GLYPH;
+
+    gmetrics.minw = bbox[2] - bbox[0] + 1;
+    gmetrics.minh = bbox[3] - bbox[1] + 1;
+    gmetrics.yOffset = bbox[3];
+
+    return glyph;
+}
+
+
+bool TtfReader::convert(Shape* shape, TtfGlyphMetrics& gmetrics, const Point& offset, const Point& kerning)
+{
+    #define ON_CURVE 0x01
+
+    auto cntrsCnt = (uint32_t) _i16(data, gmetrics.outline);
+    if (cntrsCnt == 0) return false;
+
+    auto outline = gmetrics.outline + 10;
+    if (!validate(outline, cntrsCnt * 2 + 2)) return false;
+
+    auto ptsCnt = _u16(data, outline + (cntrsCnt - 1) * 2) + 1;
+    size_t endPts[cntrsCnt];  //the index of the contour points.
+
+    for (uint32_t i = 0; i < cntrsCnt; ++i) {
+        endPts[i] = (uint32_t) _u16(data, outline);
+        outline += 2;
+    }
+    outline += 2U + _u16(data, outline);
+
+    uint8_t flags[ptsCnt];
+    if (!this->flags(&outline, flags, ptsCnt)) return false;
+
+    Point pts[ptsCnt];
+    if (!this->points(outline, flags, pts, ptsCnt, offset + kerning)) return false;
+
+    //generate tvg pathes.
+    auto& pathCmds = P(shape)->rs.path.cmds;
+    auto& pathPts = P(shape)->rs.path.pts;
+    pathCmds.reserve(ptsCnt);
+    pathPts.reserve(ptsCnt);
+
+    uint32_t begin = 0;
+
+    for (uint32_t i = 0; i < cntrsCnt; ++i) {
+        //contour must start with move to
+        pathCmds.push(PathCommand::MoveTo);
+        pathPts.push(pts[begin]);
+
+        bool offCurve = false;
+        auto last = (endPts[i] - begin) + 1;
+
+        for (uint32_t x = 1; x < last; ++x) {
+            if (flags[begin + x] & ON_CURVE) {
+                if (offCurve) {
+                    pathCmds.push(PathCommand::CubicTo);
+                    pathPts.push(pathPts.last() + (2.0f/3.0f) * (pts[begin + x - 1] - pathPts.last()));
+                    pathPts.push(pts[begin + x] + (2.0f/3.0f) * (pts[begin + x - 1] - pts[begin + x]));
+                    pathPts.push(pts[begin + x]);
+                    offCurve = false;
+                } else {
+                    pathCmds.push(PathCommand::LineTo);
+                    pathPts.push(pts[begin + x]);
+                }
+            } else {
+                if (offCurve) {
+                    pathCmds.push(PathCommand::CubicTo);
+                    auto end = (pts[begin + x] + pts[begin + x - 1]) * 0.5f;
+                    pathPts.push(pathPts.last() + (2.0f/3.0f) * (pts[begin + x - 1] - pathPts.last()));
+                    pathPts.push(end + (2.0f/3.0f) * (pts[begin + x - 1] - end));
+                    pathPts.push(end);
+                } else {
+                    offCurve = true;
+                }
+            }
+        }
+        if (offCurve) {
+            pathCmds.push(PathCommand::CubicTo);
+            pathPts.push(pathPts.last() + (2.0f/3.0f) * (pts[begin + last - 1] - pathPts.last()));
+            pathPts.push(pts[begin] + (2.0f/3.0f) * (pts[begin + last - 1] - pts[begin]));
+            pathPts.push(pts[begin]);
+        }
+        //contour must end with close
+        pathCmds.push(PathCommand::Close);
+        begin = endPts[i] + 1;
+    }
+    return true;
+}
+
+
+void TtfReader::kerning(uint32_t lglyph, uint32_t rglyph, Point& out)
+{
+    #define HORIZONTAL_KERNING 0x01
+    #define MINIMUM_KERNING 0x02
+    #define CROSS_STREAM_KERNING 0x04
+
+    if (!kern) return;
+
+    auto kern = this->kern;
+
+    out.x = out.y = 0.0f;
+
+    //kern tables
+    auto tableCnt = _u16(data, kern + 2);
+    kern += 4;
+
+    while (tableCnt > 0) {
+        //read subtable header.
+        if (!validate(kern, 6)) return;
+        auto length = _u16(data, kern + 2);
+        auto format = _u8(data, kern + 4);
+        auto flags = _u8(data, kern + 5);
+        kern += 6;
+
+        if (format == 0 && (flags & HORIZONTAL_KERNING) && !(flags & MINIMUM_KERNING)) {
+            //read format 0 header.
+            if (!validate(kern, 8)) return;
+            auto pairCnt = _u16(data, kern);
+            kern += 8;
+
+            //look up character code pair via binary search.
+            uint8_t key[4];
+            key[0] = (lglyph >> 8) & 0xff;
+            key[1] = lglyph & 0xff;
+            key[2] = (rglyph >> 8) & 0xff;
+            key[3] = rglyph & 0xff;
+
+            auto match = bsearch(key, data + kern, pairCnt, 6, _cmpu32);
+
+            if (match) {
+                auto value = _i16(data, (uint32_t)((uint8_t *) match - data + 4));
+                if (flags & CROSS_STREAM_KERNING) out.y += value;
+                else out.x += value;
+            }
+        }
+        kern += length;
+        --tableCnt;
+    }
+}
---
+++ src/loaders/ttf/tvgTtfReader.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2023 the ThorVG project. All rights reserved.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _TVG_TTF_READER_H
+#define _TVG_TTF_READER_H
+
+#include "tvgCommon.h"
+#include "tvgArray.h"
+
+#define INVALID_GLYPH ((uint32_t)-1)
+
+struct TtfGlyphMetrics
+{
+    uint32_t outline;    //glyph outline table offset
+
+    float advanceWidth;
+    float leftSideBearing;
+    float yOffset;
+    float minw;
+    float minh;
+};
+
+
+struct TtfReader
+{
+public:
+    uint8_t* data = nullptr;
+    uint32_t size = 0;
+
+    struct
+    {
+        //horizontal header info
+        struct {
+            float ascent;
+            float descent;
+            float lineGap;
+        } hhea;
+
+        float minw;            //first glyph width (used for italic)
+        uint16_t unitsPerEm;
+        uint16_t numHmtx;      //the number of Horizontal metrics table
+        uint8_t locaFormat;    //0 for short offsets, 1 for long
+    } metrics;
+
+    bool header();
+    uint32_t glyph(uint32_t codepoint, TtfGlyphMetrics& gmetrics);
+    void kerning(uint32_t lglyph, uint32_t rglyph, Point& out);
+    bool convert(Shape* shape, TtfGlyphMetrics& gmetrics, const Point& offset, const Point& kerning);
+
+private:
+    //table offsets
+    uint32_t cmap = 0;
+    uint32_t hmtx = 0;
+    uint32_t loca = 0;
+    uint32_t glyf = 0;
+    uint32_t kern = 0;
+
+    uint32_t cmap_12_13(uint32_t table, uint32_t codepoint, int which) const;
+    uint32_t cmap_4(uint32_t table, uint32_t codepoint) const;
+    uint32_t cmap_6(uint32_t table, uint32_t codepoint) const;
+    bool validate(uint32_t offset, uint32_t margin) const;
+    uint32_t table(const char* tag);
+    uint32_t outlineOffset(uint32_t glyph);
+    uint32_t glyph(uint32_t codepoint);
+    bool genPath(uint8_t* flags, uint16_t basePoint, uint16_t count);
+    bool genSimpleOutline(Shape* shape, uint32_t outline, uint32_t cntrsCnt);
+    bool points(uint32_t outline, uint8_t* flags, Point* pts, uint32_t ptsCnt, const Point& offset);
+    bool flags(uint32_t *outline, uint8_t* flags, uint32_t flagsCnt);
+};
+
+#endif //_TVG_TTF_READER_H
