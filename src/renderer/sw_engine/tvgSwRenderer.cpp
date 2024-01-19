thorvg/thorvg

commit ca3c1fc1b9bc1a74b5430f3eab1952727f1762b1

sw_engine: retain the compositor cache memory
The compositor memory is likely to be reused in the next frame.
To enhance performance, it is advisable to retain this memory by default.

We may consider introducing a cache policy interface in the Initializer.
This would allow users to manage the Canvas memory more effectively.

Anyhow, this improves the Lottie example performance by 10%

src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -474,7 +474,6 @@ bool SwRenderer::postRender()
     }
     tasks.clear();
 
-    clearCompositors();
     return true;
 }
 

commit 10ad8bad36b7204d8c7a7c8832a1bc2e91009be7

sw_engine: fix a regression bug
invalidate cached compositors when target size is changed.
compositors must be re-initialized with a new size.

regression bug by ca3c1fc1b9bc1a74b5430f3eab1952727f1762b1

src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -425,6 +425,8 @@ bool SwRenderer::target(pixel_t* data, uint32_t stride, uint32_t w, uint32_t h, 
 {
     if (!data || stride == 0 || w == 0 || h == 0 || w > stride) return false;
 
+    clearCompositors();
+
     if (!surface) surface = new SwSurface;
 
     surface->data = data;