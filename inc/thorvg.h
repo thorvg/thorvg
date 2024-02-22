20a3c4d363020ea2e5d039d2ca1e4be2b4a82a6d
Jinny You <jinny@lottiefiles.com>
2024-02-22 20:46:35 +0900

    lottie: Introduce the LottieAnimation class
    This class extends the Animation and serves advanced Lottie features.
    It's designed to separately have Lottie Animation's unique specs.
    
    For now, this will have Slot overriding feature.
    
    @APIs:
    - Result LottieAnimation::override(const char* slotJson) noexcept;
    - static std::unique_ptr<LottieAnimation> LottieAnimation::gen() noexcept;
    
    @Issue: https://github.com/thorvg/thorvg/issues/1808

--- inc/thorvg.h
+++ inc/thorvg.h
@@ -1850,6 +1850,45 @@ public:
 
 
 /**
+ * @class LottieAnimation
+ *
+ * @brief The LottieAnimation class enables control of advanced Lottie features.
+ *
+ * This class extends the Animation and has additional interfaces.
+ *
+ * @see Animation
+ * 
+ * @note Experimental API
+ */
+class TVG_API LottieAnimation final : public Animation
+{
+public:
+    ~LottieAnimation();
+
+    /**
+     * @brief Rewrite lottie properties through the slot data
+     *
+     * @param[in] slot The lottie slot data
+     *
+     * @retval Result::Success When succeed.
+     * @retval Result::InsufficientCondition When the given parameter is invalid.
+     *
+     * @note Experimental API
+     */
+    Result override(const char* slot) noexcept;
+
+    /**
+     * @brief Creates a new LottieAnimation object.
+     *
+     * @return A new LottieAnimation object.
+     *
+     * @note Experimental API
+     */
+    static std::unique_ptr<LottieAnimation> gen() noexcept;
+};
+
+
+/**
  * @class Saver
  *
  * @brief A class for exporting a paint object into a specified file, from which to recover the paint data later.
