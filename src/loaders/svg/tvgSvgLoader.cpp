730a88c42bbff3082aa6d52681a377ded1079ed7
JunsuChoi <jsuya.choi@samsung.com>
2024-04-15 10:43:28 +0900

    loader/svg: Add null to the end of data
    Because memcpy() is not guaranteed to copy null at the end of the data array,
    it increase the size by 1 and add null
    This prevents invalid access of string functions in parser.

--- src/loaders/svg/tvgSvgLoader.cpp
+++ src/loaders/svg/tvgSvgLoader.cpp
@@ -3809,10 +3809,11 @@ bool SvgLoader::open(const char* data, uint32_t size, bool copy)
     clear();
 
     if (copy) {
-        content = (char*)malloc(size);
+        content = (char*)malloc(size + 1);
         if (!content) return false;
         memcpy((char*)content, data, size);
-    } else content = data;
+        content[size] = '\0';
+    } else content = (char*)data;
 
     this->size = size;
     this->copy = copy;
@@ -3836,7 +3837,7 @@ bool SvgLoader::open(const string& path)
 
     if (filePath.empty()) return false;
 
-    content = filePath.c_str();
+    content = (char*)filePath.c_str();
     size = filePath.size();
 
     return header();
