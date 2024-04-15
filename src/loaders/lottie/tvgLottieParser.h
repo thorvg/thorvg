acd42f3bfa959a4c30499241d872e8ec467e50e8
Hermet Park <hermet@lottiefiles.com>
2024-04-15 21:54:47 +0900

    lottie: generalize parent context access in parsing.
    ensure parent context is generally accessible,
    not limited to gradients.

--- src/loaders/lottie/tvgLottieParser.h
+++ src/loaders/lottie/tvgLottieParser.h
@@ -111,7 +111,7 @@ private:
     //Current parsing context
     struct Context {
         LottieLayer* layer = nullptr;
-        LottieGradient* gradient = nullptr;
+        LottieObject* parent = nullptr;
     } context;
 };
 
