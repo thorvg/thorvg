3b93a8f2f917631ab7d47a203434f0d12e5a7ae4
Hermet Park <hermet@lottiefiles.com>
2024-05-14 16:02:54 +0900

    examples: added a viewport sample

--- examples/Viewport.cpp
+++ examples/Viewport.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 - 2024 the ThorVG project. All rights reserved.
+ * Copyright (c) 2024 the ThorVG project. All rights reserved.
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -52,11 +52,13 @@ void tvgDrawCmds(tvg::Canvas* canvas)
 
 void tvgUpdateCmds(tvg::Canvas* canvas, float progress)
 {
+    constexpr uint32_t VPORT_SIZE = 300;
+
     if (!canvas || reqSync) return;
 
     canvas->clear(false);
 
-    pPicture->translate(WIDTH * progress * 0.05f, HEIGHT * progress * 0.05f);
+    canvas->viewport((WIDTH - VPORT_SIZE) * progress, (HEIGHT - VPORT_SIZE) * progress, VPORT_SIZE, VPORT_SIZE);
 
     auto before = ecore_time_get();
 
