thorvg/thorvg

commit 1ee79a6c2afafb6b16b14abc969b60c1dbc7a065

lottie: add 'tp' tag support
So far it hasn't been possible to specify
a matte layer - by default, it was the layer
above the calling layer. The 'tp' tag support
has been introduced, enabling referencing any
layer by its index.
In cases where the layer referencing the matte
was the first one, a segmentation fault was
observed. This issue has now been resolved.

@Issue: https://github.com/thorvg/thorvg/issues/2325

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -1319,6 +1319,20 @@ static bool _buildComposition(LottieComposition* comp, LottieGroup* parent)
         //attach the precomp layer.
         if (child->refId) _buildReference(comp, child);
 
+        if (child->matte.type != CompositeMethod::None) {
+            //no index of the matte layer is provided: the layer above is used as the matte source
+            if (child->mid == -1) {
+                if (c > parent->children.begin()) {
+                    child->matte.target = static_cast<LottieLayer*>(*(c - 1));
+                }
+            //matte layer is specified by an index.
+            } else {
+                if (auto matte = comp->layer(child->mid)) {
+                    child->matte.target = matte;
+                }
+            }
+        }
+
         if (child->matte.target) {
             //parenting
             _bulidHierarchy(parent, child->matte.target);
@@ -1353,7 +1367,8 @@ bool LottieBuilder::update(LottieComposition* comp, float frameNo)
     if (exps && comp->expressions) exps->update(comp->timeAtFrame(frameNo));
 
     for (auto child = root->children.end() - 1; child >= root->children.begin(); --child) {
-        _updateLayer(root, static_cast<LottieLayer*>(*child), frameNo, exps);
+        auto layer = static_cast<LottieLayer*>(*child);
+        if (!layer->matteSrc) _updateLayer(root, layer, frameNo, exps);
     }
 
     return true;

src/loaders/lottie/tvgLottieModel.cpp
@@ -354,7 +354,7 @@ LottieLayer::~LottieLayer()
         delete(*m);
     }
 
-    delete(matte.target);
+    matte.target = nullptr;
     delete(transform);
 }
 

src/loaders/lottie/tvgLottieModel.h
@@ -561,6 +561,7 @@ struct LottieLayer : LottieGroup
     float outFrame = 0.0f;
     float startFrame = 0.0f;
     char* refId = nullptr;      //pre-composition reference.
+    int16_t mid = -1;           //id of the matte layer.
     int16_t pid = -1;           //id of the parent layer.
     int16_t id = -1;            //id of the current layer.
 

src/loaders/lottie/tvgLottieParser.cpp
@@ -1229,6 +1229,7 @@ LottieLayer* LottieParser::parseLayer()
         else if (KEY_AS("h") || KEY_AS("sh")) getLayerSize(layer->h);
         else if (KEY_AS("sc")) layer->color = getColor(getString());
         else if (KEY_AS("tt")) layer->matte.type = getMatteType();
+        else if (KEY_AS("tp")) layer->mid = getInt();
         else if (KEY_AS("masksProperties")) parseMasks(layer);
         else if (KEY_AS("hd")) layer->hidden = getBool();
         else if (KEY_AS("refId")) layer->refId = getStringCopy();
@@ -1264,21 +1265,9 @@ LottieLayer* LottieParser::parseLayers()
 
     enterArray();
     while (nextArrayValue()) {
-        if (auto layer = parseLayer()) {
-            if (layer->matte.type == CompositeMethod::None) {
-                root->children.push(layer);
-            } else {
-                //matte source must be located in the right previous.
-                auto matte = static_cast<LottieLayer*>(root->children.last());
-                if (matte->matteSrc) {
-                    layer->matte.target = matte;
-                } else {
-                    TVGLOG("LOTTIE", "Matte Source(%s) is not designated?", matte->name);
-                }
-                root->children.last() = layer;
-            }
-        }
+        if (auto layer = parseLayer()) root->children.push(layer);
     }
+
     root->prepare();
     return root;
 }

commit 35a640ce7a504ea0b74c5c5d52184c1f4f59ce38

lottie: hotfix memory leaks by a regression
A regression bug by a recent change:
1ee79a6c2afafb6b16b14abc969b60c1dbc7a065

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -1122,8 +1122,9 @@ static void _updatePrecomp(LottieLayer* precomp, float frameNo, LottieExpression
 
     frameNo = precomp->remap(frameNo, exps);
 
-    for (auto child = precomp->children.end() - 1; child >= precomp->children.begin(); --child) {
-        _updateLayer(precomp, static_cast<LottieLayer*>(*child), frameNo, exps);
+    for (auto c = precomp->children.end() - 1; c >= precomp->children.begin(); --c) {
+        auto child = static_cast<LottieLayer*>(*c);
+        if (!child->matteSrc) _updateLayer(precomp, child, frameNo, exps);
     }
 
     //clip the layer viewport