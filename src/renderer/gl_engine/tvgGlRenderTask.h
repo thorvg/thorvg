76f98008e81e34b87eaf1396591f1b51d0413358
RuiwenTang <tangruiwen1989@gmail.com>
2024-06-25 22:15:30 +0800

    gl_engine: optimize off-screen rendering
    Performing a full-screen RenderPass resolve is too expensive.
    Because most composite cases only require a small area to be rendered off-screen.
    To improve performance, use the bounds of the Geometry for off-screen rendering whenever possible

--- src/renderer/gl_engine/tvgGlRenderTask.h
+++ src/renderer/gl_engine/tvgGlRenderTask.h
@@ -126,6 +126,8 @@ public:
 
     void run() override;
 
+    void setRenderSize(uint32_t width, uint32_t height) { mRenderWidth = width; mRenderHeight = height; }
+
     bool mClearBuffer = true;
 
 protected:
@@ -140,6 +142,8 @@ private:
     GLuint mTargetFbo;
     GlRenderTarget* mFbo;
     Array<GlRenderTask*> mTasks;
+    uint32_t mRenderWidth = 0;
+    uint32_t mRenderHeight = 0;
 };
 
 class GlBlitTask : public GlComposeTask
@@ -162,9 +166,18 @@ class GlDrawBlitTask : public GlComposeTask
 {
 public:
     GlDrawBlitTask(GlProgram*, GLuint target, GlRenderTarget* fbo, Array<GlRenderTask*>&& tasks);
-    ~GlDrawBlitTask() override = default;
+    ~GlDrawBlitTask() override;
+
+    void setPrevTask(GlRenderTask* task) { mPrevTask = task; }
+
+    void setParentSize(uint32_t width, uint32_t height) { mParentWidth = width; mParentHeight = height; }
 
     void run() override;
+
+private:
+    GlRenderTask* mPrevTask = nullptr;
+    uint32_t mParentWidth = 0;
+    uint32_t mParentHeight = 0;
 };
 
 class GlClipTask : public GlRenderTask
