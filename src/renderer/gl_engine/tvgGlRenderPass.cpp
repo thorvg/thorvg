8508227a50502ee59ca64c6963e922c874e9fbe1
RuiwenTang <tangruiwen1989@gmail.com>
2024-06-25 22:15:30 +0800

    gl_engine: optimize off-screen rendering
    Performing a full-screen RenderPass resolve is too expensive.
    Because most composite cases only require a small area to be rendered off-screen.
    To improve performance, use the bounds of the Geometry for off-screen rendering whenever possible

--- src/renderer/gl_engine/tvgGlRenderPass.cpp
+++ src/renderer/gl_engine/tvgGlRenderPass.cpp
@@ -21,67 +21,10 @@
  * SOFTWARE.
  */
 
+#include "tvgMath.h"
 #include "tvgGlRenderPass.h"
 #include "tvgGlRenderTask.h"
-
-GlRenderTarget::GlRenderTarget(uint32_t width, uint32_t height): mWidth(width), mHeight(height) {}
-
-GlRenderTarget::~GlRenderTarget()
-{
-    if (mFbo == 0) return;
-    GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, 0));
-    GL_CHECK(glDeleteFramebuffers(1, &mFbo));
-
-    if (mColorTex != 0) {
-        GL_CHECK(glDeleteTextures(1, &mColorTex));
-    }
-    if (mDepthStencilBuffer != 0) {
-        GL_CHECK(glDeleteRenderbuffers(1, &mDepthStencilBuffer));
-    }
-}
-
-void GlRenderTarget::init(GLint resolveId)
-{
-    if (mFbo != 0 || mWidth == 0 || mHeight == 0) return;
-
-    GL_CHECK(glGenFramebuffers(1, &mFbo));
-
-    GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, mFbo));
-
-    GL_CHECK(glGenRenderbuffers(1, &mColorBuffer));
-    GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, mColorBuffer));
-    GL_CHECK(glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_RGBA8, mWidth, mHeight));
-
-    GL_CHECK(glGenRenderbuffers(1, &mDepthStencilBuffer));
-
-    GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, mDepthStencilBuffer));
-
-    GL_CHECK(glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_DEPTH24_STENCIL8, mWidth, mHeight));
-
-    GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, 0));
-
-    GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, mColorBuffer));
-    GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, mDepthStencilBuffer));
-
-    // resolve target
-    GL_CHECK(glGenTextures(1, &mColorTex));
-
-    GL_CHECK(glBindTexture(GL_TEXTURE_2D, mColorTex));
-    GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, mWidth, mHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr));
-
-    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
-    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
-    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-
-    GL_CHECK(glBindTexture(GL_TEXTURE_2D, 0));
-
-    GL_CHECK(glGenFramebuffers(1, &mResolveFbo));
-    GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, mResolveFbo));
-    GL_CHECK(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, mColorTex, 0));
-
-    GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, resolveId));
-}
+#include "tvgGlGeometry.h"
 
 GlRenderPass::GlRenderPass(GlRenderTarget* fbo): mFbo(fbo), mTasks(), mDrawDepth(0) {}
 
@@ -108,4 +51,21 @@ GlRenderPass::~GlRenderPass()
 void GlRenderPass::addRenderTask(GlRenderTask* task)
 {
     mTasks.push(task);
+}
+
+void GlRenderPass::getMatrix(float *dst, const Matrix &matrix) const
+{
+    const auto& vp = getViewport();
+
+    Matrix postMatrix{};
+    mathIdentity(&postMatrix);
+    mathTranslate(&postMatrix, -vp.x, -vp.y);
+
+    auto m = postMatrix * matrix;
+
+    float modelMatrix[16];
+    GET_MATRIX44(m, modelMatrix);
+    MVP_MATRIX(vp.w, vp.h);
+
+    MULTIPLY_MATRIX(mvp, modelMatrix, dst);
 }
