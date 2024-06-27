b3385bfa482f438069c478e024027a2c8e3c6997
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-06-27 10:11:25 -0400

    spelling: propagator
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/loaders/lottie/tvgLottieBuilder.cpp
+++ src/loaders/lottie/tvgLottieBuilder.cpp
@@ -65,7 +65,7 @@ struct RenderContext
     float roundness = 0.0f;
     bool fragmenting = false;  //render context has been fragmented by filling
     bool reqFragment = false;  //requirment to fragment the render context
-    bool ownPropagator = true; //this rendering context shares the propergator
+    bool ownPropagator = true; //this rendering context shares the propagator
 
     RenderContext()
     {
