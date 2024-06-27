f49c79fc24081004ef7bb3a0d606cffac412e780
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-06-27 10:31:30 -0400

    spelling: constructed
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/loaders/lottie/jerryscript/jerry-core/ecma/base/ecma-helpers-string.cpp
+++ src/loaders/lottie/jerryscript/jerry-core/ecma/base/ecma-helpers-string.cpp
@@ -2186,7 +2186,7 @@ ecma_string_hash (const ecma_string_t *string_p) /**< ecma-string to calculate h
 /**
  * Create a substring from an ecma string
  *
- * @return a newly consturcted ecma string with its value initialized to a copy of a substring of the first argument
+ * @return a newly constructed ecma string with its value initialized to a copy of a substring of the first argument
  */
 ecma_string_t *
 ecma_string_substr (const ecma_string_t *string_p, /**< pointer to an ecma string */
