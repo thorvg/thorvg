516f7a0965f413143ca7ca31f0dc3608fe90e2e9
RuiwenTang <tangruiwen1989@gmail.com>
2024-07-03 20:53:31 +0800

    gl_engine: support simple hairline stroke rendering
    Reduce alpha if stroke width is too thin to mock hairline rendering

--- src/renderer/gl_engine/tvgGlRenderer.cpp
+++ src/renderer/gl_engine/tvgGlRenderer.cpp
@@ -581,6 +581,14 @@ void GlRenderer::drawPrimitive(GlShape& sdata, uint8_t r, uint8_t g, uint8_t b, 
 
     a = MULTIPLY(a, sdata.opacity);
 
+    if (flag & RenderUpdateFlag::Stroke) {
+        float strokeWidth = sdata.rshape->strokeWidth();
+        if (strokeWidth < MIN_GL_STROKE_WIDTH) {
+            float alpha = strokeWidth / MIN_GL_STROKE_WIDTH;
+            a = MULTIPLY(a, static_cast<uint8_t>(alpha * 255));
+        }
+    }
+
     // matrix buffer
     {
         auto matrix = sdata.geometry->getTransformMatrix();
@@ -710,6 +718,15 @@ void GlRenderer::drawPrimitive(GlShape& sdata, const Fill* fill, RenderUpdateFla
         });
     }
 
+    float alpha = 1.0f;
+
+    if (flag & RenderUpdateFlag::GradientStroke) {
+        float strokeWidth = sdata.rshape->strokeWidth();
+        if (strokeWidth < MIN_GL_STROKE_WIDTH) {
+            alpha = strokeWidth / MIN_GL_STROKE_WIDTH;
+        }
+    }
+
     // gradient block
     {
         GlBindingResource gradientBinding{};
@@ -730,7 +747,7 @@ void GlRenderer::drawPrimitive(GlShape& sdata, const Fill* fill, RenderUpdateFla
                 gradientBlock.stopColors[i * 4 + 0] = stops[i].r / 255.f;
                 gradientBlock.stopColors[i * 4 + 1] = stops[i].g / 255.f;
                 gradientBlock.stopColors[i * 4 + 2] = stops[i].b / 255.f;
-                gradientBlock.stopColors[i * 4 + 3] = stops[i].a / 255.f;
+                gradientBlock.stopColors[i * 4 + 3] = stops[i].a / 255.f * alpha;
                 nStops++;
             }
             gradientBlock.nStops[0] = nStops * 1.f;
@@ -766,7 +783,7 @@ void GlRenderer::drawPrimitive(GlShape& sdata, const Fill* fill, RenderUpdateFla
                 gradientBlock.stopColors[i * 4 + 0] = stops[i].r / 255.f;
                 gradientBlock.stopColors[i * 4 + 1] = stops[i].g / 255.f;
                 gradientBlock.stopColors[i * 4 + 2] = stops[i].b / 255.f;
-                gradientBlock.stopColors[i * 4 + 3] = stops[i].a / 255.f;
+                gradientBlock.stopColors[i * 4 + 3] = stops[i].a / 255.f * alpha;
                 nStops++;
             }
             gradientBlock.nStops[0] = nStops * 1.f;
