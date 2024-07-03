7f23b9507d535bd41b3b767c38b5af0916219df6
Hermet Park <hermet@lottiefiles.com>
2024-07-03 13:27:46 +0900

    common: Fix a compilation failure on VS 2017
    issue: https://github.com/thorvg/thorvg/issues/2512

--- src/renderer/wg_engine/tvgWgRenderData.cpp
+++ src/renderer/wg_engine/tvgWgRenderData.cpp
@@ -24,6 +24,7 @@
 #ifndef _TVG_WG_RENDER_DATA_H_
 #define _TVG_WG_RENDER_DATA_H_
 
+#include <algorithm>
 #include "tvgWgRenderData.h"
 
 //***********************************************************************
