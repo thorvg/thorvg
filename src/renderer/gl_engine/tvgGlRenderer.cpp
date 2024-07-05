eb3a53166cbb63a9741d32c3a9c7535ed04d26a3
Vincent Torri <vtorri@outlook.fr>
2024-07-05 07:03:22 +0200

    gl engine: add OpenGL support on Windows using the Glew library

--- src/renderer/gl_engine/tvgGlRenderer.cpp
+++ src/renderer/gl_engine/tvgGlRenderer.cpp
@@ -1290,6 +1290,10 @@ int GlRenderer::init(uint32_t threads)
 
 int32_t GlRenderer::init()
 {
+#ifdef _WIN32
+    GLenum err = glewInit();
+    assert (err == GLEW_OK);
+#endif
     return initEngineCnt;
 }
 
