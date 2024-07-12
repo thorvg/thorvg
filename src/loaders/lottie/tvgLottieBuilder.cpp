thorvg/thorvg

commit d95ec723354d532598b9a7192d959a35c2eeecb0

lottie: ++ scene composing optimization
retain reusable precomp clipper shapes during animation.

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -1000,18 +1000,21 @@ static void _updatePrecomp(LottieLayer* precomp, float frameNo, LottieExpression
         if (!child->matteSrc) _updateLayer(precomp, child, frameNo, exps);
     }
 
-    //clip the layer viewport
-    if (precomp->w > 0 && precomp->h > 0) {
-        auto clipper = Shape::gen().release();
-        clipper->appendRect(0, 0, static_cast<float>(precomp->w), static_cast<float>(precomp->h));
-        clipper->transform(precomp->cache.matrix);
-
-        //TODO: remove the intermediate scene....
-        auto cscene = Scene::gen();
-        cscene->composite(cast(clipper), CompositeMethod::ClipPath);
+    //TODO: remove the intermediate scene....
+    if (precomp->scene->composite(nullptr) != tvg::CompositeMethod::None) {
+        auto cscene = Scene::gen().release();
         cscene->push(cast(precomp->scene));
-        precomp->scene = cscene.release();
+        precomp->scene = cscene;
     }
+
+    //clip the layer viewport
+    if (!precomp->clipper) {
+        precomp->clipper = Shape::gen().release();
+        precomp->clipper->appendRect(0, 0, static_cast<float>(precomp->w), static_cast<float>(precomp->h));
+        PP(precomp->clipper)->ref();
+    }
+    precomp->clipper->transform(precomp->cache.matrix);
+    precomp->scene->composite(cast(precomp->clipper), CompositeMethod::ClipPath);
 }
 
 

src/loaders/lottie/tvgLottieModel.cpp
@@ -331,6 +331,8 @@ LottieLayer::~LottieLayer()
         delete(*m);
     }
 
+    if (clipper && PP(clipper)->unref() == 0) delete(clipper);
+
     delete(transform);
     free(name);
 }

src/loaders/lottie/tvgLottieModel.h
@@ -594,6 +594,8 @@ struct LottieLayer : LottieGroup
     RGB24 color;  //used by Solid layer
     LottieLayer* matteTarget = nullptr;
 
+    tvg::Shape* clipper = nullptr;
+
     float timeStretch = 1.0f;
     float w = 0.0f, h = 0.0f;
     float inFrame = 0.0f;

commit f2475efd573f5c72eb0cb818cbe5a58cf73dc2e9

lottie: fix crash caught by asan
Address sanitizer detected a bug introduced
in d95ec72, causing a crash during precomposition
update. Fixed.

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -1039,9 +1039,9 @@ static void _updatePrecomp(LottieLayer* precomp, float frameNo, LottieExpression
         precomp->clipper = Shape::gen().release();
         precomp->clipper->appendRect(0, 0, static_cast<float>(precomp->w), static_cast<float>(precomp->h));
         PP(precomp->clipper)->ref();
+        precomp->scene->composite(cast(precomp->clipper), CompositeMethod::ClipPath);
     }
     precomp->clipper->transform(precomp->cache.matrix);
-    precomp->scene->composite(cast(precomp->clipper), CompositeMethod::ClipPath);
 }
 
 