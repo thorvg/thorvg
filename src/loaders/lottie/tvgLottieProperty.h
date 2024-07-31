397e33b15879f61614eb63ac944e318a34243a7b
Hermet Park <hermet@lottiefiles.com>
2024-07-31 15:03:02 +0900

    lottie: code refactoring
    Access its type from a property instance,
    through coherent data structure.

--- src/loaders/lottie/tvgLottieProperty.h
+++ src/loaders/lottie/tvgLottieProperty.h
@@ -176,9 +176,11 @@ struct LottieVectorFrame
 struct LottieProperty
 {
     enum class Type : uint8_t { Point = 0, Float, Opacity, Color, PathSet, ColorStop, Position, TextDoc, Invalid };
-    virtual ~LottieProperty() {}
 
     LottieExpression* exp = nullptr;
+    Type type;
+
+    virtual ~LottieProperty() {}
 
     //TODO: Apply common bodies?
     virtual uint32_t frameCnt() = 0;
@@ -196,7 +198,6 @@ struct LottieExpression
     LottieLayer* layer;
     LottieObject* object;
     LottieProperty* property;
-    LottieProperty::Type type;
 
     bool enabled;
 
