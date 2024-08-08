67938180bcdbd723abc7cd3d3169bf25a834ceb3
Hermet Park <hermet@lottiefiles.com>
2024-08-08 14:13:58 +0900

    renderer: rectified the bounds() behavior
    Text fill is broken by 43c87b4eb5cef0fbaa3a4ab8084790b5b5c19735

--- src/renderer/tvgPaint.cpp
+++ src/renderer/tvgPaint.cpp
@@ -402,7 +402,7 @@ Matrix Paint::transform() noexcept
 
 Result Paint::bounds(float* x, float* y, float* w, float* h, bool transformed) const noexcept
 {
-    if (pImpl->bounds(x, y, w, h, transformed, true, true)) return Result::Success;
+    if (pImpl->bounds(x, y, w, h, transformed, true, transformed)) return Result::Success;
     return Result::InsufficientCondition;
 }
 
