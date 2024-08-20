d704a2503a92f18baca2068b3242a35ae176e482
Jinny You <jinny@lottiefiles.com>
2024-08-20 16:58:11 +0900

    bindings/wasm: Support WebGPU
    Updated WASM binding to bring WebGPU on the web player.
    
    binding will generate a WASM binary, which can render animation through both SW and WG raster engine.
    
    A raster engine will be chosen by parameter `engine`, when initializing.

--- wasm_build.sh
+++ wasm_build.sh
@@ -2,10 +2,26 @@
 
 #https://github.com/thorvg/thorvg/wiki/ThorVG-Viewer-Development-Guide
 
-if [ ! -d "./build_wasm" ]; then
-    sed "s|EMSDK:|$1|g" ./cross/wasm_x86_i686.txt > /tmp/.wasm_cross.txt
-    meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dthreads=false -Dbindings="wasm_beta" --cross-file /tmp/.wasm_cross.txt build_wasm
+BACKEND="$1"
+EMSDK="$2"
+
+if [ -z "$2" ]; then
+  BACKEND="all"
+  EMSDK="$1"
 fi
 
-ninja -C build_wasm/
-ls -lrt build_wasm/src/bindings/wasm/thorvg-wasm.*
+if [ ! -d "./build_wasm32_$BACKEND" ]; then
+    if [[ "$BACKEND" == "wg" ]]; then
+      sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_wg.txt > /tmp/.wasm_cross.txt
+      meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dthreads=false -Dbindings="wasm_beta" -Dengines="wg_beta" --cross-file /tmp/.wasm_cross.txt build_wasm32_wg
+    elif [[ "$BACKEND" == "sw" ]]; then
+      sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_sw.txt > /tmp/.wasm_cross.txt
+      meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dthreads=false -Dbindings="wasm_beta" --cross-file /tmp/.wasm_cross.txt build_wasm32_sw
+    else
+      sed "s|EMSDK:|$EMSDK|g" ./cross/wasm32_wg.txt > /tmp/.wasm_cross.txt
+      meson -Db_lto=true -Ddefault_library=static -Dstatic=true -Dloaders="all" -Dsavers="all" -Dthreads=false -Dbindings="wasm_beta" -Dengines="wg_beta, sw" --cross-file /tmp/.wasm_cross.txt build_wasm32_all
+    fi
+fi
+
+ninja -C build_wasm32_$BACKEND/
+ls -lrt build_wasm32_$BACKEND/src/bindings/wasm/*.{js,wasm}
