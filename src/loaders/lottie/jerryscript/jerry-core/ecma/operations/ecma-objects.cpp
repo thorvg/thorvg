df2d3a95f8e8f98682462494c26d8cabce68fe44
Hermet Park <hermet@lottiefiles.com>
2024-08-23 14:50:23 +0900

    lottie/jerryscript: ++build reliance

--- src/loaders/lottie/jerryscript/jerry-core/ecma/operations/ecma-objects.cpp
+++ src/loaders/lottie/jerryscript/jerry-core/ecma/operations/ecma-objects.cpp
@@ -2701,7 +2701,10 @@ ecma_object_check_class_name_is_object (ecma_object_t *obj_p) /**< object */
           || ecma_builtin_is (obj_p, ECMA_BUILTIN_ID_URI_ERROR_PROTOTYPE)
           || ecma_builtin_is (obj_p, ECMA_BUILTIN_ID_ERROR_PROTOTYPE)
 #endif
+
+#if JERRY_BUILTIN_REGEXP
           || ecma_builtin_is (obj_p, ECMA_BUILTIN_ID_REGEXP_PROTOTYPE)
+#endif /* JERRY_BUILTIN_REGEXP */
           || ecma_builtin_is (obj_p, ECMA_BUILTIN_ID_SYMBOL_PROTOTYPE)
           || ecma_builtin_is (obj_p, ECMA_BUILTIN_ID_ASYNC_FUNCTION_PROTOTYPE)
           || ecma_builtin_is (obj_p, ECMA_BUILTIN_ID_PROMISE_PROTOTYPE)
