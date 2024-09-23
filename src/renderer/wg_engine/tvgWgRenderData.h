0d2f5c7d0989900e1e9b6a187ca5d30e69ec5f96
Sergii Liebodkin <s.lebedkin@gmail.com>
2024-09-23 18:22:30 +0000

    wg_engine: geometry generating optimization
    Streaming model for massive vertex and index creations: minimize memory allocations, range checks and other conditions
    Reduce number of segments length calculations (sqrt) and bbox (min and max).
    Update distances and bboxes on a whole buffer and only if necessary. For shapes without strokes compute distances not necessary at all. bbox can be updated only on the final stage of geometry workflow, but not on the each stage.
    Using stack memory instead of heap. its more cache friendly and did not fragment memory, faster memory allocations (weak place of realization)
    Using cache for points distances and whole path length. Updates only if necessary
    Validation of geometry consistency executes only on the final stage of path life cicle. It more friendly for data streaming: no any conditions and branches.
    Using binary search for strokes trimming
    Pre-cached circles geometry for caps and joints
    Refactored strokes elements generation functions. Code is more readable and modifiable in general. Can be easily fixed if some geometry issues will be finded

--- src/renderer/wg_engine/tvgWgRenderData.h
+++ src/renderer/wg_engine/tvgWgRenderData.h
@@ -37,9 +37,9 @@ struct WgMeshData {
     void drawFan(WgContext& context, WGPURenderPassEncoder renderPassEncoder);
     void drawImage(WgContext& context, WGPURenderPassEncoder renderPassEncoder);
 
-    void update(WgContext& context, const WgPolyline* polyline);
-    void update(WgContext& context, const WgGeometryData* geometryData);
-    void update(WgContext& context, const WgPoint pmin, const WgPoint pmax);
+    void update(WgContext& context, const WgVertexBuffer& vertexBuffer);
+    void update(WgContext& context, const WgVertexBufferInd& vertexBufferInd);
+    void update(WgContext& context, const Point pmin, const Point pmax);
     void release(WgContext& context);
 };
 
@@ -58,9 +58,9 @@ struct WgMeshDataGroup {
 
     Array<WgMeshData*> meshes{};
     
-    void append(WgContext& context, const WgPolyline* polyline);
-    void append(WgContext& context, const WgGeometryData* geometryData);
-    void append(WgContext& context, const WgPoint pmin, const WgPoint pmax);
+    void append(WgContext& context, const WgVertexBuffer& vertexBuffer);
+    void append(WgContext& context, const WgVertexBufferInd& vertexBufferInd);
+    void append(WgContext& context, const Point pmin, const Point pmax);
     void release(WgContext& context);
 };
 
@@ -119,17 +119,17 @@ struct WgRenderDataShape: public WgRenderDataPaint
     WgMeshData meshDataBBox{};
     WgMeshDataGroup meshGroupStrokes{};
     WgMeshDataGroup meshGroupStrokesBBox{};
-    WgPoint pMin{};
-    WgPoint pMax{};
+    Point pMin{};
+    Point pMax{};
     bool strokeFirst{};
     FillRule fillRule{};
 
-    void updateBBox(WgPoint pmin, WgPoint pmax);
-    void updateAABB(const Matrix& rt);
+    void appendShape(WgContext context, const WgVertexBuffer& vertexBuffer);
+    void appendStroke(WgContext context, const WgVertexBufferInd& vertexBufferInd);
+    void updateBBox(Point pmin, Point pmax);
+    void updateAABB(const Matrix& tr);
     void updateMeshes(WgContext& context, const RenderShape& rshape, const Matrix& tr);
-    void updateShapes(WgContext& context, const WgPolyline* polyline);
-    void updateStrokesList(WgContext& context, Array<WgPolyline*> polylines, const RenderStroke* rstroke, float scale, float totalLen, float trimBegin, float trimEnd);
-    void updateStrokes(WgContext& context, const WgPolyline* polyline, const RenderStroke* rstroke, float scale, float trimBegin, float trimEnd);
+    void proceedStrokes(WgContext context, const RenderStroke* rstroke, float tbeg, float tend, const WgVertexBuffer& buff);
     void releaseMeshes(WgContext& context);
     void release(WgContext& context) override;
     Type type() override { return Type::Shape; };
