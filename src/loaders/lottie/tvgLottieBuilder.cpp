thorvg/thorvg

commit db800c8d4500cd379238c4ec1c9f6c59e70fe908

lottie: fixed a memory leak
A regression bug by efe7440fa0e38f00e1235fb57f56717914e6000c

To address this, removed the non-essential image pooling mechanism.

issue: https://github.com/thorvg/thorvg/issues/2959

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -980,7 +980,7 @@ void LottieBuilder::updateSolid(LottieLayer* layer)
 void LottieBuilder::updateImage(LottieGroup* layer)
 {
     auto image = static_cast<LottieImage*>(layer->children.first());
-    layer->scene->push(image->pooling(true));
+    layer->scene->push(image->picture);
 }
 
 

src/loaders/lottie/tvgLottieModel.cpp
@@ -159,7 +159,10 @@ void LottieImage::prepare()
 {
     LottieObject::type = LottieObject::Image;
 
-    auto picture = Picture::gen();
+    if (!picture) {
+        picture = Picture::gen();
+        PP(picture)->ref();
+    }
 
     //force to load a picture on the same thread
     TaskScheduler::async(false);
@@ -170,10 +173,6 @@ void LottieImage::prepare()
     TaskScheduler::async(true);
 
     picture->size(data.width, data.height);
-    PP(picture)->ref();
-
-    pooler.reset();
-    pooler.push(picture);
 }
 
 

src/loaders/lottie/tvgLottieModel.h
@@ -672,9 +672,15 @@ struct LottieGradientStroke : LottieGradient, LottieStroke
 };
 
 
-struct LottieImage : LottieObject, LottieRenderPooler<tvg::Picture>
+struct LottieImage : LottieObject
 {
     LottieBitmap data;
+    Picture* picture = nullptr;
+
+    ~LottieImage()
+    {
+        if (PP(picture)->unref() == 0) delete(picture);
+    }
 
     void override(LottieProperty* prop, bool byDefault = false) override
     {

commit 1779689f03f57cccde973ec72cf2525ad91c789e

lottie: Fixed a regression bug
Reverted commit db800c8d4500cd379238c4ec1c9f6c59e70fe908,
which introduced another regression that omitted support
for multiple image asset references.

Updated the data of active images in the pooler
within the override() function to resolve the issue.

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -978,7 +978,7 @@ void LottieBuilder::updateSolid(LottieLayer* layer)
 void LottieBuilder::updateImage(LottieGroup* layer)
 {
     auto image = static_cast<LottieImage*>(layer->children.first());
-    layer->scene->push(tvg::cast(image->picture));
+    layer->scene->push(tvg::cast(image->pooling(true)));
 }
 
 

src/loaders/lottie/tvgLottieModel.cpp
@@ -159,10 +159,7 @@ void LottieImage::prepare()
 {
     LottieObject::type = LottieObject::Image;
 
-    if (!picture) {
-        picture = Picture::gen().release();
-        PP(picture)->ref();
-    }
+    auto picture = Picture::gen().release();
 
     //force to load a picture on the same thread
     TaskScheduler::async(false);
@@ -173,6 +170,22 @@ void LottieImage::prepare()
     TaskScheduler::async(true);
 
     picture->size(data.width, data.height);
+    PP(picture)->ref();
+
+    pooler.push(picture);
+}
+
+
+void LottieImage::update()
+{
+    //Update the picture data
+    TaskScheduler::async(false);
+    for (auto p = pooler.begin(); p < pooler.end(); ++p) {
+        if (data.size > 0) (*p)->load((const char*)data.b64Data, data.size, data.mimeType, false);
+        else (*p)->load(data.path);
+        (*p)->size(data.width, data.height);
+    }
+    TaskScheduler::async(true);
 }
 
 

src/loaders/lottie/tvgLottieModel.h
@@ -656,24 +656,19 @@ struct LottieGradientStroke : LottieGradient, LottieStroke
 };
 
 
-struct LottieImage : LottieObject
+struct LottieImage : LottieObject, LottieRenderPooler<tvg::Picture>
 {
     LottieBitmap data;
-    Picture* picture = nullptr;
-
-    ~LottieImage()
-    {
-        if (PP(picture)->unref() == 0) delete(picture);
-    }
 
     void override(LottieProperty* prop, bool byDefault = false) override
     {
         if (byDefault) data.release();
         data = *static_cast<LottieBitmap*>(prop);
-        prepare();
+        update();
     }
 
     void prepare();
+    void update();
 };
 
 