b5d81be33969802983e7d7a27f25a441c6da3f81
Mira Grudzinska <mira@lottiefiles.com>
2024-11-21 13:58:20 +0100

    tests: remove capi tests
    @Issue: https://github.com/thorvg/thorvg/issues/2978

--- test/capi/capiMain.cpp
+++
@@ -1,5 +0,0 @@
-// The only purpose of this file is to DEFINE the catch config so it can include main()
-
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "../catch.hpp"
