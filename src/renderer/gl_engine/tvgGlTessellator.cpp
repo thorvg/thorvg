6fcb9f4473f0835def28b911874d234a1d4abdc1
Marcin Baszczewski <marcin@baszczewski.pl>
2024-12-20 10:36:18 +0100

    gl_engine: Fix for rendering short paths
    Ensure they do not terminate prematurely for paths
    with a step of 0 or exceptionally small values
    when a valid stroke-width is present.
    
    In my opinion, the optimal approach was to separate vertex generation
    into dedicated methods: strokeRoundPoint and strokeSquarePoint.
    
    My update supports two different stroke-cap styles.
    I have also tested it with various files (JSON, SVG)
    as well as a small example application similar
    to the one included in the previous pull request (#3066).
    
    issue: https://github.com/thorvg/thorvg/issues/3065

--- src/renderer/gl_engine/tvgGlTessellator.cpp
+++ src/renderer/gl_engine/tvgGlTessellator.cpp
@@ -1670,10 +1670,20 @@ void Stroker::doStroke(const PathCommand *cmds, uint32_t cmd_count, const Point 
                 pts++;
             } break;
             case PathCommand::LineTo: {
+                // check if the line is a point
+                if (mStrokeState.prevPt == *pts) {
+                    if (mStrokeCap == StrokeCap::Square) strokeSquarePoint(*pts);
+                    else if (mStrokeCap == StrokeCap::Round) strokeRoundPoint(*pts);
+                }
                 this->strokeLineTo(*pts);
                 pts++;
             } break;
             case PathCommand::CubicTo: {
+                // check if the curve is a point
+                if (mStrokeState.prevPt == pts[2]) {
+                    if (mStrokeCap == StrokeCap::Square) strokeSquarePoint(pts[2]);
+                    else if (mStrokeCap == StrokeCap::Round) strokeRoundPoint(pts[2]);
+                }
                 this->strokeCubicTo(pts[0], pts[1], pts[2]);
                 pts += 3;
             } break;
@@ -1887,6 +1897,33 @@ void Stroker::strokeRound(const Point &prev, const Point& curr, const Point& cen
 }
 
 
+void Stroker::strokeRoundPoint(const Point &p)
+{
+    // Fixme: just use bezier curve to calculate step count
+    auto count = _bezierCurveCount(_bezFromArc(p, p, strokeRadius())) * 2;
+    auto c = _pushVertex(mResGlPoints, p.x, p.y);
+    auto step = 2 * M_PI / (count - 1);
+
+    for (uint32_t i = 1; i <= static_cast<uint32_t>(count); i++) {
+        float angle = i * step;
+        Point dir = {cos(angle), sin(angle)};
+        Point out = p + dir * strokeRadius();
+        auto oi = _pushVertex(mResGlPoints, out.x, out.y);
+
+        if (oi > 1) {
+            mResIndices->push(c);
+            mResIndices->push(oi);
+            mResIndices->push(oi - 1);
+        }
+    }
+
+    mLeftTop.x = std::min(mLeftTop.x, p.x - strokeRadius());
+    mLeftTop.y = std::min(mLeftTop.y, p.y - strokeRadius());
+    mRightBottom.x = std::max(mRightBottom.x, p.x + strokeRadius());
+    mRightBottom.y = std::max(mRightBottom.y, p.y + strokeRadius());
+}
+
+
 void Stroker::strokeMiter(const Point& prev, const Point& curr, const Point& center)
 {
     auto pp1 = prev - center;
@@ -1955,6 +1992,36 @@ void Stroker::strokeSquare(const Point& p, const Point& outDir)
     mResIndices->push(ci);
     mResIndices->push(bi);
     mResIndices->push(di);
+
+    mLeftTop.x = std::min(mLeftTop.x, std::min(std::min(a.x, b.x), std::min(c.x, d.x)));
+    mLeftTop.y = std::min(mLeftTop.y, std::min(std::min(a.y, b.y), std::min(c.y, d.y)));
+    mRightBottom.x = std::max(mRightBottom.x, std::max(std::max(a.x, b.x), std::max(c.x, d.x)));
+    mRightBottom.y = std::max(mRightBottom.y, std::max(std::max(a.y, b.y), std::max(c.y, d.y)));
+}
+
+
+void Stroker::strokeSquarePoint(const Point& p)
+{
+    auto offsetX = Point{strokeRadius(), 0.0f};
+    auto offsetY = Point{0.0f, strokeRadius()};
+
+    auto a = p + offsetX + offsetY;
+    auto b = p - offsetX + offsetY;
+    auto c = p - offsetX - offsetY;
+    auto d = p + offsetX - offsetY;
+
+    auto ai = _pushVertex(mResGlPoints, a.x, a.y);
+    auto bi = _pushVertex(mResGlPoints, b.x, b.y);
+    auto ci = _pushVertex(mResGlPoints, c.x, c.y);
+    auto di = _pushVertex(mResGlPoints, d.x, d.y);
+
+    mResIndices->push(ai);
+    mResIndices->push(bi);
+    mResIndices->push(ci);
+
+    mResIndices->push(ci);
+    mResIndices->push(di);
+    mResIndices->push(ai);
 
     mLeftTop.x = std::min(mLeftTop.x, std::min(std::min(a.x, b.x), std::min(c.x, d.x)));
     mLeftTop.y = std::min(mLeftTop.y, std::min(std::min(a.y, b.y), std::min(c.y, d.y)));
