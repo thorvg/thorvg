thorvg/thorvg

commit 1806b329718589a36b606cac5760e41dacb586f6

common: optimization pImpl data structures
ThorVG pImpl idiom caused internal data to be scattered
across hierarchical classes. This refactoring consolidates
the data by inheriting pImpl internally, reducing memory
allocation counts and eliminating unnecessary strategy methods.

src/renderer/tvgPaint.h
@@ -26,6 +26,8 @@
 #include "tvgRender.h"
 #include "tvgMath.h"
 
+#define PAINT(A) PIMPL(A, Paint)
+
 namespace tvg
 {
     enum ContextFlag : uint8_t {Default = 0, FastTrack = 1};
@@ -83,7 +85,7 @@ namespace tvg
             reset();
         }
 
-        ~Impl()
+        virtual ~Impl()
         {
             if (maskData) {
                 maskData->target->unref();
@@ -91,6 +93,32 @@ namespace tvg
             }
             if (clipper) clipper->unref();
             if (renderer && (renderer->unref() == 0)) delete(renderer);
+        }
+
+        uint8_t ref()
+        {
+            if (refCnt == UINT8_MAX) TVGERR("RENDERER", "Reference Count Overflow!");
+            else ++refCnt;
+            return refCnt;
+        }
+
+        uint8_t unref(bool free)
+        {
+            if (refCnt > 0) --refCnt;
+            else TVGERR("RENDERER", "Corrupted Reference Count!");
+
+            if (free && refCnt == 0) {
+                //TODO: use the global dismiss function?
+                delete(this);
+                return 0;
+            }
+
+            return refCnt;
+        }
+
+        void update(RenderUpdateFlag flag)
+        {
+            renderFlag |= flag;
         }
 
         bool transform(const Matrix& m)
@@ -143,16 +171,87 @@ namespace tvg
             return true;
         }
 
+        MaskMethod mask(const Paint** target) const
+        {
+            if (maskData) {
+                if (target) *target = maskData->target;
+                return maskData->method;
+            } else {
+                if (target) *target = nullptr;
+                return MaskMethod::None;
+            }
+        }
+
+        void reset()
+        {
+            if (clipper) {
+                clipper->unref();
+                clipper = nullptr;
+            }
+
+            if (maskData) {
+                maskData->target->unref();
+                free(maskData);
+                maskData = nullptr;
+            }
+
+            tvg::identity(&tr.m);
+            tr.degree = 0.0f;
+            tr.scale = 1.0f;
+            tr.overriding = false;
+
+            blendMethod = BlendMethod::Normal;
+            renderFlag = RenderUpdateFlag::None;
+            ctxFlag = ContextFlag::Default;
+            opacity = 255;
+            paint->id = 0;
+        }
+
+        bool rotate(float degree)
+        {
+            if (tr.overriding) return false;
+            if (tvg::equal(degree, tr.degree)) return true;
+            tr.degree = degree;
+            renderFlag |= RenderUpdateFlag::Transform;
+
+            return true;
+        }
+
+        bool scale(float factor)
+        {
+            if (tr.overriding) return false;
+            if (tvg::equal(factor, tr.scale)) return true;
+            tr.scale = factor;
+            renderFlag |= RenderUpdateFlag::Transform;
+
+            return true;
+        }
+
+        bool translate(float x, float y)
+        {
+            if (tr.overriding) return false;
+            if (tvg::equal(x, tr.m.e13) && tvg::equal(y, tr.m.e23)) return true;
+            tr.m.e13 = x;
+            tr.m.e23 = y;
+            renderFlag |= RenderUpdateFlag::Transform;
+
+            return true;
+        }
+
+        void blend(BlendMethod method)
+        {
+            if (blendMethod != method) {
+                blendMethod = method;
+                renderFlag |= RenderUpdateFlag::Blend;
+            }
+        }
+
         RenderRegion bounds(RenderMethod* renderer) const;
         Iterator* iterator();
-        bool rotate(float degree);
-        bool scale(float factor);
-        bool translate(float x, float y);
         bool bounds(float* x, float* y, float* w, float* h, bool transformed, bool stroking, bool origin = false);
         RenderData update(RenderMethod* renderer, const Matrix& pm, Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag pFlag, bool clipper = false);
         bool render(RenderMethod* renderer);
         Paint* duplicate(Paint* ret = nullptr);
-        void reset();
     };
 }
 

inc/thorvg.h
@@ -43,11 +43,15 @@
 
 #define _TVG_DECLARE_PRIVATE(A) \
     struct Impl; \
-    Impl* pImpl; \
 protected: \
     A(const A&) = delete; \
     const A& operator=(const A&) = delete; \
     A()
+
+#define _TVG_DECLARE_PRIVATE_BASE(A) \
+    _TVG_DECLARE_PRIVATE(A); \
+public: \
+    Impl* pImpl
 
 #define _TVG_DISABLE_CTOR(A) \
     A() = delete; \
@@ -515,7 +519,7 @@ public:
      */
     uint32_t id = 0;
 
-    _TVG_DECLARE_PRIVATE(Paint);
+    _TVG_DECLARE_PRIVATE_BASE(Paint);
 };
 
 
@@ -616,7 +620,7 @@ public:
      */
     virtual Type type() const noexcept = 0;
 
-    _TVG_DECLARE_PRIVATE(Fill);
+    _TVG_DECLARE_PRIVATE_BASE(Fill);
 };
 
 
@@ -633,7 +637,6 @@ public:
 class TVG_API Canvas
 {
 public:
-    Canvas(RenderMethod*);
     virtual ~Canvas();
 
     /**
@@ -749,7 +752,7 @@ public:
      */
     Result sync() noexcept;
 
-    _TVG_DECLARE_PRIVATE(Canvas);
+    _TVG_DECLARE_PRIVATE_BASE(Canvas);
 };
 
 
@@ -764,8 +767,6 @@ public:
 class TVG_API LinearGradient final : public Fill
 {
 public:
-    ~LinearGradient();
-
     /**
      * @brief Sets the linear gradient bounds.
      *
@@ -828,8 +829,6 @@ public:
 class TVG_API RadialGradient final : public Fill
 {
 public:
-    ~RadialGradient();
-
     /**
      * @brief Sets the radial gradient attributes.
      *
@@ -906,8 +905,6 @@ public:
 class TVG_API Shape final : public Paint
 {
 public:
-    ~Shape();
-
     /**
      * @brief Resets the shape path.
      *
@@ -1287,8 +1284,6 @@ public:
 class TVG_API Picture final : public Paint
 {
 public:
-    ~Picture();
-
     /**
      * @brief Loads a picture data directly from a file.
      *
@@ -1420,8 +1415,6 @@ public:
 class TVG_API Scene final : public Paint
 {
 public:
-    ~Scene();
-
     /**
      * @brief Inserts a paint object to the scene.
      *
@@ -1519,8 +1512,6 @@ public:
 class TVG_API Text final : public Paint
 {
 public:
-    ~Text();
-
     /**
      * @brief Sets the font properties for the text.
      *
@@ -1902,7 +1893,7 @@ public:
 class TVG_API Animation
 {
 public:
-    ~Animation();
+    virtual ~Animation();
 
     /**
      * @brief Specifies the current frame in the animation.
@@ -2012,7 +2003,7 @@ public:
      */
     static Animation* gen() noexcept;
 
-    _TVG_DECLARE_PRIVATE(Animation);
+    _TVG_DECLARE_PRIVATE_BASE(Animation);
 };
 
 
@@ -2115,7 +2106,7 @@ public:
      */
     static Saver* gen() noexcept;
 
-    _TVG_DECLARE_PRIVATE(Saver);
+    _TVG_DECLARE_PRIVATE_BASE(Saver);
 };
 
 
@@ -2171,7 +2162,7 @@ public:
      */
     static Accessor* gen() noexcept;
 
-    _TVG_DECLARE_PRIVATE(Accessor);
+    _TVG_DECLARE_PRIVATE_BASE(Accessor);
 };
 
 /** @}*/

src/loaders/external_jpg/tvgJpgLoader.cpp
@@ -36,7 +36,6 @@ void JpgLoader::clear()
     freeData = false;
 }
 
-
 /************************************************************************/
 /* External Class Implementation                                        */
 /************************************************************************/

src/loaders/external_png/tvgPngLoader.cpp
@@ -33,7 +33,6 @@ void PngLoader::clear()
     image = nullptr;
 }
 
-
 /************************************************************************/
 /* External Class Implementation                                        */
 /************************************************************************/

src/loaders/external_webp/tvgWebpLoader.cpp
@@ -30,7 +30,6 @@
 /* Internal Class Implementation                                        */
 /************************************************************************/
 
-
 void WebpLoader::run(unsigned tid)
 {
     //TODO: acquire the current colorspace format & pre-multiplied alpha image.

src/loaders/lottie/thorvg_lottie.h
@@ -20,8 +20,6 @@ namespace tvg
 class TVG_API LottieAnimation final : public Animation
 {
 public:
-    ~LottieAnimation();
-
     /**
      * @brief Override Lottie properties using slot data.
      *
@@ -87,6 +85,8 @@ public:
      * @since 0.15
      */
     static LottieAnimation* gen() noexcept;
+
+    _TVG_DECLARE_PRIVATE(LottieAnimation);
 };
 
 } //namespace

src/loaders/lottie/tvgLottieAnimation.cpp
@@ -26,24 +26,16 @@
 #include "tvgAnimation.h"
 
 
-/************************************************************************/
-/* Internal Class Implementation                                        */
-/************************************************************************/
-
-/************************************************************************/
-/* External Class Implementation                                        */
-/************************************************************************/
-
-LottieAnimation::~LottieAnimation()
+LottieAnimation::LottieAnimation()
 {
 }
 
 
 Result LottieAnimation::override(const char* slot) noexcept
 {
-    if (!pImpl->picture->pImpl->loader) return Result::InsufficientCondition;
+    if (!PICTURE(pImpl->picture)->loader) return Result::InsufficientCondition;
 
-    if (static_cast<LottieLoader*>(pImpl->picture->pImpl->loader)->override(slot)) return Result::Success;
+    if (static_cast<LottieLoader*>(PICTURE(pImpl->picture)->loader)->override(slot)) return Result::Success;
 
     return Result::InvalidArguments;
 }
@@ -51,7 +43,7 @@ Result LottieAnimation::override(const char* slot) noexcept
 
 Result LottieAnimation::segment(const char* marker) noexcept
 {
-    auto loader = pImpl->picture->pImpl->loader;
+    auto loader = PICTURE(pImpl->picture)->loader;
     if (!loader) return Result::InsufficientCondition;
 
     if (!marker) {
@@ -68,7 +60,7 @@ Result LottieAnimation::segment(const char* marker) noexcept
 
 uint32_t LottieAnimation::markersCnt() noexcept
 {
-    auto loader = pImpl->picture->pImpl->loader;
+    auto loader = PICTURE(pImpl->picture)->loader;
     if (!loader) return 0;
     return static_cast<LottieLoader*>(loader)->markersCnt();
 }
@@ -76,7 +68,7 @@ uint32_t LottieAnimation::markersCnt() noexcept
 
 const char* LottieAnimation::marker(uint32_t idx) noexcept
 {
-    auto loader = pImpl->picture->pImpl->loader;
+    auto loader = PICTURE(pImpl->picture)->loader;
     if (!loader) return nullptr;
     return static_cast<LottieLoader*>(loader)->markers(idx);
 }

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -189,10 +189,10 @@ void LottieBuilder::updateTransform(LottieGroup* parent, LottieObject** child, f
     if (!_updateTransform(transform, frameNo, false, matrix, opacity, exps)) return;
 
     ctx->propagator->transform(ctx->propagator->transform() * matrix);
-    ctx->propagator->opacity(MULTIPLY(opacity, PP(ctx->propagator)->opacity));
+    ctx->propagator->opacity(MULTIPLY(opacity, PAINT(ctx->propagator)->opacity));
 
     //FIXME: preserve the stroke width. too workaround, need a better design.
-    if (P(ctx->propagator)->rs.strokeWidth() > 0.0f) {
+    if (SHAPE(ctx->propagator)->rs.strokeWidth() > 0.0f) {
         auto denominator = sqrtf(matrix.e11 * matrix.e11 + matrix.e12 * matrix.e12);
         if (denominator > 1.0f) ctx->propagator->strokeWidth(ctx->propagator->strokeWidth() / denominator);
     }
@@ -213,7 +213,7 @@ void LottieBuilder::updateGroup(LottieGroup* parent, LottieObject** child, float
     if (group->mergeable()) _draw(parent, nullptr, ctx);
 
     Inlist<RenderContext> contexts;
-    auto propagator = group->mergeable() ? ctx->propagator : static_cast<Shape*>(PP(ctx->propagator)->duplicate(group->pooling()));
+    auto propagator = group->mergeable() ? ctx->propagator : static_cast<Shape*>(PAINT(ctx->propagator)->duplicate(group->pooling()));
     contexts.back(new RenderContext(*ctx, propagator, group->mergeable()));
 
     updateChildren(group, frameNo, contexts);
@@ -243,7 +243,7 @@ static bool _fragmented(LottieGroup* parent, LottieObject** child, Inlist<Render
     if (!ctx->reqFragment) return false;
     if (ctx->fragmenting) return true;
 
-    contexts.back(new RenderContext(*ctx, static_cast<Shape*>(PP(ctx->propagator)->duplicate(parent->pooling()))));
+    contexts.back(new RenderContext(*ctx, static_cast<Shape*>(PAINT(ctx->propagator)->duplicate(parent->pooling()))));
     auto fragment = contexts.tail;
     fragment->begin = child - 1;
     ctx->fragmenting = true;
@@ -313,7 +313,7 @@ static bool _draw(LottieGroup* parent, LottieShape* shape, RenderContext* ctx)
 
     if (shape) {
         ctx->merging = shape->pooling();
-        PP(ctx->propagator)->duplicate(ctx->merging);
+        PAINT(ctx->propagator)->duplicate(ctx->merging);
     } else {
         ctx->merging = static_cast<Shape*>(ctx->propagator->duplicate());
     }
@@ -338,7 +338,7 @@ static void _repeat(LottieGroup* parent, Shape* path, RenderContext* ctx)
 
             for (auto propagator = propagators.begin(); propagator < propagators.end(); ++propagator) {
                 auto shape = static_cast<Shape*>((*propagator)->duplicate());
-                P(shape)->rs.path = P(path)->rs.path;
+                SHAPE(shape)->rs.path = SHAPE(path)->rs.path;
 
                 auto opacity = repeater->interpOpacity ? lerp<uint8_t>(repeater->startOpacity, repeater->endOpacity, static_cast<float>(i + 1) / repeater->cnt) : repeater->startOpacity;
                 shape->opacity(opacity);
@@ -405,7 +405,7 @@ static void _appendRect(Shape* shape, float x, float y, float w, float h, float 
             }
         }
 
-        if (offsetPath) offsetPath->modifyRect(commands, 5, points, 4, P(shape)->rs.path.cmds, P(shape)->rs.path.pts);
+        if (offsetPath) offsetPath->modifyRect(commands, 5, points, 4, SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts);
         else shape->appendPath(commands, 5, points, 4);
     //round rect
     } else {
@@ -458,7 +458,7 @@ static void _appendRect(Shape* shape, float x, float y, float w, float h, float 
             }
         }
 
-        if (offsetPath) offsetPath->modifyRect(commands, cmdCnt, points, ptsCnt, P(shape)->rs.path.cmds, P(shape)->rs.path.pts);
+        if (offsetPath) offsetPath->modifyRect(commands, cmdCnt, points, ptsCnt, SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts);
         else shape->appendPath(commands, cmdCnt, points, ptsCnt);
     }
 }
@@ -557,12 +557,12 @@ void LottieBuilder::updatePath(LottieGroup* parent, LottieObject** child, float 
     if (!ctx->repeaters.empty()) {
         auto shape = path->pooling();
         shape->reset();
-        path->pathset(frameNo, P(shape)->rs.path.cmds, P(shape)->rs.path.pts, ctx->transform, ctx->roundness, ctx->offsetPath, exps);
+        path->pathset(frameNo, SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, ctx->transform, ctx->roundness, ctx->offsetPath, exps);
         _repeat(parent, shape, ctx);
     } else {
         _draw(parent, path, ctx);
-        if (path->pathset(frameNo, P(ctx->merging)->rs.path.cmds, P(ctx->merging)->rs.path.pts, ctx->transform, ctx->roundness, ctx->offsetPath, exps)) {
-            P(ctx->merging)->update(RenderUpdateFlag::Path);
+        if (path->pathset(frameNo, SHAPE(ctx->merging)->rs.path.cmds, SHAPE(ctx->merging)->rs.path.pts, ctx->transform, ctx->roundness, ctx->offsetPath, exps)) {
+            PAINT(ctx->merging)->update(RenderUpdateFlag::Path);
         }
     }
 }
@@ -615,11 +615,11 @@ static void _updateStar(TVG_UNUSED LottieGroup* parent, LottiePolyStar* star, Ma
     }
 
     if (tvg::zero(innerRoundness) && tvg::zero(outerRoundness)) {
-        P(shape)->rs.path.pts.reserve(numPoints + 2);
-        P(shape)->rs.path.cmds.reserve(numPoints + 3);
+        SHAPE(shape)->rs.path.pts.reserve(numPoints + 2);
+        SHAPE(shape)->rs.path.cmds.reserve(numPoints + 3);
     } else {
-        P(shape)->rs.path.pts.reserve(numPoints * 3 + 2);
-        P(shape)->rs.path.cmds.reserve(numPoints + 3);
+        SHAPE(shape)->rs.path.pts.reserve(numPoints * 3 + 2);
+        SHAPE(shape)->rs.path.cmds.reserve(numPoints + 3);
         hasRoundness = true;
     }
 
@@ -687,13 +687,13 @@ static void _updateStar(TVG_UNUSED LottieGroup* parent, LottiePolyStar* star, Ma
     if (roundedCorner) {
         if (offsetPath) {
             auto intermediate = Shape::gen();
-            roundness->modifyPolystar(P(shape)->rs.path.cmds, P(shape)->rs.path.pts, P(intermediate)->rs.path.cmds, P(intermediate)->rs.path.pts, outerRoundness, hasRoundness);
-            offsetPath->modifyPolystar(P(intermediate)->rs.path.cmds, P(intermediate)->rs.path.pts, P(merging)->rs.path.cmds, P(merging)->rs.path.pts);
+            roundness->modifyPolystar(SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, SHAPE(intermediate)->rs.path.cmds, SHAPE(intermediate)->rs.path.pts, outerRoundness, hasRoundness);
+            offsetPath->modifyPolystar(SHAPE(intermediate)->rs.path.cmds, SHAPE(intermediate)->rs.path.pts, SHAPE(merging)->rs.path.cmds, SHAPE(merging)->rs.path.pts);
             delete(intermediate);
         } else {
-            roundness->modifyPolystar(P(shape)->rs.path.cmds, P(shape)->rs.path.pts, P(merging)->rs.path.cmds, P(merging)->rs.path.pts, outerRoundness, hasRoundness);
+            roundness->modifyPolystar(SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, SHAPE(merging)->rs.path.cmds, SHAPE(merging)->rs.path.pts, outerRoundness, hasRoundness);
         }
-    } else if (offsetPath) offsetPath->modifyPolystar(P(shape)->rs.path.cmds, P(shape)->rs.path.pts, P(merging)->rs.path.cmds, P(merging)->rs.path.pts);
+    } else if (offsetPath) offsetPath->modifyPolystar(SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, SHAPE(merging)->rs.path.cmds, SHAPE(merging)->rs.path.pts);
 }
 
 
@@ -722,11 +722,11 @@ static void _updatePolygon(LottieGroup* parent, LottiePolyStar* star, Matrix* tr
     } else {
         shape = merging;
         if (hasRoundness) {
-            P(shape)->rs.path.pts.reserve(ptsCnt * 3 + 2);
-            P(shape)->rs.path.cmds.reserve(ptsCnt + 3);
+            SHAPE(shape)->rs.path.pts.reserve(ptsCnt * 3 + 2);
+            SHAPE(shape)->rs.path.cmds.reserve(ptsCnt + 3);
         } else {
-            P(shape)->rs.path.pts.reserve(ptsCnt + 2);
-            P(shape)->rs.path.cmds.reserve(ptsCnt + 3);
+            SHAPE(shape)->rs.path.pts.reserve(ptsCnt + 2);
+            SHAPE(shape)->rs.path.cmds.reserve(ptsCnt + 3);
         }
     }
 
@@ -774,13 +774,13 @@ static void _updatePolygon(LottieGroup* parent, LottiePolyStar* star, Matrix* tr
     if (roundedCorner) {
         if (offsetPath) {
             auto intermediate = Shape::gen();
-            roundness->modifyPolystar(P(shape)->rs.path.cmds, P(shape)->rs.path.pts, P(intermediate)->rs.path.cmds, P(intermediate)->rs.path.pts, 0.0f, false);
-            offsetPath->modifyPolystar(P(intermediate)->rs.path.cmds, P(intermediate)->rs.path.pts, P(merging)->rs.path.cmds, P(merging)->rs.path.pts);
+            roundness->modifyPolystar(SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, SHAPE(intermediate)->rs.path.cmds, SHAPE(intermediate)->rs.path.pts, 0.0f, false);
+            offsetPath->modifyPolystar(SHAPE(intermediate)->rs.path.cmds, SHAPE(intermediate)->rs.path.pts, SHAPE(merging)->rs.path.cmds, SHAPE(merging)->rs.path.pts);
             delete(intermediate);
         } else {
-            roundness->modifyPolystar(P(shape)->rs.path.cmds, P(shape)->rs.path.pts, P(merging)->rs.path.cmds, P(merging)->rs.path.pts, 0.0f, false);
+            roundness->modifyPolystar(SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, SHAPE(merging)->rs.path.cmds, SHAPE(merging)->rs.path.pts, 0.0f, false);
         }
-    } else if (offsetPath) offsetPath->modifyPolystar(P(shape)->rs.path.cmds, P(shape)->rs.path.pts, P(merging)->rs.path.cmds, P(merging)->rs.path.pts);
+    } else if (offsetPath) offsetPath->modifyPolystar(SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, SHAPE(merging)->rs.path.cmds, SHAPE(merging)->rs.path.pts);
 }
 
 
@@ -809,7 +809,7 @@ void LottieBuilder::updatePolystar(LottieGroup* parent, LottieObject** child, fl
         _draw(parent, star, ctx);
         if (star->type == LottiePolyStar::Star) _updateStar(parent, star, identity ? nullptr : &matrix, ctx->roundness, ctx->offsetPath, frameNo, ctx->merging, exps);
         else _updatePolygon(parent, star, identity  ? nullptr : &matrix, ctx->roundness, ctx->offsetPath, frameNo, ctx->merging, exps);
-        P(ctx->merging)->update(RenderUpdateFlag::Path);
+        PAINT(ctx->merging)->update(RenderUpdateFlag::Path);
     }
 }
 
@@ -861,15 +861,15 @@ void LottieBuilder::updateTrimpath(TVG_UNUSED LottieGroup* parent, LottieObject*
     float begin, end;
     trimpath->segment(frameNo, begin, end, exps);
 
-    if (P(ctx->propagator)->rs.stroke) {
-        auto pbegin = P(ctx->propagator)->rs.stroke->trim.begin;
-        auto pend = P(ctx->propagator)->rs.stroke->trim.end;
+    if (SHAPE(ctx->propagator)->rs.stroke) {
+        auto pbegin = SHAPE(ctx->propagator)->rs.stroke->trim.begin;
+        auto pend = SHAPE(ctx->propagator)->rs.stroke->trim.end;
         auto length = fabsf(pend - pbegin);
         begin = (length * begin) + pbegin;
         end = (length * end) + pbegin;
     }
 
-    P(ctx->propagator)->strokeTrim(begin, end, trimpath->type == LottieTrimpath::Type::Simultaneous);
+    ctx->propagator->strokeTrim(begin, end, trimpath->type == LottieTrimpath::Type::Simultaneous);
     ctx->merging = nullptr;
 }
 
@@ -1083,8 +1083,8 @@ void LottieBuilder::updateText(LottieLayer* layer, float frameNo)
                 for (auto g = glyph->children.begin(); g < glyph->children.end(); ++g) {
                     auto group = static_cast<LottieGroup*>(*g);
                     for (auto p = group->children.begin(); p < group->children.end(); ++p) {
-                        if (static_cast<LottiePath*>(*p)->pathset(frameNo, P(shape)->rs.path.cmds, P(shape)->rs.path.pts, nullptr, nullptr, nullptr)) {
-                            P(shape)->update(RenderUpdateFlag::Path);
+                        if (static_cast<LottiePath*>(*p)->pathset(frameNo, SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, nullptr, nullptr, nullptr)) {
+                            PAINT(shape)->update(RenderUpdateFlag::Path);
                         }
                     }
                 }
@@ -1237,11 +1237,11 @@ void LottieBuilder::updateMaskings(LottieLayer* layer, float frameNo)
 
     //Apply Masking Expansion (Offset)
     if (expand == 0.0f) {
-        pMask->pathset(frameNo, P(pShape)->rs.path.cmds, P(pShape)->rs.path.pts, nullptr, nullptr, nullptr, exps);
+        pMask->pathset(frameNo, SHAPE(pShape)->rs.path.cmds, SHAPE(pShape)->rs.path.pts, nullptr, nullptr, nullptr, exps);
     } else {
         //TODO: Once path direction support is implemented, ensure that the direction is ignored here
         auto offset = LottieOffsetModifier(pMask->expand(frameNo));
-        pMask->pathset(frameNo, P(pShape)->rs.path.cmds, P(pShape)->rs.path.pts, nullptr, nullptr, &offset, exps);
+        pMask->pathset(frameNo, SHAPE(pShape)->rs.path.cmds, SHAPE(pShape)->rs.path.pts, nullptr, nullptr, &offset, exps);
     }
 
     auto compMethod = (pMethod == MaskMethod::Subtract || pMethod == MaskMethod::InvAlpha) ? MaskMethod::InvAlpha : MaskMethod::Alpha;
@@ -1269,14 +1269,14 @@ void LottieBuilder::updateMaskings(LottieLayer* layer, float frameNo)
 
         //Append the mask shape
         if (pMethod == method && (method == MaskMethod::Subtract || method == MaskMethod::Difference)) {
-            mask->pathset(frameNo, P(pShape)->rs.path.cmds, P(pShape)->rs.path.pts, nullptr, nullptr, nullptr, exps);
+            mask->pathset(frameNo, SHAPE(pShape)->rs.path.cmds, SHAPE(pShape)->rs.path.pts, nullptr, nullptr, nullptr, exps);
         //Chain composition
         } else {
             auto shape = layer->pooling();
             shape->reset();
             shape->fill(255, 255, 255, mask->opacity(frameNo));
             shape->transform(layer->cache.matrix);
-            mask->pathset(frameNo, P(shape)->rs.path.cmds, P(shape)->rs.path.pts, nullptr, nullptr, nullptr, exps);
+            mask->pathset(frameNo, SHAPE(shape)->rs.path.cmds, SHAPE(shape)->rs.path.pts, nullptr, nullptr, nullptr, exps);
             pShape->mask(shape, method);
             pShape = shape;
             pMethod = method;

commit 476bead7e37d476217036c82013f6978fb152ce6

renderer: fixed a memory leak
This issue only valid on the main branch.

regression by 1806b329718589a36b606cac5760e41dacb586f6

issue: https://github.com/thorvg/thorvg/issues/3070

src/renderer/tvgPaint.h
@@ -109,7 +109,7 @@ namespace tvg
 
             if (free && refCnt == 0) {
                 //TODO: use the global dismiss function?
-                delete(this);
+                delete(paint);
                 return 0;
             }
 