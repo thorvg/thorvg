ceb063b9e738f44c547c81fa92fefe96cb68c688
Mira Grudzinska <mira@lottiefiles.com>
2025-02-11 22:28:40 +0100

    lottie: ++ dashed stroke support
    Until now lottie loader supported only a single
    dash-gap pair, and only the case where only the dash
    was provided was handled correctly. When both values
    were provided, the gap was incorrectly increased by
    the dash value. If more values were supplied, only
    the last pair was considered.
    
    @Issue: https://github.com/thorvg/thorvg/issues/3191

--- src/loaders/lottie/tvgLottieModel.h
+++ src/loaders/lottie/tvgLottieModel.h
@@ -37,35 +37,39 @@ struct LottieStroke
 {
     struct DashAttr
     {
-        //0: offset, 1: dash, 2: gap
-        LottieFloat value[3] = {0.0f, 0.0f, 0.0f};
+        LottieFloat offset = 0.0f;
+        LottieFloat* values = nullptr;
+        uint8_t size = 0;
+        uint8_t allocated = 0;
     };
 
     virtual ~LottieStroke()
     {
+        if (dashattr) delete[] dashattr->values;
         delete(dashattr);
     }
 
-    LottieFloat& dash(int no)
+
+    LottieFloat& dashValue()
     {
         if (!dashattr) dashattr = new DashAttr;
-        return dashattr->value[no];
+
+        if (dashattr->size + 1 > dashattr->allocated) {
+            dashattr->allocated = dashattr->size + 2;
+            auto newValues = new LottieFloat[dashattr->allocated];
+            for (uint8_t i = 0; i < dashattr->size; ++i) newValues[i] = LottieFloat(dashattr->values[i]);
+            delete[] dashattr->values;
+            dashattr->values = newValues;
+        }
+
+        return dashattr->values[dashattr->size++];
     }
 
-    float dashOffset(float frameNo, Tween& tween, LottieExpressions* exps)
-    {
-        return dash(0)(frameNo, tween, exps);
-    }
 
-    float dashSize(float frameNo, Tween& tween, LottieExpressions* exps)
+    LottieFloat& dashOffset()
     {
-        auto d = dash(1)(frameNo, tween, exps);
-        return (d > 0.0f) ? d : 0.0f;
-    }
-
-    float dashGap(float frameNo, Tween& tween, LottieExpressions* exps)
-    {
-        return dash(2)(frameNo, tween, exps);
+        if (!dashattr) dashattr = new DashAttr;
+        return dashattr->offset;
     }
 
     LottieFloat width = 0.0f;
@@ -614,9 +618,8 @@ struct LottieSolidStroke : LottieSolid, LottieStroke
     {
         if (width.ix == ix) return &width;
         if (dashattr) {
-            if (dashattr->value[0].ix == ix) return &dashattr->value[0];
-            if (dashattr->value[1].ix == ix) return &dashattr->value[1];
-            if (dashattr->value[2].ix == ix) return &dashattr->value[2];
+            for (uint8_t i = 0; i < dashattr->size ; ++i)
+                if (dashattr->values[i].ix == ix) return &dashattr->values[i];
         }
         return LottieSolid::property(ix);
     }
@@ -723,9 +726,8 @@ struct LottieGradientStroke : LottieGradient, LottieStroke
     {
         if (width.ix == ix) return &width;
         if (dashattr) {
-            if (dashattr->value[0].ix == ix) return &dashattr->value[0];
-            if (dashattr->value[1].ix == ix) return &dashattr->value[1];
-            if (dashattr->value[2].ix == ix) return &dashattr->value[2];
+            for (uint8_t i = 0; i < dashattr->size ; ++i)
+                if (dashattr->values[i].ix == ix) return &dashattr->values[i];
         }
         return LottieGradient::property(ix);
     }
