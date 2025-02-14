808a32a3379f810cfcf7d211276970d20c4be173
Mira Grudzinska <mira@lottiefiles.com>
2025-02-14 01:58:15 +0100

    api: handling values <= 0 in strokeDash() api
    The API allows values <= 0 for dashes and gaps. Negative values
    are treated as zero. The exception is when all provided values
    are <= 0, in which case the dash is ignored.
    
    On the SVG loader side, compatibility with the SVG standard has been
    ensured (negative values cause the dash to be ignored).
    
    Docs - the strokeDash API behavior's clarification for odd numbers
    of values in dashPattern and refinement of the accepted values.
    
    @Issue: https://github.com/thorvg/thorvg/issues/3205

--- src/renderer/sw_engine/tvgSwShape.cpp
+++ src/renderer/sw_engine/tvgSwShape.cpp
@@ -511,7 +511,9 @@ bool shapeGenStrokeRle(SwShape* shape, const RenderShape* rshape, const Matrix& 
 
     //Dash style with/without trimming
     auto trimmed = rshape->strokeTrim();
-    if (rshape->stroke->dashCnt > 0) {
+    auto len = 0.0f;
+    for (size_t i = 0; i < rshape->stroke->dashCnt; ++i) len += rshape->stroke->dashPattern[i];
+    if (rshape->stroke->dashCnt > 0 && len > FLT_EPSILON) {
         shapeOutline = _genDashOutline(rshape, transform, mpool, tid, trimmed);
         if (!shapeOutline) return false;
         dashStroking = true;
