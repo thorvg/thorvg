852424e69d09c913648d140253938897d56e4f72
Mira Grudzinska <mira@lottiefiles.com>
2025-04-10 10:09:58 +0200

    ttf: prevent crash while converting utf8 to unicode
    If wrong utf8 sequence if given as an input, decoding
    return nullptr and it has to be handled correctly.

--- src/loaders/ttf/tvgTtfLoader.cpp
+++ src/loaders/ttf/tvgTtfLoader.cpp
@@ -277,6 +277,7 @@ bool TtfLoader::read(Shape* shape, char* text, FontMetrics& out)
 
     auto n = strlen(text);
     auto code = _codepoints(text, n);
+    if (!code) return false;
 
     //TODO: optimize with the texture-atlas?
     TtfGlyphMetrics gmetrics;
