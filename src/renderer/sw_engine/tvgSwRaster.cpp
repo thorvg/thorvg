thorvg/thorvg

commit 2990e72b23b51d4700417325763eb86484944fc6

sw_engine: ++raster color accuracy
Improves the color accuracy of rasterized images by skipping the pre-multiply step when the alpha is full opaque or fully transparent.

fixes: #3429

src/renderer/sw_engine/tvgSwRaster.cpp
@@ -1706,6 +1706,7 @@ void rasterPremultiply(RenderSurface* surface)
         for (uint32_t x = 0; x < surface->w; ++x, ++dst) {
             auto c = *dst;
             auto a = (c >> 24);
+            if (a == 255 || a == 0) continue;
             *dst = (c & 0xff000000) + ((((c >> 8) & 0xff) * a) & 0xff00) + ((((c & 0x00ff00ff) * a) >> 8) & 0x00ff00ff);
         }
     }

commit ae84708f9b668c07b922e7890e20afd9a529167b

sw_engine: fix regression in alpha handling
revert the alpha skip condition introduced in
commit 2990e72b23b51d4700417325763eb86484944fc6.

color values with alpha = 0 must be transformed to 0
to ensure correct intermediate composition results.

src/renderer/sw_engine/tvgSwRaster.cpp
@@ -1706,7 +1706,7 @@ void rasterPremultiply(RenderSurface* surface)
         for (uint32_t x = 0; x < surface->w; ++x, ++dst) {
             auto c = *dst;
             auto a = (c >> 24);
-            if (a == 255 || a == 0) continue;
+            if (a == 255) continue;
             *dst = (c & 0xff000000) + ((((c >> 8) & 0xff) * a) & 0xff00) + ((((c & 0x00ff00ff) * a) >> 8) & 0x00ff00ff);
         }
     }