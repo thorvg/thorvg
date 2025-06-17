bd3d92f599d4df15aa5fbf7cfeb91c33fecfa902
Hermet Park <hermet@lottiefiles.com>
2025-06-17 18:52:07 +0900

    gl_engine: ++safety

--- src/renderer/gl_engine/tvgGlRenderer.cpp
+++ src/renderer/gl_engine/tvgGlRenderer.cpp
@@ -864,12 +864,13 @@ bool GlRenderer::sync()
 
 RenderRegion GlRenderer::region(RenderData data)
 {
-    if (!data || currentPass()->isEmpty()) return {};
+    auto pass = currentPass();
+    if (!data || !pass || pass->isEmpty()) return {};
 
     auto shape = reinterpret_cast<GlShape*>(data);
     auto bounds = shape->geometry.getBounds();
 
-    auto const& vp = currentPass()->getViewport();
+    auto const& vp = pass->getViewport();
     bounds.intersect(vp);
 
     return bounds;
