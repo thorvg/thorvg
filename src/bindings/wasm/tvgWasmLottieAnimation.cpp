4174dc0f9401efc60dd48b4132b42ab1af8eded1
Hermet Park <hermet@lottiefiles.com>
2025-06-23 18:27:48 +0900

    initial draft

--- src/bindings/wasm/tvgWasmLottieAnimation.cpp
+++ src/bindings/wasm/tvgWasmLottieAnimation.cpp
@@ -24,30 +24,22 @@
 #include <emscripten.h>
 #include <emscripten/bind.h>
 #include "tvgPicture.h"
-#include "tvgWasmDefaultFont.h"
 
 using namespace emscripten;
 using namespace std;
 using namespace tvg;
 
-static const char* NoError = "None";
 
 struct TvgEngineMethod
 {
     virtual ~TvgEngineMethod() {}
-    virtual Canvas* init(string&) = 0;
+    virtual Canvas* init() = 0;
     virtual void resize(Canvas* canvas, int w, int h) = 0;
     virtual val output(int w, int h)
     {
         return val(typed_memory_view<uint8_t>(0, nullptr));
     }
-
-    void loadFont() {
-        Text::load("default", requestFont(), DEFAULT_FONT_SIZE, "ttf", false);
-    }
 };
-
-#ifdef THORVG_SW_RASTER_SUPPORT
 
 struct TvgSwEngine : TvgEngineMethod
 {
@@ -57,13 +49,11 @@ struct TvgSwEngine : TvgEngineMethod
     {
         std::free(buffer);
         Initializer::term();
-        retrieveFont();
     }
 
-    Canvas* init(string&) override
+    Canvas* init() override
     {
         Initializer::init(0);
-        loadFont();
         return SwCanvas::gen();
     }
 
@@ -80,202 +70,23 @@ struct TvgSwEngine : TvgEngineMethod
     }
 };
 
-#endif
-
-
-#ifdef THORVG_WG_RASTER_SUPPORT
-
-#include <emscripten/html5_webgpu.h>
-
-static WGPUInstance instance{};
-static WGPUAdapter adapter{};
-static WGPUDevice device{};
-static bool adapterRequested = false;
-static bool deviceRequested = false;
-static bool initializationFailed = false;
-
-struct TvgWgEngine : TvgEngineMethod
-{
-    WGPUSurface surface{};
-
-    ~TvgWgEngine()
-    {
-        wgpuSurfaceRelease(surface);
-        Initializer::term();
-        retrieveFont();
-    }
-
-    Canvas* init(string& selector) override
-    {
-        WGPUSurfaceDescriptorFromCanvasHTMLSelector canvasDesc{};
-        canvasDesc.chain.next = nullptr;
-        canvasDesc.chain.sType = WGPUSType_SurfaceDescriptorFromCanvasHTMLSelector;
-        canvasDesc.selector = selector.c_str();
-
-        WGPUSurfaceDescriptor surfaceDesc{};
-        surfaceDesc.nextInChain = &canvasDesc.chain;
-        surface = wgpuInstanceCreateSurface(instance, &surfaceDesc);
-
-        Initializer::init(0);
-        loadFont();
-        return WgCanvas::gen();
-    }
-
-    void resize(Canvas* canvas, int w, int h) override
-    {
-        if (canvas) static_cast<WgCanvas*>(canvas)->target(device, instance, surface, w, h, ColorSpace::ABGR8888S);
-    }
-
-    static int init()
-    {
-        if (initializationFailed) return 1;
-
-        //Init WebGPU
-        if (!instance) instance = wgpuCreateInstance(nullptr);
-
-        // request adapter
-        if (!adapter) {
-            if (adapterRequested) return 2;
-
-            const WGPURequestAdapterOptions requestAdapterOptions { .nextInChain = nullptr, .powerPreference = WGPUPowerPreference_HighPerformance, .forceFallbackAdapter = false };
-            auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const* message, void* pUserData) {
-                if (status != WGPURequestAdapterStatus_Success) {
-                    initializationFailed = true;
-                    return;
-                }
-                *((WGPUAdapter*)pUserData) = adapter;
-            };
-            wgpuInstanceRequestAdapter(instance, &requestAdapterOptions, onAdapterRequestEnded, &adapter);
-
-            adapterRequested = true;
-            return 2;
-        }
-
-        // request device
-        if (deviceRequested) return device == nullptr ? 2 : 0;
-
-        WGPUFeatureName featureNames[32]{};
-        size_t featuresCount = wgpuAdapterEnumerateFeatures(adapter, featureNames);
-        if (!device) {
-            const WGPUDeviceDescriptor deviceDesc { .nextInChain = nullptr, .label = "The device", .requiredFeatureCount = featuresCount, .requiredFeatures = featureNames };
-            auto onDeviceRequestEnded = [](WGPURequestDeviceStatus status, WGPUDevice device, char const* message, void* pUserData) {
-                if (status != WGPURequestDeviceStatus_Success) {
-                    initializationFailed = true;
-                    return;
-                }
-                *((WGPUDevice*)pUserData) = device;
-            };
-            wgpuAdapterRequestDevice(adapter, &deviceDesc, onDeviceRequestEnded, &device);
-
-            deviceRequested = true;
-            return 2;
-        }
-        return 0;
-    }
-
-    static void term()
-    {
-        wgpuDeviceRelease(device);
-        wgpuAdapterRelease(adapter);
-        wgpuInstanceRelease(instance);
-    }
-
-};
-#endif
-
-
-#ifdef THORVG_GL_RASTER_SUPPORT
-
-#include <emscripten/html5_webgl.h>
-
-struct TvgGLEngine : TvgEngineMethod
-{
-    intptr_t context = 0;
-
-    ~TvgGLEngine()
-    {
-        if (context) {
-            Initializer::term();
-            emscripten_webgl_destroy_context(context);
-            context = 0;
-        }
-        retrieveFont();
-    }
-
-    Canvas* init(string& selector) override
-    {
-        EmscriptenWebGLContextAttributes attrs{};
-        attrs.alpha = true;
-        attrs.depth = false;
-        attrs.stencil = false;
-        attrs.premultipliedAlpha = true;
-        attrs.failIfMajorPerformanceCaveat = false;
-        attrs.majorVersion = 2;
-        attrs.minorVersion = 0;
-        attrs.enableExtensionsByDefault = true;
-
-        context = emscripten_webgl_create_context(selector.c_str(), &attrs);
-        if (context == 0) return nullptr;
-
-        emscripten_webgl_make_context_current(context);
-
-        if (Initializer::init(0) != Result::Success) return nullptr;
-        loadFont();
-
-        return GlCanvas::gen();
-    }
-
-    void resize(Canvas* canvas, int w, int h) override
-    {
-        if (canvas) static_cast<GlCanvas*>(canvas)->target((void*)context, 0, w, h, ColorSpace::ABGR8888S);
-    }
-};
-#endif
-
-
-class __attribute__((visibility("default"))) TvgLottieAnimation
+class __attribute__((visibility("default"))) TvgTestApp
 {
 public:
-    ~TvgLottieAnimation()
+    ~TvgTestApp()
     {
-        delete(animation);
+        delete(cityview);
+        delete(darkness);
         delete(canvas);
         delete(engine);
     }
 
-    explicit TvgLottieAnimation(string engine = "sw", string selector = "")
+    explicit TvgTestApp()
     {
-        errorMsg = NoError;
-
-#ifdef THORVG_SW_RASTER_SUPPORT
-        if (engine == "sw") this->engine = new TvgSwEngine;
-#endif
-#ifdef THORVG_GL_RASTER_SUPPORT
-        if (engine == "gl") this->engine = new TvgGLEngine;
-#endif
-#ifdef THORVG_WG_RASTER_SUPPORT
-        if (engine == "wg") this->engine = new TvgWgEngine;
-#endif
-
-        if (!this->engine) {
-            errorMsg = "Invalid engine";
-            return;
-        }
-
-        canvas = this->engine->init(selector);
-
-        if (!canvas) {
-            errorMsg = "Unsupported!";
-            return;
-        }
-
-        animation = Animation::gen();
-        if (!animation) errorMsg = "Invalid animation";
-    }
-
-    string error()
-    {
-        return errorMsg;
+        this->engine = new TvgSwEngine;
+        canvas = this->engine->init();
+        cityview = Picture::gen();
+        darkness = Shape::gen();
     }
 
     val size()
@@ -283,125 +94,81 @@ public:
         return val(typed_memory_view(2, psize));
     }
 
-    float duration()
+    bool load(string data, string mimetype, int width, int height)
     {
-        if (!canvas || !animation) return 0;
-        return animation->duration();
-    }
-
-    float totalFrame()
-    {
-        if (!canvas || !animation) return 0;
-        return animation->totalFrame();
-    }
-
-    float curFrame()
-    {
-        if (!canvas || !animation) return 0;
-        return animation->curFrame();
-    }
-
-    bool load(string data, string mimetype, int width, int height, string rpath = "")
-    {
-        errorMsg = NoError;
-
         if (!canvas) return false;
 
-        if (data.empty()) {
-            errorMsg = "Invalid data";
-            return false;
-        }
+        if (data.empty()) return false;
 
-        canvas->remove();
-
-        delete(animation);
-        animation = Animation::gen();
-
-        string filetype = mimetype;
-        if (filetype == "json") {
-            filetype = "lottie+json";
-        }
-
-        if (animation->picture()->load(data.c_str(), data.size(), filetype.c_str(), rpath.c_str(), false) != Result::Success) {
-            errorMsg = "load() fail";
-            return false;
-        }
-
-        animation->picture()->size(&psize[0], &psize[1]);
-
-        /* need to reset size to calculate scale in Picture.size internally before calling resize() */
         this->width = 0;
         this->height = 0;
 
-        resize(width, height);
+        canvas->remove();
+        raindrops.clear();
 
-        if (canvas->push(animation->picture()) != Result::Success) {
-            errorMsg = "push() fail";
-            return false;
+        cityview->load(data.c_str(), data.size(), mimetype.c_str(), nullptr, false);
+        cityview->size(&psize[0], &psize[1]);
+        canvas->push(cityview);
+
+        darkness->fill(0, 0, 0, 150);
+        canvas->push(darkness);
+
+        //rain drops
+        auto size = width / COUNT;
+        raindrops.reserve(COUNT);
+
+        for (int i = 0; i < COUNT; ++i) {
+            auto shape = tvg::Shape::gen();
+            raindrops.push_back({shape, 0, 0, 0, 0});
+            shape->appendRect(0, 0, 1, rand() % 20 + size);
+            shape->fill(255, 255, 255, 70 + rand() % 100);
+            canvas->push(shape);
         }
 
-        updated = true;
+        resize(width, height);
 
         return true;
     }
 
     val render()
     {
-        errorMsg = NoError;
+        if (!canvas || !cityview) return val(typed_memory_view<uint8_t>(0, nullptr));
 
-        if (!canvas || !animation) return val(typed_memory_view<uint8_t>(0, nullptr));
-
-        if (!updated) return engine->output(width, height);
-
-        if (canvas->draw(true) != Result::Success) {
-            errorMsg = "draw() fail";
-            return val(typed_memory_view<uint8_t>(0, nullptr));
-        }
-
+        canvas->draw(clear);
         canvas->sync();
-
-        updated = false;
 
         return engine->output(width, height);
     }
 
     bool update()
     {
-        if (!updated) return true;
-
-        errorMsg = NoError;
-
-        if (canvas->update() != Result::Success) {
-            errorMsg = "update() fail";
-            return false;
+        for (auto& p : raindrops) {
+            p.y += p.speed;
+            if (p.y > height) {
+                p.y -= height;
+            }
+            p.obj->translate(p.x, p.y);
         }
 
-        return true;
-    }
-
-    bool frame(float no)
-    {
-        if (!canvas || !animation) return false;
-        if (animation->frame(no) == Result::Success) {
-            updated = true;
-        }
+        canvas->update();
         return true;
     }
 
     bool viewport(float x, float y, float width, float height)
     {
-        if (!canvas || !animation) return false;
-        if (canvas->viewport(x, y, width, height) != Result::Success) {
-            errorMsg = "viewport() fail";
-            return false;
-        }
-
+        if (!canvas) return false;
+        canvas->viewport(x, y, width, height);
         return true;
+    }
+
+    void partial(bool on)
+    {
+        clear = !on;
     }
 
     void resize(int width, int height)
     {
-        if (!canvas || !animation) return;
+        if (!canvas) return;
         if (this->width == width && this->height == height) return;
 
         canvas->sync();
@@ -411,6 +178,11 @@ public:
 
         engine->resize(canvas, width, height);
 
+        //darkness
+        darkness->reset();
+        darkness->appendRect(0, 0, width, height);
+
+        //citiview
         float scale;
         float shiftX = 0.0f, shiftY = 0.0f;
         if (psize[0] > psize[1]) {
@@ -420,131 +192,52 @@ public:
             scale = height / psize[1];
             shiftX = (width - psize[0] * scale) * 0.5f;
         }
-        animation->picture()->scale(scale);
-        animation->picture()->translate(shiftX, shiftY);
+        cityview->scale(scale);
+        cityview->translate(shiftX, shiftY);
 
-        updated = true;
+        auto size = width / COUNT;
+        auto i = 0;
+        for (auto& p : raindrops) {
+            p.x = size * i;
+            p.y = float(rand()%height);
+            p.speed = (10 + float(rand() % 100) * 0.1f) * 2;
+            ++i;
+        }
     }
-
-    bool save(string data, string mimetype)
-    {
-        if (mimetype == "gif") return save2Gif(data);
-        errorMsg = "Invalid mimetype";
-        return false;
-    }
-
-    bool save2Gif(string data)
-    {
-        errorMsg = NoError;
-
-        if (data.empty()) {
-            errorMsg = "Invalid data";
-            return false;
-        }
-
-        auto saver = unique_ptr<Saver>(Saver::gen());
-        if (!saver) {
-            errorMsg = "Invalid saver";
-            return false;
-        }
-
-        //animation to save
-        auto animation = unique_ptr<Animation>(Animation::gen());
-        if (!animation) {
-            errorMsg = "Invalid animation";
-            return false;
-        }
-
-        if (animation->picture()->load(data.c_str(), data.size(), "lot", nullptr, false) != Result::Success) {
-            errorMsg = "load() fail";
-            return false;
-        }
-
-        //gif resolution (600x600)
-        constexpr float GIF_SIZE = 600;
-
-        //transform
-        float width, height;
-        animation->picture()->size(&width, &height);
-        float scale;
-        if (width > height) scale = GIF_SIZE / width;
-        else scale = GIF_SIZE / height;
-        animation->picture()->size(width * scale, height * scale);
-
-        //set a white opaque background
-        auto bg = tvg::Shape::gen();
-        if (!bg) {
-            errorMsg = "Invalid bg";
-            return false;
-        }
-        bg->fill(255, 255, 255, 255);
-        bg->appendRect(0, 0, GIF_SIZE, GIF_SIZE);
-
-        if (saver->background(bg) != Result::Success) {
-            errorMsg = "background() fail";
-            return false;
-        }
-
-        if (saver->save(animation.release(), "output.gif", 100, 30) != tvg::Result::Success) {
-            errorMsg = "save(), fail";
-            return false;
-        }
-
-        saver->sync();
-
-        return true;
-    }
-
-    // TODO: Advanced APIs wrt Interactivity & theme methods...
 
 private:
-    string                 errorMsg;
+    struct Particle {
+        tvg::Paint* obj;
+        float x, y;
+        float speed;
+        float size;
+    };
+
+    Picture*                cityview = nullptr;
+    Shape*                  darkness = nullptr;
+
+    const float COUNT = 3600.0f;
+    std::vector<Particle> raindrops;
+    std::vector<Particle> clouds;
+
     Canvas*                canvas = nullptr;
-    Animation*             animation = nullptr;
     TvgEngineMethod*       engine = nullptr;
     uint32_t               width = 0;
     uint32_t               height = 0;
-    float                  psize[2];         //picture size
-    bool                   updated = false;
+    float                  psize[2];         //cityview size
+    bool                   clear = false;
 };
-
-
-// 0: success, 1: fail, 2: wait for async request
-int init()
-{
-#ifdef THORVG_WG_RASTER_SUPPORT
-    return TvgWgEngine::init();
-#else
-    return 0;
-#endif
-}
-
-
-void term()
-{
-#ifdef THORVG_WG_RASTER_SUPPORT
-    TvgWgEngine::term();
-#endif
-}
 
 
 EMSCRIPTEN_BINDINGS(thorvg_bindings)
 {
-    emscripten::function("init", &init);
-    emscripten::function("term", &term);
-
-    class_<TvgLottieAnimation>("TvgLottieAnimation")
-        .constructor<string, string>()
-        .function("error", &TvgLottieAnimation ::error, allow_raw_pointers())
-        .function("size", &TvgLottieAnimation ::size)
-        .function("duration", &TvgLottieAnimation ::duration)
-        .function("totalFrame", &TvgLottieAnimation ::totalFrame)
-        .function("curFrame", &TvgLottieAnimation ::curFrame)
-        .function("render", &TvgLottieAnimation::render)
-        .function("load", &TvgLottieAnimation ::load)
-        .function("update", &TvgLottieAnimation ::update)
-        .function("frame", &TvgLottieAnimation ::frame)
-        .function("viewport", &TvgLottieAnimation ::viewport)
-        .function("resize", &TvgLottieAnimation ::resize)
-        .function("save", &TvgLottieAnimation ::save);
+    class_<TvgTestApp>("TvgTestApp")
+        .constructor()
+        .function("size", &TvgTestApp::size)
+        .function("render", &TvgTestApp::render)
+        .function("load", &TvgTestApp::load)
+        .function("update", &TvgTestApp::update)
+        .function("viewport", &TvgTestApp::viewport)
+        .function("resize", &TvgTestApp::resize)
+        .function("partial", &TvgTestApp::partial);
 }
