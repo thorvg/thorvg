04ab394eadb7d1cfff72326edfcea782ecd545e8
Jinny You <jinny@lottiefiles.com>
2025-08-06 13:19:39 +0900

    lottie: skip timeStretch when timeRemap is present
    Fixed logic correctly that was previously reverted due to a regression bug: 3edcde
    
    timeStretch should not be applied when timeRemap exists.
    
    issue: https://github.com/thorvg/thorvg/issues/3591

--- src/loaders/lottie/tvgLottieModel.cpp
+++ src/loaders/lottie/tvgLottieModel.cpp
@@ -693,11 +693,9 @@ void LottieLayer::prepare(RGB32* color)
 float LottieLayer::remap(LottieComposition* comp, float frameNo, LottieExpressions* exp)
 {
     if (timeRemap.frames || timeRemap.value >= 0.0f) {
-        frameNo = comp->frameAtTime(timeRemap(frameNo, exp));
-    } else {
-        frameNo -= startFrame;
+        return comp->frameAtTime(timeRemap(frameNo, exp));
     }
-    return (frameNo / timeStretch);
+    return (frameNo - startFrame) / timeStretch;
 }
 
 
