thorvg/thorvg

commit b2875b6c1428528e1235197e116c8ff724787d7b

sw_engine: stability++
fixed a corner case that rendering is not properly updated
only if the opacity is updated.

src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -117,15 +117,14 @@ struct SwShapeTask : SwTask
         }
 
         auto strokeWidth = validStrokeWidth(clipper);
-        RenderRegion renderBox{};
+        auto renderBox = curBox;
         auto updateShape = flags & (RenderUpdateFlag::Path | RenderUpdateFlag::Transform | RenderUpdateFlag::Clip);
-        auto updateFill = false;
+        auto updateFill = flags & (RenderUpdateFlag::Color | RenderUpdateFlag::Gradient);
 
         //Shape
-        if (updateShape || flags & (RenderUpdateFlag::Color | RenderUpdateFlag::Gradient)) {
-            updateFill = (MULTIPLY(rshape->color.a, opacity) || rshape->fill);
+        if (updateShape || updateFill) {
             if (updateShape) shapeReset(&shape);
-            if (updateFill || clipper) {
+            if (!shape.rle || shape.rle->invalid()) {
                 if (shapePrepare(&shape, rshape, transform, curBox, renderBox, mpool, tid, clips.count > 0 ? true : false)) {
                     if (!shapeGenRle(&shape, rshape, antialiasing(strokeWidth))) goto err;
                 } else {
@@ -218,14 +217,17 @@ struct SwImageTask : SwTask
         image.stride = source->stride;
         image.channelSize = source->channelSize;
 
+        auto updateImage = flags & (RenderUpdateFlag::Image | RenderUpdateFlag::Clip | RenderUpdateFlag::Transform);
+        auto updateColor = flags & (RenderUpdateFlag::Color);
+
         //Invisible shape turned to visible by alpha.
-        if ((flags & (RenderUpdateFlag::Image | RenderUpdateFlag::Clip | RenderUpdateFlag::Transform | RenderUpdateFlag::Color)) && (opacity > 0)) {
-            imageReset(&image);
-            if (!image.data || image.w == 0 || image.h == 0) goto end;
-            if (!imagePrepare(&image, transform, clipBox, curBox, mpool, tid)) goto end;
+        if ((updateImage || updateColor) && (opacity > 0)) {
+            if (updateImage) imageReset(&image);
+            if (!image.data || image.w == 0 || image.h == 0) goto err;
+            if (!imagePrepare(&image, transform, clipBox, curBox, mpool, tid)) goto err;
             valid = true;
             if (clips.count > 0) {
-                if (!imageGenRle(&image, curBox, false)) goto end;
+                if (!imageGenRle(&image, curBox, false)) goto err;
                 if (image.rle) {
                     //Clear current task memorypool here if the clippers would use the same memory pool
                     imageDelOutline(&image, mpool, tid);

commit c837d9a59ed59fbf888b1fdc047269a3c3ba862f

sw_engine: recovered performance regression after v1.0-pre26
Streamlined the update logic computation
with a more accurate retained drawing region.

This fixes the performance regression -5% introduced by
commit b2875b6c1428528e1235197e116c8ff724787d7b.

this also fixes https://github.com/thorvg/thorvg/issues/3674

src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -40,8 +40,9 @@ struct SwTask : Task
 {
     SwSurface* surface = nullptr;
     SwMpool* mpool = nullptr;
-    RenderRegion curBox = {};  //current rendering region
-    RenderRegion prvBox = {};  //previous rendering region
+    RenderRegion clipBox;      //clipping region applied to the task, may differ from curBox which is the actual rendering region
+    RenderRegion curBox{};     //current rendering region
+    RenderRegion prvBox{};     //previous rendering region
     Matrix transform;
     Array<RenderData> clips;
     RenderDirtyRegion* dirtyRegion;
@@ -89,15 +90,10 @@ struct SwShapeTask : SwTask
 
     float validStrokeWidth(bool clipper)
     {
-        if (!rshape->stroke) return 0.0f;
-
-        auto width = rshape->stroke->width;
-        if (tvg::zero(width)) return 0.0f;
-
-        if (!clipper && (!rshape->stroke->fill && (MULTIPLY(rshape->stroke->color.a, opacity) == 0))) return 0.0f;
+        if (!rshape->stroke || tvg::zero(rshape->stroke->width)) return 0.0f;
+        if (!clipper && (!rshape->stroke->fill && (rshape->stroke->color.a == 0))) return 0.0f;
         if (tvg::zero(rshape->stroke->trim.begin - rshape->stroke->trim.end)) return 0.0f;
-
-        return (width * sqrt(transform.e11 * transform.e11 + transform.e12 * transform.e12));
+        return (rshape->stroke->width * sqrt(transform.e11 * transform.e11 + transform.e12 * transform.e12));
     }
 
     bool clip(SwRle* target) override
@@ -117,19 +113,18 @@ struct SwShapeTask : SwTask
         }
 
         auto strokeWidth = validStrokeWidth(clipper);
-        auto renderBox = curBox;
         auto updateShape = flags & (RenderUpdateFlag::Path | RenderUpdateFlag::Transform | RenderUpdateFlag::Clip);
-        auto updateFill = flags & (RenderUpdateFlag::Color | RenderUpdateFlag::Gradient);
+        auto updateFill = (flags & (RenderUpdateFlag::Color | RenderUpdateFlag::Gradient));
 
         //Shape
-        if (updateShape || updateFill) {
-            if (updateShape) shapeReset(shape);
-            if (!shape.rle || shape.rle->invalid()) {
-                if (shapePrepare(shape, rshape, transform, curBox, renderBox, mpool, tid, clips.count > 0 ? true : false)) {
-                    if (!shapeGenRle(shape, renderBox, antialiasing(strokeWidth))) goto err;
+        if (updateShape) {
+            shapeReset(shape);
+            if (rshape->fill || rshape->color.a > 0 || clipper) {
+                if (shapePrepare(shape, rshape, transform, clipBox, curBox, mpool, tid, clips.count > 0 ? true : false)) {
+                    if (!shapeGenRle(shape, curBox, antialiasing(strokeWidth))) goto err;
                 } else {
                     updateFill = false;
-                    renderBox.reset();
+                    curBox.reset();
                 }
             }
         }
@@ -145,7 +140,7 @@ struct SwShapeTask : SwTask
         if (updateShape || flags & RenderUpdateFlag::Stroke) {
             if (strokeWidth > 0.0f) {
                 shapeResetStroke(shape, rshape, transform);
-                if (!shapeGenStrokeRle(shape, rshape, transform, curBox, renderBox, mpool, tid)) goto err;
+                if (!shapeGenStrokeRle(shape, rshape, transform, clipBox, curBox, mpool, tid)) goto err;
                 if (auto fill = rshape->strokeFill()) {
                     auto ctable = (flags & RenderUpdateFlag::GradientStroke) ? true : false;
                     if (ctable) shapeResetStrokeFill(shape);
@@ -168,7 +163,6 @@ struct SwShapeTask : SwTask
         }
 
         valid = true;
-        curBox = renderBox; //sync
         if (!nodirty) dirtyRegion->add(prvBox, curBox);
         return;
 
@@ -204,8 +198,6 @@ struct SwImageTask : SwTask
             if (flags & RenderUpdateFlag::Color) invisible();
             return;
         }
-
-        auto clipBox = curBox;
 
         //Convert colorspace if it's not aligned.
         rasterConvertCS(source, surface->cs);
@@ -859,11 +851,9 @@ void SwRenderer::dispose(RenderData data)
 
 void* SwRenderer::prepareCommon(SwTask* task, const Matrix& transform, const Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags)
 {
-    if (!surface || (transform.e11 == 0.0f && transform.e12 == 0.0f) || (transform.e21 == 0.0f && transform.e22 == 0.0f)) return task;  //invalid
-
     task->surface = surface;
     task->mpool = mpool;
-    task->curBox = RenderRegion::intersect(vport, {{0, 0}, {int32_t(surface->w), int32_t(surface->h)}});
+    task->clipBox = RenderRegion::intersect(vport, {{0, 0}, {int32_t(surface->w), int32_t(surface->h)}});
     task->transform = transform;
     task->clips = clips;
     task->dirtyRegion = &dirtyRegion;