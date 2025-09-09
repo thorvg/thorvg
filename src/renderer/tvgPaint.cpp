thorvg/thorvg

commit 7c834b4c8ac86151aa1a4bb831d40c44abe85931

renderer: paint duplication++
supplemented a few missing properites at duplicate()

src/renderer/tvgPaint.cpp
@@ -145,18 +145,16 @@ Iterator* Paint::Impl::iterator()
 
 Paint* Paint::Impl::duplicate(Paint* ret)
 {
-    if (ret) ret->mask(nullptr, MaskMethod::None);
-
     PAINT_METHOD(ret, duplicate(ret));
-
-    //duplicate Transform
-    ret->pImpl->tr = tr;
-    ret->pImpl->mark(RenderUpdateFlag::Transform);
-
-    ret->pImpl->opacity = opacity;
 
     if (maskData) ret->mask(maskData->target->duplicate(), maskData->method);
     if (clipper) ret->clip(static_cast<Shape*>(clipper->duplicate()));
+
+    ret->pImpl->tr = tr;
+    ret->pImpl->blendMethod = blendMethod;
+    ret->pImpl->opacity = opacity;
+    ret->pImpl->hidden = hidden;
+    ret->pImpl->mark(RenderUpdateFlag::All);
 
     return ret;
 }

src/renderer/tvgPicture.h
@@ -196,6 +196,7 @@ struct PictureImpl : Picture
         }
 
         dup->bitmap = bitmap;
+        dup->origin = origin;
         dup->w = w;
         dup->h = h;
         dup->resizing = resizing;

src/renderer/tvgRender.h
@@ -310,20 +310,17 @@ struct RenderStroke
         else fill = nullptr;
 
         tvg::free(dash.pattern);
+        dash = rhs.dash;
         if (rhs.dash.count > 0) {
             dash.pattern = tvg::malloc<float*>(sizeof(float) * rhs.dash.count);
             memcpy(dash.pattern, rhs.dash.pattern, sizeof(float) * rhs.dash.count);
-        } else {
-            dash.pattern = nullptr;
         }
-        dash.count = rhs.dash.count;
-        dash.offset = rhs.dash.offset;
-        dash.length = rhs.dash.length;
+
         miterlimit = rhs.miterlimit;
+        trim = rhs.trim;
         cap = rhs.cap;
         join = rhs.join;
         first = rhs.first;
-        trim = rhs.trim;
     }
 
     ~RenderStroke()

src/renderer/tvgScene.h
@@ -323,6 +323,8 @@ struct SceneImpl : Scene
             }
         }
 
+        if (fixed) dup->size(fsize);
+
         return scene;
     }
 

src/renderer/tvgShape.h
@@ -468,20 +468,18 @@ struct ShapeImpl : Shape
     Paint* duplicate(Paint* ret)
     {
         auto shape = static_cast<Shape*>(ret);
-        if (shape) shape->reset();
-        else shape = Shape::gen();
-
+        if (!shape) shape = Shape::gen();
         auto dup = SHAPE(shape);
-        delete(dup->rs.fill);
-
-        //Default Properties
-        dup->impl.mark(RenderUpdateFlag::All);
-        dup->rs.rule = rs.rule;
-        dup->rs.color = rs.color;
 
         //Path
+        dup->rs.path.clear();
         dup->rs.path.cmds.push(rs.path.cmds);
         dup->rs.path.pts.push(rs.path.pts);
+
+        //Fill
+        delete(dup->rs.fill);
+        if (rs.fill) dup->rs.fill = rs.fill->duplicate();
+        else dup->rs.fill = nullptr;
 
         //Stroke
         if (rs.stroke) {
@@ -492,9 +490,8 @@ struct ShapeImpl : Shape
             dup->rs.stroke = nullptr;
         }
 
-        //Fill
-        if (rs.fill) dup->rs.fill = rs.fill->duplicate();
-        else dup->rs.fill = nullptr;
+        dup->rs.color = rs.color;
+        dup->rs.rule = rs.rule;
 
         return shape;
     }

src/renderer/tvgText.h
@@ -199,9 +199,14 @@ struct TextImpl : Text
             ++dup->loader->sharing;
         }
 
+        dup->metrics = metrics;
         dup->utf8 = tvg::duplicate(utf8);
-        dup->italicShear = italicShear;
         dup->fontSize = fontSize;
+        dup->italicShear = italicShear;
+        dup->outlineWidth = outlineWidth;
+        dup->align = align;
+
+        if (box) dup->layout(box->size.x, box->size.y);
 
         return text;
     }

commit f20d47193e3150d83fb48ad754a9e02078db1a52

renderer: fixed a regression bug
reste the masking if the duplication has no masks.

regression by 7c834b4c8ac86151aa1a4bb831d40c44abe85931

src/renderer/tvgPaint.cpp
@@ -145,6 +145,8 @@ Iterator* Paint::Impl::iterator()
 
 Paint* Paint::Impl::duplicate(Paint* ret)
 {
+    if (ret) ret->mask(nullptr, MaskMethod::None);
+
     PAINT_METHOD(ret, duplicate(ret));
 
     if (maskData) ret->mask(maskData->target->duplicate(), maskData->method);