f2e6f9ba6eff5289b0ecd52f3495eff20e22317d
Hermet Park <hermet@lottiefiles.com>
2025-09-18 23:27:56 +0900

    common: ++window portability
    compare the size with a pointer is invalid.
    aligned the logic with filename()
    
    issue: https://github.com/thorvg/thorvg/issues/3807

--- src/common/tvgStr.cpp
+++ src/common/tvgStr.cpp
@@ -232,8 +232,8 @@ char* dirname(const char* path)
 {
     auto ptr = strrchr(path, '/');
 #ifdef _WIN32
-    auto backslash = strrchr(ptr ? ptr : path, '\\');
-    if (backslash > ptr) ptr = backslash;
+    auto ptr2 = strrchr(ptr ? ptr : path, '\\');
+    if (ptr2) ptr = ptr2;
 #endif
     auto len = ptr ? size_t(ptr - path + 1) : SIZE_MAX;
     return duplicate(path, len);
