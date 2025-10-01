f339f33e6c72924ec88a56efb9d9f163800873b8
Hermet Park <hermet@lottiefiles.com>
2025-10-01 18:37:34 +0900

    engines: revised the dropshadow angle
    Properly apply the transformation to the shadow angle.
    
    issue: https://github.com/thorvg/thorvg/issues/3813

--- src/renderer/sw_engine/tvgSwPostEffect.cpp
+++ src/renderer/sw_engine/tvgSwPostEffect.cpp
@@ -390,7 +390,7 @@ void effectDropShadowUpdate(RenderEffectDropShadow* params, const Matrix& transf
     }
 
     //offset
-    auto radian = tvg::deg2rad(90.0f - params->angle);
+    auto radian = tvg::deg2rad(90.0f - params->angle) - tvg::radian(transform);
     rd->offset = {(int32_t)((params->distance * scale) * cosf(radian)), (int32_t)(-1.0f * (params->distance * scale) * sinf(radian))};
 
     params->valid = true;
