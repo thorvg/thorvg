c7c62f4d9b96fe308ce9c88804f102dbe09e0a3f
Hermet Park <hermetpark@gmail.com>
2025-11-24 12:30:21 +0900

    example: apply the default font in svg sample

--- examples/PictureSvg.cpp
+++ examples/PictureSvg.cpp
@@ -30,6 +30,9 @@ struct UserExample : tvgexam::Example
 {
     bool content(tvg::Canvas* canvas, uint32_t w, uint32_t h) override
     {
+        //The default font for fallback in case
+        tvg::Text::load(EXAMPLE_DIR"/font/Arial.ttf");
+
         auto opacity = 36;
 
         //Load svg file from path
