thorvg/thorvg

commit 9494cd07bb4c68d7b916b71429a2b39b4e924a92

lottie: enhance the asset resolver for font
Pass "name:{font name}" as the src argument
when the font path is not specified.

Users now have the option to resolve the font themselves.
If src begins with "name:", they can manually
prepare the font using the given name.

Otherwise, they can load the font asset
from the provided path in src.

issue: https://github.com/thorvg/thorvg/issues/3961

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -916,14 +916,25 @@ void LottieBuilder::updateImage(LottieGroup* layer)
 }
 
 
-void LottieBuilder::updateURLFont( LottieLayer* layer, float frameNo, LottieText* text, const TextDocument& doc)
+void LottieBuilder::updateURLFont(LottieLayer* layer, float frameNo, LottieText* text, const TextDocument& doc)
 {
     //text load
+    //TODO: cache the text instance, don't need to reload every frame.
     auto paint = Text::gen();
-    if (paint->font(doc.name) != Result::Success) {
-        if (!(text->font && resolver && resolver->func(paint, text->font->path, resolver->data))) {
+    if ((paint->font(doc.name) != Result::Success) && resolver) {
+        char* src;
+        bool free = false;
+        if (text->font && text->font->path) src = text->font->path;
+        else {
+            auto len = (strlen(doc.name) + 6);
+            src = tvg::malloc<char>(sizeof(char) * len);
+            snprintf(src, len, "name:%s", doc.name);
+            free = true;
+        }
+        if (!(resolver->func(paint, src, resolver->data))) {
             paint->font(nullptr);  //fallback to any available font
         }
+        if (free) tvg::free(src);
     }
 
     //text build

inc/thorvg.h
@@ -1661,10 +1661,12 @@ struct TVG_API Picture : Paint
      *
      * @retval Result::InsufficientCondition If the picture is already loaded.
      * 
-     * @note This function must be called before @ref Picture::load()
-     *       Setting the resolver after loading will have no effect on asset resolution for that asset.
+     * @warning This function must be called before @ref Picture::load()
+     *          Setting the resolver after loading will have no effect on asset resolution for that asset.
+     * @note @p src will be either a font path or a font name. In the case of a font name, @p src will begin with "name:", e.g., "name:FreeSans-Medium".
      * @note If @c false is returned by @p func, ThorVG will attempt to resolve the resource using its internal resolution mechanism as a fallback.
      * @note To unset the resolver, pass @c nullptr as the @p func parameter.
+     *
      * @note Experimental API
      */
     Result resolver(std::function<bool(Paint* paint, const char* src, void* data)> func, void* data) noexcept;

commit 5f363f9e836824d948e69bcac59505b06e69320c

lottie: hotfix a regression during dev
fallback should be applied if the resolver is invalid

regression by 9494cd07bb4c68d7b916b71429a2b39b4e924a92

src/loaders/lottie/tvgLottieBuilder.cpp
@@ -921,7 +921,7 @@ void LottieBuilder::updateURLFont(LottieLayer* layer, float frameNo, LottieText*
     //text load
     //TODO: cache the text instance, don't need to reload every frame.
     auto paint = Text::gen();
-    if ((paint->font(doc.name) != Result::Success) && resolver) {
+    if (paint->font(doc.name) != Result::Success) {
         char* src;
         bool free = false;
         if (text->font && text->font->path) src = text->font->path;
@@ -931,7 +931,7 @@ void LottieBuilder::updateURLFont(LottieLayer* layer, float frameNo, LottieText*
             snprintf(src, len, "name:%s", doc.name);
             free = true;
         }
-        if (!(resolver->func(paint, src, resolver->data))) {
+        if (!resolver || !resolver->func(paint, src, resolver->data)) {
             paint->font(nullptr);  //fallback to any available font
         }
         if (free) tvg::free(src);