6648d791972169d9cd22168f5bd11089bbec56c9
Hermet Park <hermetpark@gmail.com>
2026-02-12 23:15:42 +0900

    bump up version v1.0.0 -> v1.0.1

--- src/bindings/capi/thorvg_capi.h
+++ src/bindings/capi/thorvg_capi.h
@@ -10,7 +10,7 @@
 
 #define TVG_VERSION_MAJOR 1  // for compile-time checks
 #define TVG_VERSION_MINOR 0  // for compile-time checks
-#define TVG_VERSION_MICRO 0  // for compile-time checks
+#define TVG_VERSION_MICRO 1  // for compile-time checks
 
 #ifndef TVG_STATIC
     #ifdef _WIN32
