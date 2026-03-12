thorvg/thorvg

commit 81c73303e938acb2f1b33e4ce0f81e21edda218c

sw_engine: improve thread safety in partial rendering
Invisible paints might not be synchronized since they are not requested
rendering. However, the sw engine may still process update the partial
draw region for those invisible patins, which can lead to a race condition
with accessing the rendering sequence.

Fixed this by handing non-visible case in the main scenquence

src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -120,8 +120,6 @@ struct SwShapeTask : SwTask
 
     void run(unsigned tid) override
     {
-        if (ready(opacity == 0 && !clipper)) return;
-
         auto strokeWidth = validStrokeWidth(clipper);
         auto updateShape = flags[0] & (RenderUpdateFlag::Path | RenderUpdateFlag::Transform | RenderUpdateFlag::Clip);
         auto updateFill = (flags[0] & (RenderUpdateFlag::Color | RenderUpdateFlag::Gradient));
@@ -203,8 +201,6 @@ struct SwImageTask : SwTask
 
     void run(unsigned tid) override
     {
-        if (ready(opacity == 0)) return;
-
         //Convert colorspace if it's not aligned.
         rasterConvertCS(source, surface->cs);
         rasterPremultiply(source);
@@ -885,6 +881,9 @@ RenderData SwRenderer::prepare(RenderSurface* surface, RenderData data, const Ma
         task = new SwImageTask;
         task->source = surface;
     }
+
+    if (task->ready(opacity == 0)) return task;
+
     task->image.filter = filter;
     return prepareCommon(task, transform, clips, opacity, flags);
 }
@@ -898,6 +897,8 @@ RenderData SwRenderer::prepare(const RenderShape& rshape, RenderData data, const
         task = new SwShapeTask;
         task->rshape = &rshape;
     }
+
+    if (task->ready(opacity == 0 && !clipper)) return task;
 
     task->clipper = clipper;
 

commit 56a04da4a2d35fc81db0ecdd3c4caa3a55f5c424

sw_engine: hotfix a regression
rendering broken by 81c73303e938acb2f1b33e4ce0f81e21edda218c

make it up to retains those precise context.

src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -836,7 +836,7 @@ void SwRenderer::dispose(RenderData data)
 }
 
 
-void* SwRenderer::prepareCommon(SwTask* task, const Matrix& transform, const Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags)
+SwTask* SwRenderer::prepareCommon(SwTask* task, const Matrix& transform, const Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags, bool ready)
 {
     if (task->disposed) return task;
 
@@ -863,6 +863,8 @@ void* SwRenderer::prepareCommon(SwTask* task, const Matrix& transform, const Arr
         static_cast<SwTask*>(*p)->done();
     }
 
+    if (task->ready(ready)) return task;
+
     if (flags) TaskScheduler::request(task);
 
     return task;
@@ -878,10 +880,8 @@ RenderData SwRenderer::prepare(RenderSurface* surface, RenderData data, const Ma
         task->source = surface;
     }
 
-    if (task->ready(opacity == 0)) return task;
-
     task->image.filter = filter;
-    return prepareCommon(task, transform, clips, opacity, flags);
+    return prepareCommon(task, transform, clips, opacity, flags, (opacity == 0));
 }
 
 RenderData SwRenderer::prepare(const RenderShape& rshape, RenderData data, const Matrix& transform, Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags, bool clipper)
@@ -893,11 +893,9 @@ RenderData SwRenderer::prepare(const RenderShape& rshape, RenderData data, const
         task->rshape = &rshape;
     }
 
-    if (task->ready(opacity == 0 && !clipper)) return task;
-
     task->clipper = clipper;
 
-    return prepareCommon(task, transform, clips, opacity, flags);
+    return prepareCommon(task, transform, clips, opacity, flags, (opacity == 0 && !clipper));
 }
 
 bool SwRenderer::term()

src/renderer/sw_engine/tvgSwRenderer.h
@@ -86,7 +86,7 @@ private:
 
     ~SwRenderer();
 
-    RenderData prepareCommon(SwTask* task, const Matrix& transform, const Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags);
+    SwTask* prepareCommon(SwTask* task, const Matrix& transform, const Array<RenderData>& clips, uint8_t opacity, RenderUpdateFlag flags, bool ready);
 };
 
 }